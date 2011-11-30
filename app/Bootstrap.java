8e948de41ef67734d30a617f7085da5c1a48588f
cristian <cristian.lucchesi@iit.cnr.it>
2011-11-30 12:28:20 +0100

    Aggiunta configurazione per fare una sequenza per tabella e caricare i dati iniziali degli utenti

---
+++ app/Bootstrap.java
@@ -0,0 +1,23 @@
+import models.Person;
+import play.jobs.Job;
+import play.jobs.OnApplicationStart;
+import play.test.Fixtures;
+
+/**
+ * 
+ */
+
+/**
+ * @author cristian
+ *
+ */
+@OnApplicationStart
+public class Bootstrap extends Job {
+	
+	public void doJob() {
+		if (Person.count() == 0) {
+			Fixtures.loadModels("initial-persons.yml");
+		} 
+	}
+
+}
