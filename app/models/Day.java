6ed0a06d13f0764d6efb37c2d01ee57b77099fb7
Dario <dario.tagliaferri@iit.cnr.it>
2012-01-18 14:41:52 +0100

    Modificato il campo data della tabella stamping per permettere il mapping del tipo LocalDate tramite hibernate sul database. Risolto bug sul metodo di importazione. Commentata un po' di logica della classe day relativa ai campi data che prima della modifica in stamping prendevano come tipo di dato per le date il tipo Date.

--- app/models/Day.java
+++ app/models/Day.java
@@ -1,23 +1,35 @@
 package models;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
 
+import javax.persistence.EntityManager;
+
+import org.joda.time.DateTime;
 import org.joda.time.LocalDate;
+
+import play.Logger;
+import play.Play;
+import play.db.jpa.JPA;
 
 
 public class Day {
 
 	private Person person;
 	
-	private boolean mealTicket;
+	//private boolean mealTicket;
 	
-	private List<AbsenceType> absenceTypeList;
+	//private List<AbsenceType> absenceTypeList;
 	
-	private List<Stamping> stampingList;
+	//private List<Stamping> stampingList;
 	
 	private String workingTimeType;
 	
@@ -27,14 +39,23 @@ public class Day {
 	
 	private int progressive;
 		
+	private static Connection postgresqlConn = null;
+	public static String myPostgresDriver = Play.configuration.getProperty("db.new.driver");
 	
-	public List<AbsenceType> absenceList(Person person, Date date){
-		List<AbsenceType> listaAssenze = new ArrayList<AbsenceType>();
-		listaAssenze = AbsenceType.find("Select abs, at from Absence abs, AbsenceType at where " +
-				"abs.AbsenceType = at and abs.person = ? and abs.date = ? ", person, date).fetch();
-		return listaAssenze;
-		
-	}	
+	
+	public static Connection getMyPostgresConnection() throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException {
+		if (postgresqlConn != null ) {
+			return postgresqlConn;
+		}
+		Class.forName(myPostgresDriver).newInstance();
+
+		return DriverManager.getConnection(
+				
+			Play.configuration.getProperty("db.new.url"),
+			Play.configuration.getProperty("db.new.user"),
+			Play.configuration.getProperty("db.new.password"));
+				
+	}
 	
 	public int mealTicketToUse(){
 		return 0;
@@ -51,19 +72,56 @@ public class Day {
 	
 	/**
 	 * 
+	 * @param person
+	 * @param date
+	 * @return la lista di codici di assenza fatti da quella persona in quella data
+	 */
+	public List<AbsenceType> absenceList(Person person, Date date){
+				
+		List<AbsenceType> listaAssenze = new ArrayList<AbsenceType>();
+		listaAssenze = AbsenceType.find("Select at, abs from AbsenceType at, Absence abs where " +
+				"at.Absence = abs and abs.person = ? and abs.date = ? ", person, date).fetch();
+		return listaAssenze;
+		
+	}	
+	
+	/**
+	 * 
 	 * @param date
 	 * @return calcola il numero di minuti di cui è composta la data passata come parametro (di cui considera solo
 	 * ora e minuti
 	 */
 	@SuppressWarnings("deprecation")
-	public int toMinute(Date date){
+	public int toMinute(LocalDate date){
 		int dateToMinute = 0;
+		
 		if (date!=null){
-			int hour = date.getHours();
-			int minute = date.getMinutes();
-			dateToMinute = (60*hour)+minute;
+			//int hour = date.getHours();
+			//int minute = date.getMinutes();
+			//dateToMinute = (60*hour)+minute;
 		}
 		return dateToMinute;
+	}
+	
+	/**
+	 * 
+	 * @param date
+	 * @return la LocalDate creata a partire dai parametri di giorno mese e anno presi dalla data passata come parametro.
+	 * N.B.: questo metodo è creato per essere usato nei casi in cui sia necessario creare esclusivamente la data senza bisogno dell'ora
+	 * (vedi metodo isWorkingDay(Person person, Date date) ).
+	 */
+	public LocalDate toLocalDate(Date date){
+		String s = date.toString();		
+		
+		int anno = Integer.parseInt(s.substring(0, 4));
+		
+		int mese = Integer.parseInt(s.substring(5, 7));
+		
+		int giorno = Integer.parseInt(s.substring(8, 10));
+		
+		LocalDate dataLocale = new LocalDate(anno, mese, giorno);
+		
+		return dataLocale;
 	}
 	
 	/**
@@ -76,31 +134,30 @@ public class Day {
 			
 			LocalDate date = new LocalDate(); 
 			date.fromDateFields(data);
+			Logger.warn("Nel metodo isHoliday la data è: " +date);
+			//System.out.println("Nel metodo idHoliday la data è: " +date);
 			if(date.getDayOfWeek() == 7)
+				return true;		
+			if((date.getMonthOfYear() == 12) && (date.getDayOfMonth() == 25))
 				return true;
-			else{
-				if((date.getMonthOfYear() == 12) && (date.getDayOfMonth() == 25))
-					return true;
-				if((date.getMonthOfYear() == 12) && (date.getDayOfMonth() == 26))
-					return true;
-				if((date.getMonthOfYear() == 12) && (date.getDayOfMonth() == 8))
-					return true;
-				if((date.getMonthOfYear() == 6) && (date.getDayOfMonth() == 2))
-					return true;
-				if((date.getMonthOfYear() == 4) && (date.getDayOfMonth() == 25))
-					return true;
-				if((date.getMonthOfYear() == 5) && (date.getDayOfMonth() == 1))
-					return true;
-				if((date.getMonthOfYear() == 8) && (date.getDayOfMonth() == 15))
-					return true;
-				if((date.getMonthOfYear() == 1) && (date.getDayOfMonth() == 1))
-					return true;
-				if((date.getMonthOfYear() == 1) && (date.getDayOfMonth() == 6))
-					return true;
-				if((date.getMonthOfYear() == 11) && (date.getDayOfMonth() == 1))
-					return true;
-			}
-			
+			if((date.getMonthOfYear() == 12) && (date.getDayOfMonth() == 26))
+				return true;
+			if((date.getMonthOfYear() == 12) && (date.getDayOfMonth() == 8))
+				return true;
+			if((date.getMonthOfYear() == 6) && (date.getDayOfMonth() == 2))
+				return true;
+			if((date.getMonthOfYear() == 4) && (date.getDayOfMonth() == 25))
+				return true;
+			if((date.getMonthOfYear() == 5) && (date.getDayOfMonth() == 1))
+				return true;
+			if((date.getMonthOfYear() == 8) && (date.getDayOfMonth() == 15))
+				return true;
+			if((date.getMonthOfYear() == 1) && (date.getDayOfMonth() == 1))
+				return true;
+			if((date.getMonthOfYear() == 1) && (date.getDayOfMonth() == 6))
+				return true;
+			if((date.getMonthOfYear() == 11) && (date.getDayOfMonth() == 1))
+				return true;			
 		}
 		return false;
 	}
@@ -110,18 +167,30 @@ public class Day {
 	 * @param date
 	 * @param person
 	 * @return true se per quella data, quella persona aveva un giorno di lavoro. False altrimenti
+	 * @throws SQLException 
+	 * @throws ClassNotFoundException 
+	 * @throws IllegalAccessException 
+	 * @throws InstantiationException 
 	 */
-	public boolean isWorkingDay(Date date, Person person){
+	public boolean isWorkingDay(Date date, Person person) throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException{
 		
-		WorkingTimeTypeDay wttd = (WorkingTimeTypeDay) WorkingTimeTypeDay.find("Select wttd from WorkingTimeType w," +
-				"WorkingTimeTypeDay wttd where w.WorkingTimeTypeDay = wttd and w.person = ? and w.date = ? ",person, date).fetch();
-		if(wttd.holiday == true){
-			return true;
+		LocalDate data = toLocalDate(date);
+		boolean isHoliday = false;
+		int day = data.getDayOfWeek();
+		//EntityManager em = JPA.em();
+		Connection mypostgresCon = getMyPostgresConnection();
+		PreparedStatement stmt = mypostgresCon.prepareStatement("Select wttd.holiday from WorkingTimeTypeDay wttd, WorkingTimeType w" +
+				"where wttd.workingTimeType = w and w.person_id = " +person+ "and w.dayOfWeek = " +day);
+		ResultSet rs = stmt.executeQuery();
+		while(rs.next()){	
+			if(rs.getBoolean("holiday") == true)
+				isHoliday = true;
+			
+			else
+				isHoliday = false;
+			
 		}
-		else{
-			return false;
-		}
-	
+		return isHoliday;
 	}
 	
 	/**
@@ -129,9 +198,9 @@ public class Day {
 	 * @param date
 	 * @return numero di minuti in cui una persona è stata a lavoro in quella data
 	 */
-	public int timeAtWork(Date date){
+	public int timeAtWork(Person person, Date date){
 		
-		List<Stamping> listStamp = Stamping.find("select s, st from Stamping s " +
+		List<Stamping> listStamp = Stamping.find("select s from Stamping s " +
 			    "where s.person = ? and s.date = ? order by date", person, date).fetch();
 		
 		int size = listStamp.size();
@@ -148,13 +217,16 @@ public class Day {
 			while(iter.hasNext()){
 				Stamping s = iter.next();
 				if(s.way == Stamping.WayType.in){
-					timeAtWork -= toMinute(s.date);					
+				//	timeAtWork -= toMinute(s.date);		
+					System.out.println("Timbratura di ingresso: "+timeAtWork);
 				}
 				if(s.way == Stamping.WayType.out){
-					timeAtWork += toMinute(s.date);
+				//	timeAtWork += toMinute(s.date);
+					System.out.println("Timbratura di uscita: "+timeAtWork);
 				}
 			}
 		}
+		System.out.println("Totale: "+timeAtWork);
 		return timeAtWork;
 		
 	}
@@ -168,11 +240,12 @@ public class Day {
 		if(date != null){
 			LocalDate data = new LocalDate();
 			data.fromDateFields(date);
-			if((data.getDayOfMonth()==1) && (data.getDayOfWeek()==6 || data.getDayOfWeek()==7)){
+			if((data.getDayOfMonth()==1) && (data.getDayOfWeek()==6 || data.getDayOfWeek()==7))
 				return 0;
-			}
+			if((data.getDayOfMonth()==2) && (data.getDayOfWeek()==7))
+					return 0;			
 			else{
-				int minutiDiLavoro = timeAtWork(date);
+				int minutiDiLavoro = timeAtWork(person,date);
 				int orarioGiornaliero = 432; //valore in minuti di una giornata lavorativa
 				difference = orarioGiornaliero-minutiDiLavoro;
 			}
@@ -222,9 +295,13 @@ public class Day {
 	 * @return se la persona può usufruire del buono pasto per quella data
 	 */
 	public boolean mealTicket(Date date, int timeAtWork, Person person){
-		if(timeAtWork >= 432)
+		if(timeAtWork == 0){
 			return true;
-		else
-			return false;
+		}
+			
+		else{
+			timeAtWork = timeAtWork(person, date);
+		}
+		return false;
 	}
 }
