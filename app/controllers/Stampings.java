b6e2b7189a58af63dfdfda14e8adcfcfb880be10
cristian <cristian.lucchesi@iit.cnr.it>
2012-01-23 16:58:46 +0100

    Nella visualizzazione del riepilogo mensile utilizzato sempre l'utente in sessione invece di passarlo come parametro.

--- app/controllers/Stampings.java
+++ app/controllers/Stampings.java
@@ -2,14 +2,24 @@ package controllers;
 
 import models.MonthRecap;
 import models.Person;
+import net.sf.oval.constraint.NotNull;
 
 import org.joda.time.LocalDate;
 
+import play.mvc.Before;
 import play.mvc.Controller;
 
 public class Stampings extends Controller {
 
-    public static void show(Long id) {
+    @Before
+    static void checkPerson() {
+        if(session.get(Application.PERSON_ID_SESSION_KEY) == null) {
+            flash.error("Please log in first");
+            Application.index();
+        }
+    }
+
+    private static void show(@NotNull Long id) {
     	Person person = Person.findById(id);
     	
     	LocalDate now = new LocalDate();
@@ -17,4 +27,7 @@ public class Stampings extends Controller {
         render(monthRecap);
     }
 
+    public static void show() {
+    	show(Long.parseLong(session.get(Application.PERSON_ID_SESSION_KEY)));
+    }
 }
