56d3d0efe80b91390ec72fa756f3e0d476e96ba1
cristian <cristian.lucchesi@iit.cnr.it>
2012-02-14 16:32:02 +0100

    Aggiunta configurazione route e metodo per la visualizzazione del riepilogo mensile.

---
+++ app/controllers/MonthRecap.java
@@ -0,0 +1,30 @@
+package controllers;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import models.Person;
+import models.PersonMonth;
+
+import org.joda.time.LocalDate;
+
+import play.data.binding.As;
+import play.mvc.Controller;
+
+public class MonthRecap extends Controller{
+	
+	public static void show(@As("yyyy-MM-dd") Date date) {
+		LocalDate localDate = new LocalDate(date);
+		List<Person> persons = Person.findAll();
+		List<PersonMonth> personMonths = new ArrayList<PersonMonth>();
+		for( Person p : persons) {
+			personMonths.add(new PersonMonth(p, localDate));
+    	
+	    	//MonthRecap monthRecap = MonthRecap.byPersonAndYearAndMonth(person, now.getYear(), now.getMonthOfYear());
+	       
+		}
+		render(date, personMonths);
+	}
+
+}
