aad4491b1a58ddb7e971f056cda041761c6f5f3a
Dario <dario.tagliaferri@iit.cnr.it>
2012-04-16 15:54:31 +0200

    aggiunta funzione show al controller vacations, aggiunta tabella al template delle ferie e aggiunta rotta sul file di routes per raggiungere la vista delle ferie. In più modificato lo stylesheet con le specifiche dei campi per la tabella delle ferie

--- app/controllers/Vacations.java
+++ app/controllers/Vacations.java
@@ -1,9 +1,48 @@
 package controllers;
 
+import models.Person;
+import models.YearRecap;
+
+import org.joda.time.LocalDate;
+
+import play.Logger;
+import play.mvc.Before;
 import play.mvc.Controller;
 
 public class Vacations extends Controller{
-	/**
-	 * TODO completare la classe
-	 */
+	
+	@Before
+    static void checkPerson() {
+        if(session.get(Application.PERSON_ID_SESSION_KEY) == null) {
+            flash.error("Please log in first");
+            Application.index();
+        }
+    }
+	
+	private static void show(Long id) {
+    	Person person = Person.findById(id);
+    	String anno = params.get("year");
+    	Logger.info("Anno: "+anno);
+    	
+    	if(anno==null){
+    		        	
+        	LocalDate now = new LocalDate();
+        	YearRecap yearRecap = YearRecap.byPersonAndYear(person, (short)now.getYear());
+            render(yearRecap);
+    	}
+    	else{
+    		Logger.info("Sono dentro il ramo else della creazione del month recap");
+    		Integer year = new Integer(params.get("year"));
+			
+    		YearRecap yearRecap = YearRecap.byPersonAndYear(person, (short)year.intValue());
+    		    		
+            render(yearRecap);
+    	}
+    	
+    }
+	
+	public static void show() {
+    	show(Long.parseLong(session.get(Application.PERSON_ID_SESSION_KEY)));
+    }
+	
 }
