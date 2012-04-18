2d4267d0ec67b2a35ca6f5753d686afb6a034ac1
Dario <dario.tagliaferri@iit.cnr.it>
2012-04-18 16:01:00 +0200

    Aggiunto controller e vista oltre ad aver aggiunto la rotta per la visualizzazione delle competenze (questo relativo al commit precedente)

---
+++ app/controllers/Competences.java
@@ -0,0 +1,56 @@
+package controllers;
+
+import it.cnr.iit.epas.ActionMenuItem;
+import models.MonthRecap;
+import models.Person;
+
+import org.joda.time.LocalDate;
+
+import play.Logger;
+import play.mvc.Before;
+import play.mvc.Controller;
+
+public class Competences extends Controller{
+
+	/* corrisponde alla voce di menu selezionata */
+	private final static ActionMenuItem actionMenuItem = ActionMenuItem.competences;
+	
+	@Before
+    static void checkPerson() {
+        if(session.get(Application.PERSON_ID_SESSION_KEY) == null) {
+            flash.error("Please log in first");
+            Application.index();
+        }
+    }
+	
+	private static void show(Long id) {
+		String menuItem = actionMenuItem.toString();
+		
+    	Person person = Person.findById(id);
+    	String anno = params.get("year");
+    	Logger.info("Anno: "+anno);
+    	String mese= params.get("month");
+    	Logger.info("Mese: "+mese);
+    	if(anno==null || mese==null){
+    		        	
+        	LocalDate now = new LocalDate();
+        	MonthRecap monthRecap = MonthRecap.byPersonAndYearAndMonth(person, now.getYear(), now.getMonthOfYear());
+            render(monthRecap, menuItem);
+    	}
+    	else{
+    		Logger.info("Sono dentro il ramo else della creazione del month recap");
+    		Integer year = new Integer(params.get("year"));
+			Integer month = new Integer(params.get("month"));
+    		MonthRecap monthRecap = MonthRecap.byPersonAndYearAndMonth(person, year.intValue(), month.intValue());
+    		Logger.info("Il month recap è formato da: " +person.id+ ", " +year.intValue()+ ", " +month.intValue());
+    		
+            render(monthRecap, menuItem);
+    	}
+    	
+    }
+	
+	public static void show() {
+    	show(Long.parseLong(session.get(Application.PERSON_ID_SESSION_KEY)));
+    }
+	
+}
