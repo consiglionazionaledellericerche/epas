31a866b805ac789c3ce314358ab6a54e820db5e0
Dario <dario.tagliaferri@iit.cnr.it>
2012-04-20 14:42:10 +0200

    Aggiunto file javascript per la creazione delle finestre popup, aggiunto nel main.html il riferimento a questo file, aggiunto poi nella show delle vacation il riferimento alla creazione di un popup con in ingresso il file vacations.html che contiene le date dei giorni di ferie presi.

--- app/controllers/Vacations.java
+++ app/controllers/Vacations.java
@@ -51,4 +51,29 @@ public class Vacations extends Controller{
     	show(Long.parseLong(session.get(Application.PERSON_ID_SESSION_KEY)));
     }
 	
+	private static void vacations(Long id){
+		Person person = Person.findById(id);
+    	String anno = params.get("year");
+    	Logger.info("Anno: "+anno);
+    	
+    	if(anno==null){
+    		        	
+        	LocalDate now = new LocalDate();
+        	YearRecap yearRecap = YearRecap.byPersonAndYear(person, (short)now.getYear());
+            render(yearRecap);
+    	}
+    	else{
+    		Logger.info("Sono dentro il ramo else della creazione del month recap");
+    		Integer year = new Integer(params.get("year"));
+			
+    		YearRecap yearRecap = YearRecap.byPersonAndYear(person, (short)year.intValue());
+    		    		
+            render(yearRecap);
+    	}
+	}
+	
+	public static void vacations() {
+    	vacations(Long.parseLong(session.get(Application.PERSON_ID_SESSION_KEY)));
+    }
+	
 }
