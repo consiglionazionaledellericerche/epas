55cdbfab63afec6f029b037fbaa00dc55a311926
cristian <cristian.lucchesi@iit.cnr.it>
2012-06-12 18:33:54 +0200

    Implementata una prima versione funzionante del metodo per ricevere le informazioni relative alla reperibilità delle persone via JSON.

--- app/models/PersonReperibilityDay.java
+++ app/models/PersonReperibilityDay.java
@@ -4,6 +4,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 
@@ -37,4 +38,7 @@ public class PersonReperibilityDay extends Model {
 	@Column(name = "holiday_day")
 	public Boolean holidayDay;
 	
+	@ManyToOne
+	@JoinColumn(name = "reperibility_type")
+	public PersonReperibilityType reperibilityType;
 }
