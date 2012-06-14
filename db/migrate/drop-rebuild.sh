25f08609d79fbe7f87a8414ea40ecda8a0eef4a8
Electronic Personnel Attendance Sheet <epas@iit.cnr.it>
2012-06-14 11:40:40 +0200

    Migliorati i log della drop-rebuild e aggiunto aggiornamento delle dipendenze.

--- db/migrate/drop-rebuild.sh
+++ db/migrate/drop-rebuild.sh
@@ -6,27 +6,38 @@ PROJECT_DIR=$CURRENT_DIR/../../
 DB_NAME=epas
 DB_USER=epas
 
+function log {
+  echo "`date`: $1"
+} 
+
 cd $PROJECT_DIR
 
 play stop
+log "Stoppato il play"
+
 git pull origin master
+log "Eseguita la git pull"
+
+play deps
+log "Aggiornate le dipendenze"
 
 #play db:export --drop --output=$PROJECT_DIR/db/migrate/drop.ddl
 #echo "Creato il file drop.dll contenente le info per il drop delle sequenze e tabelle del database"
 
 play db:export --create --output=$PROJECT_DIR/db/migrate/create.ddl
-echo "Creato il file drop.dll contenente le info per le craete delle tabelle del database"
+log "Creato il file drop.dll contenente le info per le craete delle tabelle del database"
 
 dropdb -U $DB_USER $DB_NAME
-echo "Cancellato il database $DB_NAME"
+log "Cancellato il database $DB_NAME"
 
 createdb -U $DB_USER $DB_NAME
-echo "Creato il nuovo database $DB_NAME"
+log "Creato il nuovo database $DB_NAME"
 
 #psql -U $DB_USER $DB_NAME < db/migrate/drop.ddl
 #echo "Eseguite le drop sul database $DB_NAME"
 
 psql -U $DB_USER $DB_NAME < db/migrate/create.ddl
-echo "Eseguite le create sul database $DB_NAME"
+log "Eseguite le create sul database $DB_NAME"
 
 play start
+
