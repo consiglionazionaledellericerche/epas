bba1e471d9a29094353a8a67c4ffd4e203b4fc6a
Dario <dario.tagliaferri@iit.cnr.it>
2012-06-22 17:55:23 +0200

    migliorata funzione per il recupero del vacationCode, aggiunti i controller per la visualizzazione e l'inserimento dei workingTimeType, modificata la visualizzazione del changePassword, aggiunti i permessi nel controller Security per inserire e modificare workingTimeType e password. Aggiunta view per l'inserimento di un nuovo workingTimeType e spostata la visualizzazione dei workingTimeType insieme a quest'ultima all'interno di un nuovo package relativo ai workingTimeType

--- app/controllers/WorkingTimes.java
+++ app/controllers/WorkingTimes.java
@@ -13,10 +13,18 @@ public class WorkingTimes extends Controller{
 	 * ritorna la lista dei workingTimeType da cui posso accedere ai workingTimeTypeDays per le caratteristiche che devo evidenziare
 	 * nella view
 	 */
+	@Check(Security.INSERT_AND_UPDATE_WORKINGTIME)
 	public static void manageWorkingTime(){
 		List<WorkingTimeType> wttList = WorkingTimeType.findAll();
 		
 		render(wttList);
+	}
+	
+	@Check(Security.INSERT_AND_UPDATE_WORKINGTIME)
+	public static void insertWorkingTime(){
+		/**
+		 * TODO: completare il controller 
+		 */
 	}
 	
 }
