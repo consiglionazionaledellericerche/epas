5a5e9f6ef223bb14245831cbd1d68d806ec6b2a9
arianna <arianna.delsoldato@iit.cnr.it>
2012-09-27 16:55:54 +0200

    ggiunta base per la gestione delle assenze via json

---
+++ app/models/exports/AbsenceReperibilityPeriod.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package models.exports;
+
+import org.joda.time.LocalDate;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import models.Person;
+import models.PersonReperibilityType;
+
+/**
+ * Classe di supporto per l'importazione delle informazioni relative
+ * ai giorni di assenza delle persone reperibili.
+ * 
+ * @author arianna
+ *
+ */
+public class AbsenceReperibilityPeriod {
+
+	public final Person person;
+	public final LocalDate start;
+	public LocalDate end;
+	public final PersonReperibilityType reperibilityType;
+
+	public AbsenceReperibilityPeriod(Person person, LocalDate start, PersonReperibilityType type) {
+		this.person = person;
+		this.start = start;
+		this.reperibilityType = type;
+	}
+
+	public AbsenceReperibilityPeriod(Person person, LocalDate start, LocalDate end, PersonReperibilityType type) {
+		this.person = person;
+		this.start = start;
+		this.end = end;
+		this.reperibilityType = type;
+	}
+}
