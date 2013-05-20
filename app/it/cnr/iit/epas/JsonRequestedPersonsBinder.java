8c5a3f0f79e8411f6e393065b7d40a8ade99fcc3
arianna <arianna.delsoldato@iit.cnr.it>
2013-05-20 18:23:29 +0200

    Implementati i servizi REST per la lettura e la scrittura delle ore di straordinario disponibili al Responsabile per la distribuzione ai dipendenti (Portale SISTorg)
    Implemento il servizio REST per la creazione del file PDF contenente le
    motivazioni e le ore di straordinario mensili dei dipendenti

---
+++ app/it/cnr/iit/epas/JsonRequestedPersonsBinder.java
@@ -0,0 +1,85 @@
+/**
+ * 
+ */
+package it.cnr.iit.epas;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import models.Competence;
+import models.CompetenceCode;
+import models.Person;
+import models.exports.PersonsCompetences;
+import models.exports.PersonsList;
+import models.exports.ReperibilityPeriod;
+import models.exports.ReperibilityPeriods;
+
+import org.joda.time.LocalDate;
+
+import play.Logger;
+import play.data.binding.Global;
+import play.data.binding.TypeBinder;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import controllers.Persons;
+
+
+/**
+ * @author arianna
+ *
+ */
+@Global
+public class JsonRequestedPersonsBinder implements TypeBinder<PersonsList> {
+
+	/**
+	 * @see play.data.binding.TypeBinder#bind(java.lang.String, java.lang.annotation.Annotation[], java.lang.String, java.lang.Class, java.lang.reflect.Type)
+	 */
+	@Override
+	public Object bind(String name, Annotation[] annotations, String value,	Class actualClass, Type genericType) throws Exception {
+		
+		Logger.debug("binding Persons: %s, %s, %s, %s, %s", name, annotations, value, actualClass, genericType);
+		try {
+			List<Person> persons = new ArrayList<Person>();
+			Logger.debug("letto vaue = %s", value);
+			
+			JsonArray jsonArray = new JsonParser().parse(value).getAsJsonArray();
+			Logger.debug("jsonArray = %s", jsonArray);
+
+			JsonObject jsonObject = null;
+			Person person = null;
+			String personEmail = "";
+			
+			for (JsonElement jsonElement : jsonArray) {
+				
+				jsonObject = jsonElement.getAsJsonObject();
+				Logger.trace("jsonObject = %s", jsonObject);
+				
+				personEmail = jsonObject.get("email").getAsString();
+				
+				person = Person.find("SELECT p FROM Person p WHERE p.contactData.email = ? ORDER BY p.surname", personEmail).first();
+				if (person == null) {
+					throw new IllegalArgumentException(String.format("Person with email = %s doesn't exist", personEmail));			
+				}
+				Logger.debug("Find person %s with email %s", person.name, personEmail);
+				
+				persons.add(person);
+			}
+			
+			Logger.debug("persons = %s", persons);
+			
+			return new PersonsList(persons);
+			
+		} catch (Exception e) {
+			Logger.error(e, "Problem during binding List<Person>.");
+			throw e;
+		}
+	}
+	
+}
