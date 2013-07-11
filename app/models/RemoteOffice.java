3eba33338594445f314433012ea5fd9158eec7f5
dario <dario.tagliaferri@iit.cnr.it>
2013-07-11 12:12:15 +0200

    Aggiunta la classe RemoteOffice legata alla persona che serve per capire se una persona appartiene a una sede centrale o distaccata

---
+++ app/models/RemoteOffice.java
@@ -0,0 +1,34 @@
+package models;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import org.hibernate.envers.NotAudited;
+
+import play.db.jpa.Model;
+
+/**
+ * 
+ * @author dario
+ *
+ */
+
+@Entity
+@Table(name = "remote_office")
+public class RemoteOffice extends Model{
+
+	@Column
+	public String description;
+	
+	@Column
+	public String address;
+	
+	@NotAudited
+	@OneToMany(mappedBy="remoteOffice", fetch=FetchType.LAZY)
+	public List<Person> persons;
+}
