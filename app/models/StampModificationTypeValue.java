c47abbd2f3c60a0cf8b71183170dcb2ecad17e1e
dario <dario.tagliaferri@iit.cnr.it>
2013-10-03 18:04:28 +0200

    Aggiunto modification type relativo al tempo di lavoro fisso. E aggiunto valore nella classe stampModificationTypeValue. Modificata la visualizzazione di timbrature e situazione mensile contenente adesso anche il codice di timbrature a cavallo della mezzanotte. Migliorato l'algoritmo che controllava le timbrature a cavallo della mezzanotte

--- app/models/StampModificationTypeValue.java
+++ app/models/StampModificationTypeValue.java
@@ -7,7 +7,9 @@ public enum StampModificationTypeValue {
     FOR_DAILY_LUNCH_TIME(1l),
     FOR_MIN_LUNCH_TIME(2l),
     MARKED_BY_ADMIN(3l),
-    TO_CONSIDER_TIME_AT_TURN_OF_MIDNIGHT(4l);
+    TO_CONSIDER_TIME_AT_TURN_OF_MIDNIGHT(4l),
+    ACTUAL_TIME_AT_WORK(5l),
+    FIXED_WORKINGTIME(6l);
     
     
     private Long id;
