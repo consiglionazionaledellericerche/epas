a85888d050ca6d3be2f0ea86668e675c1cf115d1
dario <dario.tagliaferri@iit.cnr.it>
2014-01-09 14:30:04 +0100

    Aggiunta rotta per la modifica di un codice di assenza

--- app/controllers/Absences.java
+++ app/controllers/Absences.java
@@ -859,7 +859,7 @@ public class Absences extends Controller{
 		absence.save();
 
 		flash.success("Modificato codice di assenza %s", absence.code);
-		render("@Stampings.redirectToIndex");
+		Absences.manageAbsenceCode();
 
 	}
 
