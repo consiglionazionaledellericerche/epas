67a17f6c844fa2703577bcfca6590a6c86460566
alessandro.martelli <alessandro.martelli@iit.cnr.it>
2014-01-10 17:27:01 +0100

    catturata eccezione fixPersonSituation in fase rollback

--- app/it/cnr/iit/epas/PersonUtility.java
+++ app/it/cnr/iit/epas/PersonUtility.java
@@ -1092,7 +1092,7 @@ public class PersonUtility {
 			JPAPlugin.startTx(false);
 			while(!actualMonth.isAfter(endMonth))
 			{
-				Logger.info("Mese inizio %s", actualMonth);
+				//Logger.info("Mese inizio %s", actualMonth);
 				JPAPlugin.closeTx(false);
 				JPAPlugin.startTx(false);
 				List<PersonDay> pdList = PersonDay.find("Select pd from PersonDay pd where pd.person = ? and pd.date between ? and ? order by pd.date", 
@@ -1101,16 +1101,28 @@ public class PersonUtility {
 						actualMonth.dayOfMonth().withMaximumValue())
 						.fetch();
 				for(PersonDay pd : pdList){
-					if(pd.date.getMonthOfYear()==6)Logger.info("Giorno inizio %s", pd.date);
 					
+					//JPAPlugin.closeTx(false);
+					//JPAPlugin.startTx(false);
+					
+					//pd = PersonDay.find("Select pd from PersonDay pd where pd.person = ? and pd.date = ?", pd.person, pd.date).first();
 					pd.populatePersonDay();
-					if(pd.date.getMonthOfYear()==6)Logger.info("Giorno fine %s", pd.date);
+					
+					//JPAPlugin.closeTx(false);
+					//JPAPlugin.startTx(false);
+					
 				}
-				actualMonth = actualMonth.plusMonths(1);
-				JPAPlugin.closeTx(false);
-				JPAPlugin.startTx(false);
-				Logger.info("Mese fine %s", actualMonth);
 				
+				try{
+					JPAPlugin.closeTx(false);
+				}catch(Exception e)
+				{
+					Logger.error("Impossibile fixare Mese %s", actualMonth.getMonthOfYear());
+				}
+				JPAPlugin.startTx(false);
+				//Logger.info("Mese fine %s", actualMonth);
+				
+				actualMonth = actualMonth.plusMonths(1);
 			}
 			JPAPlugin.closeTx(false);
 		}
