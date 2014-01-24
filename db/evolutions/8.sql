c7bd6c91e9646510e44bb9def7c8131447012cf8
Daniele Murgia <daniele.murgia@iit.cnr.it>
2014-01-24 17:13:06 +0100

    Sistemata evoluzione anche per la tabella absences_history

--- db/evolutions/8.sql
+++ db/evolutions/8.sql
@@ -1,8 +1,9 @@
 # ---!Ups
 
 ALTER TABLE absences RENAME COLUMN absencerequest TO absence_file;
-
+ALTER TABLE absences_history RENAME COLUMN absencerequest TO absence_file;
 
 # -- !Downs
 
-ALTER TABLE  absences RENAME COLUMN absence_file TO absencerequest;
+ALTER TABLE absences RENAME COLUMN absence_file TO absencerequest;
+ALTER TABLE absences_history RENAME COLUMN absence_file TO absencerequest;
