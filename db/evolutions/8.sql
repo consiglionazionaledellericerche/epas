c269259107459e69201149354f403f4b497496ca
dario <dario.tagliaferri@iit.cnr.it>
2014-01-24 17:18:17 +0100

    Aggiunta modifica del campo data alla tabella dello storico dei person_children

--- db/evolutions/8.sql
+++ db/evolutions/8.sql
@@ -3,6 +3,8 @@
 ALTER TABLE absences RENAME COLUMN absencerequest TO absence_file;
 ALTER TABLE absences_history RENAME COLUMN absencerequest TO absence_file;
 
+ALTER TABLE "person_children_history" ALTER COLUMN "borndate" TYPE date using ("borndate"::text::date);
+
 # -- !Downs
 
 ALTER TABLE absences RENAME COLUMN absence_file TO absencerequest;
