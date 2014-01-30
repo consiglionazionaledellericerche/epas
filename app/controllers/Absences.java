b0d274fa5ecfb1277731d4ab85a85cdc29e6da0f
dario <dario.tagliaferri@iit.cnr.it>
2014-01-30 12:49:19 +0100

    Aggiunto controllo sul codice di assenza del figlio e risolto bug nella visualizzazione delle liste di codici di assenza con allegato

--- app/controllers/Absences.java
+++ app/controllers/Absences.java
@@ -1236,9 +1236,9 @@ public class Absences extends Controller{
 		List<Absence> absenceList = Absence.find("Select abs from Absence abs where abs.absenceType.absenceTypeGroup is null and " +
 				"abs.personDay.date between ? and ?", 
 				beginMonth, beginMonth.dayOfMonth().withMaximumValue()).fetch();
-		
+		List<Absence> listaAssenze = null;
 		for(Absence abs : absenceList){
-			List<Absence> listaAssenze = null;
+			
 			if(abs.absenceFile.get() != null){
 				if(!tableAbsences.containsColumn(abs.absenceType.code)){
 					Logger.debug("Absence type per assenza %s : %s", abs, abs.absenceType.code);
@@ -1247,7 +1247,9 @@ public class Absences extends Controller{
 					tableAbsences.put(abs.personDay.date.getDayOfMonth(), abs.absenceType.code, listaAssenze);
 				}
 				else{
-					listaAssenze = tableAbsences.get(abs.personDay.date.getDayOfMonth(), abs.absenceType.code);
+					listaAssenze = tableAbsences.remove(abs.personDay.date.getDayOfMonth(), abs.absenceType.code);
+					if(listaAssenze == null)
+						listaAssenze = new ArrayList<Absence>();
 					listaAssenze.add(abs);
 					tableAbsences.put(abs.personDay.date.getDayOfMonth(), abs.absenceType.code, listaAssenze);
 				}					
