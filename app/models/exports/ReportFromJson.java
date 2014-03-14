6bf199925b59e3f88a97ec75eafa848bf2d4d333
dario <dario.tagliaferri@iit.cnr.it>
2014-03-14 16:26:12 +0100

    Aggiunti all'indice file mancanti per il corretto funzionamento del sistema di feedback

---
+++ app/models/exports/ReportFromJson.java
@@ -0,0 +1,15 @@
+package models.exports;
+
+/**
+ * 
+ * @author dario
+ * classe in cui bindare il json proveniente dalle segnalazioni
+ */
+public class ReportFromJson {
+	
+	public String note;
+	public String url;
+	public String browserInfo;
+	public byte[] image;
+	
+}
