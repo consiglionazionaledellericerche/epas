3b6b89f0cad25ba25d96c1b7e5672df64bd948b9
alessandro.martelli <alessandro.martelli@iit.cnr.it>
2014-03-21 14:56:34 +0100

    Aggiunta la possibilità di cancellare il contratto. Migliorato il controllo dei parametri del controller per modificare le date del contratto. Quando un contratto viene modificato nelle date vengono aggiornati anche i periodi di tipo orario. Fixato bug modifica password. Eliminata una cospicua quantità di params.get

---
+++ app/helpers/LocalDateBinder.java
@@ -0,0 +1,27 @@
+package helpers;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+
+import org.eclipse.jdt.internal.compiler.lookup.TypeBinding;
+import org.joda.time.LocalDate;
+
+import play.data.binding.Global;
+import play.data.binding.TypeBinder;
+import play.data.binding.types.DateBinder;
+
+@Global
+public class LocalDateBinder implements TypeBinder<LocalDate>{
+	
+	private static final DateBinder DATE_BINDER = new DateBinder();
+
+	@Override
+	public Object bind(String name, Annotation[] annotations, String value,
+			Class actualClass, Type genericType) throws Exception {
+		if( value == null || value.trim().isEmpty())
+		{
+			return null;
+		}
+		return new LocalDate(DATE_BINDER.bind(name,  annotations,  value,  actualClass, genericType));
+	}
+}
