aac67c1ed23c9e290c7c15eb1215b760c86b53ba
alessandro.martelli <alessandro.martelli@iit.cnr.it>
2014-04-02 11:31:48 +0200

    closes #341

--- db/evolutions/22.sql
+++ db/evolutions/22.sql
@@ -1,7 +1,7 @@
 # ---!Ups
 
 CREATE SEQUENCE seq_users
-  START WITH 1000
+  START WITH 10000
   INCREMENT BY 1
   NO MINVALUE
   NO MAXVALUE;
@@ -66,13 +66,63 @@ INSERT INTO users(id, username, password) SELECT  id, username, password FROM pe
 ALTER TABLE persons
   DROP COLUMN username,
   DROP COLUMN password;
+  
+ALTER TABLE persons_history
+  DROP COLUMN username,
+  DROP COLUMN password;
 
 INSERT INTO users_permissions(users_id, permissions_id) SELECT users_id, permissions_id FROM persons_permissions;
 
 DROP TABLE persons_permissions;
+DROP TABLE persons_permissions_history;
 
 UPDATE persons set user_id = id;
 
 DELETE FROM persons_working_time_types where person_id = 1;
 DELETE FROM persons where id = 1;
+
+
+# ---!Downs
+
+CREATE TABLE persons_permissions
+(
+  users_id bigint NOT NULL,
+  permissions_id bigint NOT NULL,
+  CONSTRAINT fkf0f758334428ea9 FOREIGN KEY (permissions_id)
+      REFERENCES permissions (id),
+  CONSTRAINT fkf0f7583a4f8de2b FOREIGN KEY (users_id)
+      REFERENCES persons (id)
+);
+
+CREATE TABLE persons_permissions_history
+(
+  _revision integer NOT NULL,
+  users_id bigint NOT NULL,
+  permissions_id bigint NOT NULL,
+  _revision_type smallint,
+  CONSTRAINT persons_permissions_history_pkey PRIMARY KEY (_revision, users_id, permissions_id),
+  CONSTRAINT fk799ecdd8d54d10ea FOREIGN KEY (_revision)
+      REFERENCES revinfo (rev)
+);
+
+ALTER TABLE persons 
+  ADD COLUMN username character varying(255),
+  ADD COLUMN password character varying(255),
+  DROP CONSTRAINT fkd78fcfbe47140efe,
+  DROP COLUMN user_id;
+  
+ALTER TABLE persons_history 
+  ADD COLUMN username character varying(255),
+  ADD COLUMN password character varying(255),
+  DROP COLUMN user_id;
+  
+DROP TABLE users_permissions_history;
+
+DROP TABLE users_permissions;
+
+DROP TABLE users_history;
+
+DROP TABLE users;
+
+DROP SEQUENCE seq_users;
 
