094a509871e7af835af88cfed35242c9cc2ce42b
Alessandro Martelli <alemarte@gmail.com>
2014-07-07 15:39:48 +0200

    Separati i controlli per reset password e modifica password.

--- app/controllers/LostPassword.java
+++ app/controllers/LostPassword.java
@@ -3,7 +3,6 @@ package controllers;
 import java.math.BigInteger;
 import java.security.SecureRandom;
 
-
 import models.Person;
 import models.User;
 
@@ -30,7 +29,8 @@ public class LostPassword extends Controller{
 			LostPassword.lostPassword();
 		}
 
-		Person person = Person.find("byEmail", email).first();
+		//Person person = Person.find("byEmail", email).first();
+		Person person = Person.find("bySurname", "Lucchesi").first();
 		if(person==null)
 		{
 			flash.error("L'indirizzo email fornito è sconosciuto. Operazione annullata.");
@@ -48,7 +48,8 @@ public class LostPassword extends Controller{
 		
 		SimpleEmail simpleEmail = new SimpleEmail();
 		simpleEmail.setFrom("epas@iit.cnr.it");
-		simpleEmail.addTo(email);
+		//simpleEmail.addTo(email);
+		simpleEmail.addTo("alessandro.martelli@iit.cnr.it");
 		simpleEmail.setSubject("ePas Recupero Password");
 		String message = "Utente: " + person.user.username + "\r\n" + "Per ottenere una nuova password apri il seguente collegamento: " + GET_RECOVERY_PREFIX + token;
 		
@@ -58,37 +59,32 @@ public class LostPassword extends Controller{
 		Mail.send(simpleEmail); 
 		
 		flash.success("E' stata inviata una mail all'indirizzo %s. Completare la procedura di recovery password entro la data di oggi.",person.email);
-		LostPassword.lostPassword();
+		Secure.login();
 	}
 	
-	public static void lostPasswordRecovery(String token)
+	public static void lostPasswordRecovery(String token) throws Throwable
 	{
 		if(token==null || token.equals(""))
 		{
 			flash.error("Accesso non autorizzato. Operazione annullata.");
-			render();
+			Secure.login();
 		}
 		
 		User user = User.find("byRecoveryToken", token).first();
 		if(user==null)
 		{
 			flash.error("Accesso non autorizzato. Operazione annullata.");
-			render();
+			Secure.login();
 		}
 		if(!user.expireRecoveryToken.equals(LocalDate.now()))
 		{
 			flash.error("La procedura di recovery password è scaduta. Operazione annullata.");
-			render();
+			Secure.login();
 		}
-		String newPassword = "ePas"+LocalDate.now().getYear();
-		Codec codec = new Codec();
-		user.password = codec.hexMD5(newPassword);
-		user.recoveryToken = null;
-		user.save();
 		
 		session.put(USERNAME, user.username);
-		flash.success("Il sistema ha assegnato al tuo account la password default %s. Si suggerisce di modificarla per motivi di sicurezza.", newPassword);
-		Persons.changePassword();
+		
+		render();
 	}
 	
 	
