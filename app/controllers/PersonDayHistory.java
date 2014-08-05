ed6e348a65f4f5794c867f845ec6c6902cad5f5b
Alessandro Martelli <alemarte@gmail.com>
2014-08-05 11:59:28 +0200

    Scritta una prima versione per visualizzare lo storico del giorno nel template personStampings

--- app/controllers/PersonDayHistory.java
+++ app/controllers/PersonDayHistory.java
@@ -1,13 +1,18 @@
 package controllers;
 
 import java.util.List;
+import java.util.Set;
 
 import javax.inject.Inject;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
 
 import models.Stamping;
 import play.mvc.Controller;
 import dao.HistoryValue;
 import dao.PersonDayHistoryDao;
+import dao.StampingHistoryDao;
 
 /**
  * @author marco
@@ -19,9 +24,28 @@ public class PersonDayHistory extends Controller {
 	@Inject
 	static PersonDayHistoryDao personDayHistoryDao;
 	
+	@Inject
+	static StampingHistoryDao stampingHistoryDao;
+	
+	
 	public static void stampings(long personDayId) {
-		List<HistoryValue<Stamping>> results = personDayHistoryDao
+		List<HistoryValue<Stamping>> allStampings = personDayHistoryDao
 				.stampings(personDayId);
+		
+		Set<Long> stampingIds = Sets.newHashSet();
+		for(HistoryValue<Stamping> historyValue : allStampings) {
+			stampingIds.add(historyValue.value.id);
+		}
+		
+		List<HistoryValue<Stamping>> results = Lists.newArrayList();
+		
+		for(Long stampingId : stampingIds) {
+			
+			List<HistoryValue<Stamping>> stampings = stampingHistoryDao
+					.stampings(stampingId);
+			results.addAll(stampings);
+		}
+		
 		render(results);
 	}
 }
