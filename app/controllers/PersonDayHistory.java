f10ccb46ba80ddc71670c9de5df5478003f9523c
Alessandro Martelli <alemarte@gmail.com>
2014-08-07 11:45:38 +0200

    Conclusa implementazione storico giorno

--- app/controllers/PersonDayHistory.java
+++ app/controllers/PersonDayHistory.java
@@ -8,11 +8,14 @@ import javax.inject.Inject;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 
+import models.Absence;
+import models.PersonDay;
 import models.Stamping;
 import play.mvc.Controller;
-import dao.HistoryValue;
-import dao.PersonDayHistoryDao;
-import dao.StampingHistoryDao;
+import dao.history.AbsenceHistoryDao;
+import dao.history.HistoryValue;
+import dao.history.PersonDayHistoryDao;
+import dao.history.StampingHistoryDao;
 import edu.emory.mathcs.backport.java.util.Collections;
 
 /**
@@ -28,8 +31,39 @@ public class PersonDayHistory extends Controller {
 	@Inject
 	static StampingHistoryDao stampingHistoryDao;
 	
+	@Inject
+	static AbsenceHistoryDao absenceHistoryDao;
+	
 	
 	public static void personDayHistory(long personDayId) {
+
+		PersonDay pd = PersonDay.findById(personDayId);
+		
+		// ASSENZE ////////////////////////////////////////////////////////////////////
+
+		List<HistoryValue<Absence>> allAbsences = personDayHistoryDao
+				.absences(personDayId);
+
+		Set<Long> absenceIds = Sets.newHashSet();
+		for(HistoryValue<Absence> historyValue : allAbsences) {
+			absenceIds.add(historyValue.value.id);
+		}
+
+		List<Long> sortedAbsencesIds = Lists.newArrayList(absenceIds);
+		Collections.sort(sortedAbsencesIds);
+
+		//Lista di absences
+		List<List<HistoryValue<Absence>>> historyAbsencesList = Lists.newArrayList();
+
+		for(Long absenceId : sortedAbsencesIds) {
+
+			List<HistoryValue<Absence>> historyAbsence = absenceHistoryDao
+					.absences(absenceId);
+			historyAbsencesList.add(historyAbsence);
+		}
+		
+		// TIMBRATURE /////////////////////////////////////////////////////////////////
+		
 		List<HistoryValue<Stamping>> allStampings = personDayHistoryDao
 				.stampings(personDayId);
 		
@@ -51,6 +85,8 @@ public class PersonDayHistory extends Controller {
 			historyStampingsList.add(historyStamping);
 		}
 		
-		render(historyStampingsList);
+		
+		
+		render(historyStampingsList, historyAbsencesList, pd);
 	}
 }
