0ce242e074c19017cd3de492c2494de3533538b3
Marco Andreini <marco.andreini@iit.cnr.it>
2014-08-25 10:39:08 +0200

    La collections giusta.

--- app/controllers/PersonDayHistory.java
+++ app/controllers/PersonDayHistory.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 
@@ -16,7 +17,6 @@ import dao.history.AbsenceHistoryDao;
 import dao.history.HistoryValue;
 import dao.history.PersonDayHistoryDao;
 import dao.history.StampingHistoryDao;
-import edu.emory.mathcs.backport.java.util.Collections;
 
 /**
  * @author marco
@@ -24,21 +24,21 @@ import edu.emory.mathcs.backport.java.util.Collections;
  */
 //@With(Resecure.class)
 public class PersonDayHistory extends Controller {
-	
+
 	@Inject
 	static PersonDayHistoryDao personDayHistoryDao;
-	
+
 	@Inject
 	static StampingHistoryDao stampingHistoryDao;
-	
+
 	@Inject
 	static AbsenceHistoryDao absenceHistoryDao;
-	
-	
+
+
 	public static void personDayHistory(long personDayId) {
 
 		PersonDay pd = PersonDay.findById(personDayId);
-		
+
 		// ASSENZE ////////////////////////////////////////////////////////////////////
 
 		List<HistoryValue<Absence>> allAbsences = personDayHistoryDao
@@ -61,32 +61,32 @@ public class PersonDayHistory extends Controller {
 					.absences(absenceId);
 			historyAbsencesList.add(historyAbsence);
 		}
-		
+
 		// TIMBRATURE /////////////////////////////////////////////////////////////////
-		
+
 		List<HistoryValue<Stamping>> allStampings = personDayHistoryDao
 				.stampings(personDayId);
-		
+
 		Set<Long> stampingIds = Sets.newHashSet();
 		for(HistoryValue<Stamping> historyValue : allStampings) {
 			stampingIds.add(historyValue.value.id);
 		}
-		
+
 		List<Long> sortedStampingsIds = Lists.newArrayList(stampingIds);
 		Collections.sort(sortedStampingsIds);
-		
+
 		//Lista di stampings
 		List<List<HistoryValue<Stamping>>> historyStampingsList = Lists.newArrayList();
-		
+
 		for(Long stampingId : sortedStampingsIds) {
-		
+
 			List<HistoryValue<Stamping>> historyStamping = stampingHistoryDao
 					.stampings(stampingId);
 			historyStampingsList.add(historyStamping);
 		}
-		
-		
-		
+
+
+
 		render(historyStampingsList, historyAbsencesList, pd);
 	}
 }
