ca563b48d46360b57106ae3af5566758445f34f4
Alessandro Martelli <alessandro.martelli@iit.cnr.it>
2014-11-20 17:29:33 +0100

    Integrate le dataTables

--- public/js/script.js
+++ public/js/script.js
@@ -154,7 +154,26 @@ $(function($){
 			});
 			e.preventDefault();
 		});
-		
+	    
+	    /*Data tables */
+	    // Setup - add a text input to each footer cell
+	    this.find('#example tfoot th').each( function () {
+	        var title = $('#example thead th').eq( $(this).index() ).text();
+	        $(this).html( '<input type="text" placeholder="Search '+title+'" />' );
+	    } );
+	    // DataTable
+	    var table = $('#example').DataTable();
+	    // Apply the search
+	    table.columns().eq( 0 ).each( function ( colIdx ) {
+	        $( 'input', table.column( colIdx ).footer() ).on( 'keyup change', function () {
+	            table
+	                .column( colIdx )
+	                .search( this.value )
+	                .draw();
+	        } );
+	    } );
+
+	    
 	}	/* fine initepas() */
 	
 	$('body').initepas();
