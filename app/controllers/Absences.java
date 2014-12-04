7c251bbdb5073698482731399a8133512e7cd1f7
Alessandro Martelli <alessandro.martelli@iit.cnr.it>
2014-12-04 14:29:20 +0100

    Disattivata basic auth di stampingsFromClient. Inseriti tag check e denied con target in modo da disabilitare i link di modifica per le persone di cui si dispone il solo diritto di visualizzazione.

--- app/controllers/Absences.java
+++ app/controllers/Absences.java
@@ -9,7 +9,6 @@ import it.cnr.iit.epas.PersonUtility;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
