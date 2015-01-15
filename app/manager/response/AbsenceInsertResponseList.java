d74f5573d3e04267d4e65bef2496ea22ea0bfd53
Daniele Murgia <daniele.murgia@iit.cnr.it>
2015-01-15 10:01:18 +0100

    Modificati input che sfruttano il datepicker, rimosso lombok dalle dipendenze, e altre modifiche all'absenceManager

--- app/manager/response/AbsenceInsertResponseList.java
+++ app/manager/response/AbsenceInsertResponseList.java
@@ -1,36 +1,68 @@
 package manager.response;
 
 import java.util.List;
-
 import org.joda.time.LocalDate;
-
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import com.google.common.collect.Lists;
 
-@Data @NoArgsConstructor
 public class AbsenceInsertResponseList {
-
 	private List<AbsenceInsertResponse> absences = Lists.newArrayList();
 	private List<String> warnings = Lists.newArrayList();
 	private List<LocalDate> datesInTrouble = Lists.newArrayList();
-	
 	private int totalAbsenceInsert = 0;
 	private int absenceInReperibilityOrShift = 0;
-
+	
 	public void add(AbsenceInsertResponse response) {
 		absences.add(response);
-		if(response.isInsertSucceeded()){
+		if (response.isInsertSucceeded()) {
 			totalAbsenceInsert++;
 		}
-		if(response.isDayInReperibilityOrShift()){
+		if (response.isDayInReperibilityOrShift()) {
 			absenceInReperibilityOrShift++;
 		}
 	}
-
+	
 	public boolean hasWarningOrDaysInTrouble() {
 		return !(warnings.isEmpty() && datesInTrouble.isEmpty());
+	}
+
+	public List<AbsenceInsertResponse> getAbsences() {
+		return absences;
+	}
+
+	public void setAbsences(List<AbsenceInsertResponse> absences) {
+		this.absences = absences;
+	}
+
+	public List<String> getWarnings() {
+		return warnings;
+	}
+
+	public void setWarnings(List<String> warnings) {
+		this.warnings = warnings;
+	}
+
+	public List<LocalDate> getDatesInTrouble() {
+		return datesInTrouble;
+	}
+
+	public void setDatesInTrouble(List<LocalDate> datesInTrouble) {
+		this.datesInTrouble = datesInTrouble;
+	}
+
+	public int getTotalAbsenceInsert() {
+		return totalAbsenceInsert;
+	}
+
+	public void setTotalAbsenceInsert(int totalAbsenceInsert) {
+		this.totalAbsenceInsert = totalAbsenceInsert;
+	}
+
+	public int getAbsenceInReperibilityOrShift() {
+		return absenceInReperibilityOrShift;
+	}
+
+	public void setAbsenceInReperibilityOrShift(int absenceInReperibilityOrShift) {
+		this.absenceInReperibilityOrShift = absenceInReperibilityOrShift;
 	}
 
 }
