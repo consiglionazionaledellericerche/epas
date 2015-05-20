369b2e50bfaecc8cdf1abc7d5f38dfb2292d94a9
dario <dario.tagliaferri@iit.cnr.it>
2015-05-20 15:21:11 +0200

    Riscritti i metodi da chiamare da parte di protime per conoscere tempi di lavoro e missioni delle persone. Ricreati i qmodel dal momento che su persons era stato inserito il campo iid.

--- app/manager/PersonManager.java
+++ app/manager/PersonManager.java
@@ -26,13 +26,22 @@ import org.joda.time.LocalDate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.common.collect.Lists;
+import com.google.common.reflect.TypeToken;
+import com.google.gson.Gson;
+
+import play.Play;
 import play.db.jpa.JPA;
+import play.libs.WS;
+import play.libs.WS.HttpResponse;
 import dao.ContractDao;
 import dao.OfficeDao;
 import dao.PersonChildrenDao;
 import dao.PersonDao;
 import dao.wrapper.IWrapperFactory;
 import dao.wrapper.IWrapperPersonDay;
+import dto.DepartmentDTO;
+import dto.PersonRest;
 
 public class PersonManager {
 
@@ -483,6 +492,58 @@ public class PersonManager {
 		.setParameter("end", new LocalDate(year, month, 1).dayOfMonth().withMaximumValue());
 
 		return query.getResultList().size();
+	}
+	
+	/**
+	 * questo metodo può essere chiamato dal job settimanale che sincronizza 
+	 * le email cnr del personale oppure dalla chiamata rest per conoscere i 
+	 * tempi di lavoro e le missioni del personale per la rendicontazione dei
+	 * progetti
+	 */
+	public void syncronizeCnrEmail(){
+		List<Office> helpList = officeDao.getAllOffices();
+		List<Office> officeList = Lists.newArrayList();
+		for(Office office : helpList){
+			if(office.code != null)
+				officeList.add(office);
+		}
+		int contatore = 0;
+		String url = Play.configuration.getProperty("people.rest");
+	
+		String perseoUrl = Play.configuration.getProperty("perseo.department");
+		for(Office office : officeList){
+			perseoUrl = perseoUrl+office.code.toString();
+			HttpResponse perseoResponse = WS.url(perseoUrl).get();
+			Gson gson = new Gson();
+
+			DepartmentDTO dep = gson.fromJson(perseoResponse.getJson(),DepartmentDTO.class);
+			HttpResponse response = WS.url(url+dep.code).get();
+
+			List<PersonRest> people = gson.fromJson(response.getJson().toString(),
+					new TypeToken<ArrayList<PersonRest>>() {}.getType());
+			for(PersonRest pr : people){
+				if(pr.number == null){
+					log.info("Non esiste matricola per {} {}", pr.firstname, pr.surname);
+				}
+				else{
+					Person person = personDao.getPersonByNumber(pr.number);
+					if(person != null){
+						person.cnr_email = pr.email;
+						person.iId = new Integer(pr.id);
+						person.save();
+						log.info("Salvata la mail cnr per {} {}", person.name, person.surname);
+						contatore++;
+					}
+					else{
+						log.info("La persona {} {} non è presente in anagrafica", pr.firstname, pr.surname);
+					}
+				}
+				
+			}
+			perseoUrl = Play.configuration.getProperty("perseo.department");
+
+		}
+		log.info("Terminata sincronizzazione delle email cnr per {} dipendenti", contatore);
 	}
 
 }
