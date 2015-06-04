ac6062b5f9aa97f56e28015f019171820e47c0cf
Alessandro <alessandro.martelli@iit.cnr.it>
2015-06-04 12:50:28 +0200

    Rimosso algoritmo obsoleto di riepilogo mensile.

--- app/controllers/MonthRecaps.java
+++ app/controllers/MonthRecaps.java
@@ -8,7 +8,6 @@ import java.util.List;
 
 import javax.inject.Inject;
 
-import manager.MonthRecapManager;
 import models.Contract;
 import models.ContractMonthRecap;
 import models.Person;
@@ -48,10 +47,6 @@ public class MonthRecaps extends Controller{
 	private static WrapperModelFunctionFactory wrapperFunctionFactory;
 	@Inject
 	private static IWrapperFactory wrapperFactory;
-	@Inject
-	private static MonthRecapManager monthRecapManager;
-	@Inject
-	private static SecurityRules rules;
 	
 	/**
 	 * Controller che gescisce il calcolo del riepilogo annuale residuale delle persone.
@@ -93,149 +88,6 @@ public class MonthRecaps extends Controller{
 		}
 
 		render(recaps);
-	}
-
-	/**
-	 * Controller che gestisce il calcolo del Riepilogo Mensile.
-	 * @param year
-	 * @param month
-	 * @throws ClassNotFoundException
-	 * @throws SQLException
-	 */
-	public static void show(int year, int month, String name, Integer page) {
-
-		if(page == null)
-			page = 0;
-
-		LocalDate today = new LocalDate();
-		LocalDate monthBegin = new LocalDate().withYear(year).withMonthOfYear(month).withDayOfMonth(1);
-		LocalDate monthEnd = new LocalDate().withYear(year).withMonthOfYear(month).dayOfMonth().withMaximumValue();
-		LocalDate lastDayOfMonth = monthBegin;
-
-		//numero di giorni lavorativi in month
-		int generalWorkingDaysOfMonth = 0;
-		while(!lastDayOfMonth.isAfter(monthEnd))
-		{
-			if(!lastDayOfMonth.isBefore(today))
-				break;
-			if(lastDayOfMonth.getDayOfWeek()==6 || lastDayOfMonth.getDayOfWeek()==7)
-			{
-				lastDayOfMonth = lastDayOfMonth.plusDays(1);
-				continue;
-			}
-			if(!DateUtility.isGeneralHoliday(Optional.<MonthDay>absent(), lastDayOfMonth) )
-			{
-				generalWorkingDaysOfMonth++;
-			}
-			lastDayOfMonth = lastDayOfMonth.plusDays(1);
-		}
-		lastDayOfMonth = lastDayOfMonth.minusDays(1);
-
-		Table<Person, String, Integer> tableMonthRecap = TreeBasedTable.create(MonthRecapManager.PersonNameComparator, MonthRecapManager.AbsenceCodeComparator);
-
-		SimpleResults<Person> simpleResults = personDao.list(Optional.fromNullable(name), 
-				officeDao.getOfficeAllowed(Security.getUser().get()), false, monthBegin, monthEnd, true);
-
-		List<Person> activePersons = simpleResults.paginated(page).getResults();
-
-		//logica mese attuale
-		if(today.getYear()==year && today.getMonthOfYear()==month)
-		{
-			//Se oggi e' il primo giorno del mese stampo la tabella vuota 
-			if(today.getDayOfMonth()==1)
-			{
-				tableMonthRecap = MonthRecapManager.populateDefaultTable(activePersons);
-				render(tableMonthRecap, generalWorkingDaysOfMonth, today, lastDayOfMonth, year, month, simpleResults, name);
-				return;
-			}
-			//Considero il riepilogo fino a ieri
-			monthEnd = today.minusDays(1);			
-		}	
-
-		tableMonthRecap = monthRecapManager.populateRealValueTable(activePersons, monthBegin, monthEnd, year, generalWorkingDaysOfMonth);
-
-		render(tableMonthRecap, generalWorkingDaysOfMonth, today, lastDayOfMonth, year, month, simpleResults, name);
-
-	}
-
-	/**
-	 * Controller che ritorna la lista dei Giorni di assenza non giustificati.
-	 * @param id
-	 * @param year
-	 * @param month
-	 */	
-	public static void notJustifiedAbsences(Long personId, int year, int month){
-
-		Person person = personDao.getPersonById(personId);
-		if(person == null){
-			flash.error("Persona non presente in anagrafica");
-			MonthRecaps.show(year, month, null, null);
-		}
-		rules.checkIfPermitted(person.office);
-		List<PersonDay> notJustifiedAbsences = monthRecapManager
-				.getPersonDayListRecap(personId, year, month, "notJustifiedAbsences");
-
-		render(notJustifiedAbsences, person);
-	}
-
-	/**
-	 * Controller che ritorna la lista dei Giorni di assenza giustificati.
-	 * @param id
-	 * @param year
-	 * @param month
-	 */	
-	public static void justifiedAbsences(Long personId, int year, int month){
-
-		Person person = personDao.getPersonById(personId);
-		if(person == null){
-			flash.error("Persona non presente in anagrafica");
-			MonthRecaps.show(year, month, null, null);
-		}
-		rules.checkIfPermitted(person.office);
-		List<PersonDay> justifiedAbsences = monthRecapManager
-				.getPersonDayListRecap(personId, year, month, "justifiedAbsences");
-
-		render(justifiedAbsences, person);
-	}
-
-	/**
-	 * Controller che ritorna la lista dei Giorni di presenza al lavoro nei giorni festivi.
-	 * @param id
-	 * @param year
-	 * @param month
-	 */	
-	public static void workingDayHoliday(Long personId, int year, int month){
-
-		Person person = personDao.getPersonById(personId);
-		if(person == null){
-			flash.error("Persona non presente in anagrafica");
-			MonthRecaps.show(year, month, null, null);
-		}
-		rules.checkIfPermitted(person.office);
-		List<PersonDay> workingDayHoliday = monthRecapManager
-				.getPersonDayListRecap(personId, year, month, "workingDayHoliday");
-
-		render(workingDayHoliday, person);
-	}
-
-	/**
-	 * Controller che ritorna la lista dei Giorni di presenza al lavoro nei giorni lavorativi.
-	 * @param id
-	 * @param year
-	 * @param month
-	 */	
-	public static void workingDayNotHoliday(Long personId, int year, int month){
-
-		Person person = personDao.getPersonById(personId);
-		if(person == null){
-			flash.error("Persona non presente in anagrafica");
-			MonthRecaps.show(year, month, null, null);
-		}
-		rules.checkIfPermitted(person.office);
-		List<PersonDay> workingDayNotHoliday = monthRecapManager
-				.getPersonDayListRecap(personId, year, month, "workingDayNotHoliday");
-
-		render(workingDayNotHoliday, person);
 	}
 
 }
