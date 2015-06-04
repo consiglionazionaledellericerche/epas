d62d68da6be8bbd9d0146e932ed57ad9658e77f8
alessandro martelli <alessandro.martelli@iit.cnr.it>
2015-06-04 14:54:21 +0200

    impostato valore del passato per le presenze festive ad accepted.

--- db/evolutions/51.sql
+++ db/evolutions/51.sql
@@ -8,6 +8,9 @@ ALTER TABLE person_days_history
 UPDATE person_days SET is_holiday = false;
 UPDATE person_days_history SET is_holiday = false;
 
+UPDATE person_days SET accepted_holiday_working_time = true;
+UPDATE person_days_history SET accepted_holiday_working_time = true;
+
 # ---!Downs
 
 ALTER TABLE person_days DROP COLUMN is_holiday;
