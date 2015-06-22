9eacff3a6d1fa7eb0c7b1a79378d077eca0b2108
Daniele Murgia <daniele.murgia@iit.cnr.it>
2015-06-22 18:10:06 +0200

    Commit incompleto Modificati alcuni metodi per l'import da perseo

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,48 +1,38 @@
 package controllers;
 
-import javax.inject.Inject;
-
 import org.joda.time.LocalDate;
 
 import play.Logger;
 import play.mvc.Controller;
 import play.mvc.With;
-import dao.OfficeDao;
-import dao.wrapper.IWrapperFactory;
 
 @With( {Resecure.class, RequestInit.class} )
 public class Application extends Controller {
-    
-	@Inject
-	static OfficeDao officeDao;
-	@Inject
-	static IWrapperFactory wrapperFactory;
-	
-    public static void indexAdmin() {
+
+	public static void indexAdmin() {
 		Logger.debug("chiamato metodo indexAdmin dell'Application controller");
 		render();
-       
-    }
-    
-    public static void index() {
-    	
-//    	Utenti di sistema (developer,admin)
-    	if( Security.getUser().get().person == null	){
-    		
-    		Persons.list(null);
-    		return;
-    	}
-    	
-    	//inizializzazione functional menu dopo login
-    	session.put("monthSelected", new LocalDate().getMonthOfYear());
-    	session.put("yearSelected", new LocalDate().getYear());
-    	session.put("personSelected", Security.getUser().get().person.id);
-    	
-    	session.put("methodSelected", "stampingsAdmin");
+	}
+
+	public static void index() {
+
+		//    	Utenti di sistema (developer,admin)
+		if( Security.getUser().get().person == null	){
+
+			Persons.list(null);
+			return;
+		}
+
+		//inizializzazione functional menu dopo login
+		session.put("monthSelected", new LocalDate().getMonthOfYear());
+		session.put("yearSelected", new LocalDate().getYear());
+		session.put("personSelected", Security.getUser().get().person.id);
+
+		session.put("methodSelected", "stampingsAdmin");
 		session.put("actionSelected", "Stampings.stampings");
-		
-    	Stampings.stampings(new LocalDate().getYear(), new LocalDate().getMonthOfYear());
-    }
-    
+
+		Stampings.stampings(new LocalDate().getYear(), new LocalDate().getMonthOfYear());
+	}
+
 }
 
