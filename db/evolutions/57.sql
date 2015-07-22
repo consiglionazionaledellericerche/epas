a46f97fbc47a26b880f6cc1b626339af66dfd1c6
Daniele Murgia <daniele.murgia@iit.cnr.it>
2015-07-22 10:40:59 +0200

    Riscritta evolutione 57 per renderla piu' generica

--- db/evolutions/57.sql
+++ db/evolutions/57.sql
@@ -1,8 +1,21 @@
 # ---!Ups
 
-update persons 
-SET badgenumber = default 
-where badgenumber = '0000000' or badgenumber = '';
+ -- imposta a null tutti i badgenumber che hanno piu' di un'occorrenza uguale 
+UPDATE persons
+SET badgenumber= default
+WHERE id IN
+(
+    SELECT id
+    FROM persons S
+    INNER JOIN
+    (
+        SELECT badgenumber
+        FROM persons
+        GROUP BY badgenumber
+        HAVING COUNT(*) > 1 
+    ) T
+    ON S.badgenumber=T.badgenumber
+ );
 
 ALTER TABLE persons ADD CONSTRAINT badge_office_key UNIQUE (badgenumber, office_id);
 
