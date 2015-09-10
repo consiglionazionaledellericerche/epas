821d24d620ceab5c6fd1e8eb31b83606b08e0088
Alessandro Martelli <alessandro.martelli@iit.cnr.it>
2015-09-10 12:23:26 +0200

    Aggiunti commenti e corretta evoluzione 63 (viene inizializzata correttamente la data di inizializzazione residuo buoni pasto)

--- db/evolutions/63.sql
+++ db/evolutions/63.sql
@@ -3,6 +3,8 @@
 ALTER TABLE contracts RENAME COLUMN source_date TO source_date_residual;
 ALTER TABLE contracts ADD COLUMN source_date_meal_ticket date;
 
+UPDATE contracts SET source_date_meal_ticket = source_date_residual WHERE source_date_residual IS NOT null;
+
 # ---!Downs
 
 ALTER TABLE contracts RENAME COLUMN source_date_residual TO source_date;
