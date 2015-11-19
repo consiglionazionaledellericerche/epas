6adb363dd9aa4f9fefa44a5e426943721916dd0d
Daniele Murgia <daniele.murgia@iit.cnr.it>
2015-11-19 18:08:56 +0100

    implementata piccola libreria per utilizzare le list-group bootstrap come select multipla

--- app/cnr/sync/manager/RestOfficeManager.java
+++ app/cnr/sync/manager/RestOfficeManager.java
@@ -1,109 +1,108 @@
 package cnr.sync.manager;
 
-import cnr.sync.dto.InstituteDTO;
-import cnr.sync.dto.OfficeDTO;
 import com.google.common.base.Optional;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.FluentIterable;
 import com.google.common.collect.Sets;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Collection;
+import java.util.Set;
+
+import javax.inject.Inject;
+
+import cnr.sync.dto.InstituteDTO;
+import cnr.sync.dto.OfficeDTO;
 import dao.OfficeDao;
 import manager.OfficeManager;
 import models.Institute;
 import models.Office;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import play.data.validation.Validation;
-
-import javax.inject.Inject;
-import java.util.Collection;
-import java.util.Set;
 
 public class RestOfficeManager {
 
-	@Inject
-	private OfficeDao officeDao;
-	@Inject
-	private OfficeManager officeManager;
+  @Inject
+  private OfficeDao officeDao;
+  @Inject
+  private OfficeManager officeManager;
 
-	private final static Logger log = LoggerFactory.getLogger(RestOfficeManager.class);
+  private final static Logger log = LoggerFactory.getLogger(RestOfficeManager.class);
 
-	public int saveImportedSeats(Collection<OfficeDTO> officeDTOList){
+  public int saveImportedSeats(Collection<OfficeDTO> officeDTOList) {
 
-		Preconditions.checkNotNull(officeDTOList);
+    Preconditions.checkNotNull(officeDTOList);
 
-		Set<InstituteDTO> institutesDTO = Sets.newHashSet();
+    Set<InstituteDTO> institutesDTO = Sets.newHashSet();
 
-//		Estrazione senza doppioni di tutti gli istituti dalla lista degli uffici
-		for(OfficeDTO office : officeDTOList){
-			institutesDTO.add(office.institute);
-		}
+    //  Estrazione senza doppioni di tutti gli istituti dalla lista degli uffici
+    for (OfficeDTO office : officeDTOList) {
+      institutesDTO.add(office.institute);
+    }
 
-//      Conversione dei DTO in istituti
-		Set<Institute> institutes = FluentIterable.from(institutesDTO)
-				.transform(InstituteDTO.toInstitute.ISTANCE).toSet();
+    //  Conversione dei DTO in istituti
+    Set<Institute> institutes = FluentIterable.from(institutesDTO)
+            .transform(InstituteDTO.toInstitute.ISTANCE).toSet();
 
-		for(Institute institute : institutes){
-			institute.validateAndCreate();
-			if(!Validation.hasErrors()){
-//				Caso di un nuovo istituto non ancora presente nel db
-				log.info("Importato nuovo Istituto {}", institute.name);
-			}
-			else{
-				Optional<Institute> existentInstitute = officeDao.byCds(institute.cds);
-				if(existentInstitute.isPresent()){
-//					Aggiornamento dati dell'istituto se già presente
-					existentInstitute.get().name = institute.name;
-					existentInstitute.get().code = institute.code;
-					existentInstitute.get().save();
-					log.info("Sincronizzato istituto esistente durante l'import - {}", institute.name);
-				}
-				else{
-					log.warn("Trovato istituto duplicato durante l'import! "
-							+ "valorizzare correttamente il campo cds per "
-							+ "effettuarne la sincronizzazione: {}-{}", institute.name,institute.cds);
-				}
-			}
-		}
+    for (Institute institute : institutes) {
+      institute.validateAndCreate();
+      if (!Validation.hasErrors()) {
+        //  Caso di un nuovo istituto non ancora presente nel db
+        log.info("Importato nuovo Istituto {}", institute.name);
+      } else {
+        Optional<Institute> existentInstitute = officeDao.byCds(institute.cds);
+        if (existentInstitute.isPresent()) {
+          //  Aggiornamento dati dell'istituto se già presente
+          existentInstitute.get().name = institute.name;
+          existentInstitute.get().code = institute.code;
+          existentInstitute.get().save();
+          log.info("Sincronizzato istituto esistente durante l'import - {}", institute.name);
+        } else {
+          log.warn("Trovato istituto duplicato durante l'import! "
+                  + "valorizzare correttamente il campo cds per "
+                  + "effettuarne la sincronizzazione: {}-{}", institute.name, institute.cds);
+        }
+      }
+    }
 
-		int syncedOffices = 0;
+    int syncedOffices = 0;
 
-		for(OfficeDTO officeDTO : officeDTOList){
+    for (OfficeDTO officeDTO : officeDTOList) {
 
-			Office office = new Office();
-			officeDTO.copyInto(office);
+      Office office = new Office();
+      officeDTO.copyInto(office);
 
-			Optional<Institute> institute=officeDao.byCds(officeDTO.institute.cds);
+      Optional<Institute> institute = officeDao.byCds(officeDTO.institute.cds);
 
-			if(!institute.isPresent()){
-				log.warn("Impossibile trovare l'istituto associato alla sede {} importata da Perseo." +
-						" Sede non inserita.",office);
-				continue;
-			}
+      if (!institute.isPresent()) {
+        log.warn("Impossibile trovare l'istituto associato alla sede {} importata da Perseo."
+                + " Sede non inserita.", office);
+        continue;
+      }
 
-			office.institute = institute.get();
+      office.institute = institute.get();
 
-			office.validateAndSave();
-			if(!Validation.hasErrors()){
-				syncedOffices++;
-				officeManager.generateConfAndPermission(office);
-				log.info("Importata Sede {}", office.name);
-			}
-			else{
-				Optional<Office> existentSeat = officeDao.byCode(office.code);
-				if(existentSeat.isPresent()){
+      office.validateAndSave();
+      if (!Validation.hasErrors()) {
+        syncedOffices++;
+        officeManager.generateConfAndPermission(office);
+        log.info("Importata Sede {}", office.name);
+      } else {
+        Optional<Office> existentSeat = officeDao.byCode(office.code);
+        if (existentSeat.isPresent()) {
 
-                    officeDTO.copyInto(existentSeat.get());
-					existentSeat.get().save();
-					syncedOffices++;
-					log.info("Sincronizzata sede esistente durante l'import - {}", office.name);
-				}
-				else{
-					log.warn("Trovata Sede duplicata durante l'import! "
-							+ "valorizzare correttamente il campo Codice Sede per "
-							+ "effettuarne la sincronizzazione: {}-{}", office.name,office.codeId);
-				}
-			}
-		}
-		return syncedOffices;
-	}
+          officeDTO.copyInto(existentSeat.get());
+          existentSeat.get().save();
+          syncedOffices++;
+          log.info("Sincronizzata sede esistente durante l'import - {}", office.name);
+        } else {
+          log.warn("Trovata Sede duplicata durante l'import! "
+                  + "valorizzare correttamente il campo Codice Sede per "
+                  + "effettuarne la sincronizzazione: {}-{}", office.name, office.codeId);
+        }
+      }
+    }
+    return syncedOffices;
+  }
 }
--- app/controllers/Import.java
+++ app/controllers/Import.java
@@ -1,13 +1,22 @@
 package controllers;
 
+import com.google.common.base.Predicate;
+import com.google.common.base.Strings;
+import com.google.common.collect.Collections2;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.ExecutionException;
+
+import javax.inject.Inject;
+
 import cnr.sync.consumers.OfficeConsumer;
 import cnr.sync.consumers.PeopleConsumer;
 import cnr.sync.dto.OfficeDTO;
 import cnr.sync.manager.RestOfficeManager;
-import com.google.common.base.Predicate;
-import com.google.common.collect.Collections2;
-import com.google.common.collect.Lists;
-import com.google.common.collect.Sets;
 import dao.OfficeDao;
 import lombok.extern.slf4j.Slf4j;
 import models.Office;
@@ -18,86 +27,88 @@ import play.data.validation.Validation;
 import play.mvc.Controller;
 import play.mvc.With;
 
-import javax.inject.Inject;
-import java.util.Collection;
-import java.util.List;
-import java.util.Set;
-import java.util.concurrent.ExecutionException;
-
 @Slf4j
 @With({Resecure.class, RequestInit.class})
 public class Import extends Controller {
 
-    private final static String IMPORTED_OFFICES = "importedOffices";
-    @Inject
-    private static OfficeDao officeDao;
-    @Inject
-    private static OfficeConsumer officeConsumer;
-    @Inject
-    private static PeopleConsumer peopleConsumer;
-    @Inject
-    private static RestOfficeManager restOfficeManager;
+  private final static String IMPORTED_OFFICES = "importedOffices";
+  @Inject
+  private static OfficeDao officeDao;
+  @Inject
+  private static OfficeConsumer officeConsumer;
+  @Inject
+  private static PeopleConsumer peopleConsumer;
+  @Inject
+  private static RestOfficeManager restOfficeManager;
 
-    public static void officeList() {
+  public static void officeList() {
 
-        List<OfficeDTO> importedOffices = Lists.newArrayList();
+    List<OfficeDTO> importedOffices = Lists.newArrayList();
 
-        try {
-            importedOffices = officeConsumer.getOffices().get();
-        } catch (IllegalStateException | InterruptedException
-                | ExecutionException e) {
-            flash.error("Impossibile recuperare la lista degli istituti.");
-            e.printStackTrace();
-        }
-
-        Cache.add(IMPORTED_OFFICES, importedOffices);
-
-        render(importedOffices);
+    try {
+      importedOffices = officeConsumer.getOffices().get();
+    } catch (IllegalStateException | InterruptedException
+            | ExecutionException e) {
+      flash.error("Impossibile recuperare la lista degli istituti.");
+      e.printStackTrace();
     }
 
-    public static void importOffices(@Required final List<Integer> offices) {
+    Cache.add(IMPORTED_OFFICES, importedOffices);
 
-        if (Validation.hasErrors()) {
-            flash.error("Selezionare almeno una Sede da importare");
-            officeList();
-        }
+    List<Office> allOffices = Office.findAll();
+    List<String> officeCodes = Lists.newArrayList();
 
-        List<OfficeDTO> importedOffices = Cache.get(IMPORTED_OFFICES, List.class);
+    for (Office o : allOffices) {
+      if (!Strings.isNullOrEmpty(o.code)) {
+        officeCodes.add(o.code);
+      }
+    }
+    render(importedOffices, officeCodes);
+  }
 
-        if (importedOffices == null) {
-            try {
-                importedOffices = officeConsumer.getOffices().get();
-            } catch (IllegalStateException | InterruptedException | ExecutionException e) {
-                log.warn("Impossibile importare la lista delle sedi dall'anagrafica - {}", e.getStackTrace());
-            }
-        }
+  public static void importOffices(@Required final List<Integer> offices) {
 
-//		Filtro la lista di tutti gli uffici presenti su perseo, lasciando solo i selezionati nella form
-        Collection<OfficeDTO> filteredOffices = Collections2.filter(importedOffices,
-                new Predicate<OfficeDTO>() {
-                    @Override
-                    public boolean apply(OfficeDTO input) {
-                        return offices.contains(input.id);
-                    }
-                });
-
-        int synced = restOfficeManager.saveImportedSeats(filteredOffices);
-        if (synced == 0) {
-            flash.error("Non è stato possibile importare/sincronizzare alcuna sede, controllare i log");
-        } else {
-            flash.success("Importate/sincronizzate correttamente %s Sedi", synced);
-        }
-        Institutes.index();
+    if (Validation.hasErrors()) {
+      flash.error("Selezionare almeno una Sede da importare");
+      officeList();
     }
 
-    public static void syncSeatPeople(@Required Long id) {
-        if (Validation.hasErrors()) {
-            flash.error("Impossibile effettuare la sincronizzazione della sede");
-            Institutes.index();
-        }
-        Office seat = officeDao.getOfficeById(id);
+    List<OfficeDTO> importedOffices = Cache.get(IMPORTED_OFFICES, List.class);
 
-        Set<Person> importedPeople = Sets.newHashSet();
+    if (importedOffices == null) {
+      try {
+        importedOffices = officeConsumer.getOffices().get();
+      } catch (IllegalStateException | InterruptedException | ExecutionException e) {
+        log.warn("Impossibile importare la lista delle sedi dall'anagrafica - {}", e.getStackTrace());
+      }
+    }
+
+    //  Filtro la lista di tutti gli uffici presenti su perseo, lasciando solo i selezionati nella form
+    Collection<OfficeDTO> filteredOffices = Collections2.filter(importedOffices,
+            new Predicate<OfficeDTO>() {
+              @Override
+              public boolean apply(OfficeDTO input) {
+                return offices.contains(input.id);
+              }
+            });
+
+    int synced = restOfficeManager.saveImportedSeats(filteredOffices);
+    if (synced == 0) {
+      flash.error("Non è stato possibile importare/sincronizzare alcuna sede, controllare i log");
+    } else {
+      flash.success("Importate/sincronizzate correttamente %s Sedi", synced);
+    }
+    Institutes.index();
+  }
+
+  public static void syncSeatPeople(@Required Long id) {
+    if (Validation.hasErrors()) {
+      flash.error("Impossibile effettuare la sincronizzazione della sede");
+      Institutes.index();
+    }
+    Office seat = officeDao.getOfficeById(id);
+
+    Set<Person> importedPeople = Sets.newHashSet();
 
 //		try {
 //			importedPeople = peopleConsumer.seatPeople(seat.code.toString()).get();
@@ -111,5 +122,5 @@ public class Import extends Controller {
 //			Logger.info("Persone Importata: %s-%s-%s-%s-%s-%s",p.fullName(),p.birthday,p.email,p.cnr_email,p.number,p.badgeNumber);
 //		}
 
-    }
+  }
 }
