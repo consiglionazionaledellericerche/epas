d6ceb89ed15d29989a6f2b6a49d206d7a3972f03
Alessandro Martelli <alessandro.martelli@iit.cnr.it>
2015-11-26 18:07:34 +0100

    Generalizzato il riepilogo dei ricalcoli e reso riusabile.

--- app/manager/recaps/recomputation/RecomputeRecap.java
+++ app/manager/recaps/recomputation/RecomputeRecap.java
@@ -1,14 +1,24 @@
 package manager.recaps.recomputation;
 
+import com.google.common.base.Optional;
+
+import models.base.PeriodModel;
+
 import org.joda.time.LocalDate;
+
+import java.util.List;
 
 public class RecomputeRecap {
 
+  public List<PeriodModel> periods;
+  
   public LocalDate recomputeFrom;
-  public LocalDate recomputeTo;
+  public Optional<LocalDate> recomputeTo;
   public boolean onlyRecaps;
 
   public boolean initMissing;
 
   public boolean needRecomputation;
+  
+  public int days;
 }
