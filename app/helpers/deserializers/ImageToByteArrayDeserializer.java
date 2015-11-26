03616403fe1acea3748f27fb0de4bd1109b574e3
Marco Andreini <marco.andreini@iit.cnr.it>
2015-11-26 17:13:25 +0100

    Rivisto il meccanismo di raccolta delle segnalazioni via json.
    Incluso il trattamento in memory delle PNG inviate.
    Aggiunto un ReportMailer per l'invio di email tramite template.

---
+++ app/helpers/deserializers/ImageToByteArrayDeserializer.java
@@ -0,0 +1,36 @@
+package helpers.deserializers;
+
+import java.lang.reflect.Type;
+
+import javax.xml.bind.DatatypeConverter;
+
+import com.google.common.base.Verify;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
+
+/**
+ * Converte una immagine codificata in image/png dal browser in una array di
+ * byte.
+ *
+ * @author marco
+ *
+ */
+public class ImageToByteArrayDeserializer implements JsonDeserializer<byte[]> {
+
+  private final static String IMAGE_MAGIK = "data:image/png;base64,";
+
+  /**
+   * @see com.google.gson.JsonDeserializer#deserialize(com.google.gson.JsonElement, java.lang.reflect.Type, com.google.gson.JsonDeserializationContext)
+   */
+  @Override
+  public byte[] deserialize(JsonElement json, Type typeOfT,
+      JsonDeserializationContext context) throws JsonParseException {
+    final String value = json.getAsString();
+    Verify.verify(value.startsWith(IMAGE_MAGIK));
+    return DatatypeConverter.parseBase64Binary(value
+        .substring(IMAGE_MAGIK.length()));
+  }
+}
+
