511113a85c86145d31f3e97a8fc3844935417888
Alessandro Martelli <alessandro.martelli@iit.cnr.it>
2015-12-09 12:06:52 +0100

    Completata logica di aggiunta e rimozione dei badgeSystem ai badgeReader, con reportistica e comandi di conferma.

--- app/controllers/BadgeReaders.java
+++ app/controllers/BadgeReaders.java
@@ -1,6 +1,9 @@
 package controllers;
 
 import com.google.common.base.Optional;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
 
 import com.mysema.query.SearchResults;
 
@@ -12,8 +15,10 @@ import helpers.Web;
 
 import manager.SecureManager;
 
+import models.Badge;
 import models.BadgeReader;
 import models.BadgeSystem;
+import models.Person;
 import models.User;
 
 import org.slf4j.Logger;
@@ -28,6 +33,10 @@ import play.mvc.Controller;
 import play.mvc.With;
 
 import security.SecurityRules;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
 
 import javax.inject.Inject;
 
@@ -169,12 +178,12 @@ public class BadgeReaders extends Controller {
       render("@blank", badgeReader, user);
     }
 
-
     Codec codec = new Codec();
     user.password = codec.hexMD5(user.password);
     user.save();
     badgeReader.user = user;
     badgeReader.save();
+
     flash.success(Web.msgSaved(BadgeReader.class));
     index();
   }
@@ -199,6 +208,10 @@ public class BadgeReaders extends Controller {
     index();
   }
 
+  /**
+   * Gestore associazioni con i BadgeSystem.
+   * @param badgeReaderId
+   */
   public static void joinBadgeSystems(Long badgeReaderId) {
     
     final BadgeReader badgeReader = badgeReaderDao.byId(badgeReaderId);
@@ -209,16 +222,100 @@ public class BadgeReaders extends Controller {
     render("@joinBadgeSystems", badgeReader);
   }
   
-  public static void saveBadgeSystems(@Valid BadgeReader badgeReader) {
+  /**
+   * Salva la nuova associazione.
+   * @param badgeReader.
+   */
+  public static void saveBadgeSystems(@Valid BadgeReader badgeReader, boolean confirmed) {
     
     rules.checkIfPermitted(badgeReader.owner);
     
-    flash.success(Web.msgSaved(BadgeReader.class));
-    badgeReader.save();
+    // TODO:
+    //creare gli uro mancanti, cancellare quelli non più usati
     
-    edit(badgeReader.id);
+    
+    List<Badge> badgesDefinitelyToRemove = Lists.newArrayList();
+    List<Badge> badgesToRemove = Lists.newArrayList();
+    Set<BadgeSystem> badgeSystemsToRemove = Sets.newHashSet();
+    //rimuovere i badge non più usati
+    for (Badge badge : badgeReader.badges) {
+      //rimozione
+      if (!badgeReader.badgeSystems.contains(badge.badgeSystem)) {
+        badgesToRemove.add(badge);
+        if (badge.badgeSystem.badgeReaders.size() == 1) {
+          badgesDefinitelyToRemove.add(badge);
+        }
+        if (!badgeSystemsToRemove.contains(badge.badgeSystem)) {
+          badgeSystemsToRemove.add(badge.badgeSystem);
+        }
+        continue;
+      }
+    }
+
+    List<Badge> violatedBadges = Lists.newArrayList();
+    List<Badge> badgesToSave = Lists.newArrayList();
+
+    for (BadgeSystem badgeSystem : badgeReader.badgeSystems) {
+      // Per ogni badge System ora associato
+      if (badgeSystemsToRemove.contains(badgeSystem)) {
+        continue;
+      }
+      // Prendere i codici del badge system
+      Set<String> codes = Sets.newHashSet();
+      for (Badge oldBadge : badgeSystem.badges) {
+        if (!codes.contains(oldBadge.code)) {
+         codes.add(oldBadge.code);
+
+          Badge badge = new Badge();
+          badge.person = oldBadge.person;
+          badge.badgeSystem = badgeSystem;
+          badge.badgeReader = badgeReader;
+          badge.code = oldBadge.code;
+         
+          //Controllare che esistano nel badgeReader
+          Optional<Badge> alreadyExists = BadgeSystems.alreadyExists(badge);
+          if (alreadyExists.isPresent()) {
+            if (!badgeSystemsToRemove.contains(alreadyExists.get().badgeSystem) 
+                && !alreadyExists.get().person.equals(badge.person)) {
+              violatedBadges.add(badge);
+              violatedBadges.add(alreadyExists.get());
+            }
+          } else {
+            badgesToSave.add(badge);
+          }
+        }
+      }
+    }
+
+    if (!violatedBadges.isEmpty()) {
+      response.status = 400;
+      render("@joinBadgeSystems", badgeReader, violatedBadges);
+      index();
+    }
+    
+    if (!badgesDefinitelyToRemove.isEmpty() && !confirmed) {
+      response.status = 400;
+      render("@joinBadgeSystems", badgeReader, badgesDefinitelyToRemove);
+    }
+    
+    badgeReader.save();
+    for (Badge badge : badgesToRemove) {
+      badge.delete();
+    }
+    for (Badge badge : badgesToSave) {
+      badge.save();
+    }
+
+    flash.success(Web.msgSaved(BadgeReader.class));
+    index();
     
   }
+
+  private static String key(Badge badge) {
+    String key = badge.badgeReader.id+"-"+badge.code; 
+    return key;
+  }
+
   
  
 }
