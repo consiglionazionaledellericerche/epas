7b6b8717a045d8df818bf9075affef0ea1a63687
Cristian Lucchesi <cristian.lucchesi@iit.cnr.it>
2015-12-18 15:56:53 +0100

    Piccole correzioni di stile.

--- app/controllers/AbsenceFromJson.java
+++ app/controllers/AbsenceFromJson.java
@@ -4,6 +4,8 @@ import dao.AbsenceDao;
 
 import it.cnr.iit.epas.JsonPersonEmailBinder;
 
+import lombok.extern.slf4j.Slf4j;
+
 import manager.AbsenceFromJsonManager;
 
 import models.exports.FrequentAbsenceCode;
@@ -12,7 +14,6 @@ import models.exports.PersonPeriodAbsenceCode;
 
 import org.joda.time.LocalDate;
 
-import play.Logger;
 import play.data.binding.As;
 import play.mvc.Controller;
 
@@ -28,6 +29,7 @@ import javax.inject.Inject;
  *         "cristian.lucchesi@iit.cnr.it"},{"email" : "stefano.ruberti@iit.cnr.it"}]}'
  *         http://localhost:8888/absenceFromJson/absenceInPeriod
  */
+@Slf4j
 public class AbsenceFromJson extends Controller {
 
   @Inject
@@ -39,24 +41,31 @@ public class AbsenceFromJson extends Controller {
       Integer yearFrom, Integer monthFrom, Integer dayFrom, Integer yearTo, Integer monthTo,
       Integer dayTo, @As(binder = JsonPersonEmailBinder.class) PersonEmailFromJson body) {
 
-    Logger.debug("Received personEmailFromJson %s", body);
+    log.debug("Received personEmailFromJson {}", body);
     if (body == null) {
       badRequest();
     }
-    Logger.debug("Entrato nel metodo getAbsenceInPeriod...");
+    log.debug("Entrato nel metodo getAbsenceInPeriod...");
     List<PersonPeriodAbsenceCode> personsToRender = new ArrayList<PersonPeriodAbsenceCode>();
 
     LocalDate dateFrom = null;
     LocalDate dateTo = null;
-    if (yearFrom != null && monthFrom != null && dayFrom != null)
+    if (yearFrom != null && monthFrom != null && dayFrom != null) {
       dateFrom = new LocalDate(yearFrom, monthFrom, dayFrom);
-    else
-      dateFrom = new LocalDate(params.get("yearFrom", Integer.class), params.get("monthFrom", Integer.class), params.get("dayFrom", Integer.class));
-
-    if (yearTo != null && monthTo != null && dayTo != null)
+    } else {
+      dateFrom = new LocalDate(
+          params.get("yearFrom", Integer.class),
+          params.get("monthFrom", Integer.class),
+          params.get("dayFrom", Integer.class));
+    }
+    if (yearTo != null && monthTo != null && dayTo != null) {
       dateTo = new LocalDate(yearTo, monthTo, dayTo);
-    else
-      dateTo = new LocalDate(params.get("yearTo", Integer.class), params.get("monthTo", Integer.class), params.get("dayTo", Integer.class));
+    } else {
+      dateTo = new LocalDate(
+          params.get("yearTo", Integer.class),
+          params.get("monthTo", Integer.class),
+          params.get("dayTo", Integer.class));
+    }
     personsToRender = absenceFromJsonManager.getPersonForAbsenceFromJson(body, dateFrom, dateTo);
 
     renderJSON(personsToRender);
@@ -65,12 +74,19 @@ public class AbsenceFromJson extends Controller {
   /**
    * Metodo esposto per ritornare la lista dei codici di assenza presi.
    */
-  public static void frequentAbsence(Integer yearFrom, Integer monthFrom, Integer dayFrom, Integer yearTo, Integer monthTo, Integer dayTo) {
+  public static void frequentAbsence(Integer yearFrom, Integer monthFrom, Integer dayFrom,
+      Integer yearTo, Integer monthTo, Integer dayTo) {
 
     List<FrequentAbsenceCode> frequentAbsenceCodeList = new ArrayList<FrequentAbsenceCode>();
 
-    LocalDate dateFrom = new LocalDate(params.get("yearFrom", Integer.class), params.get("monthFrom", Integer.class), params.get("dayFrom", Integer.class));
-    LocalDate dateTo = new LocalDate(params.get("yearTo", Integer.class), params.get("monthTo", Integer.class), params.get("dayTo", Integer.class));
+    LocalDate dateFrom = new LocalDate(
+        params.get("yearFrom", Integer.class),
+        params.get("monthFrom", Integer.class),
+        params.get("dayFrom", Integer.class));
+    LocalDate dateTo = new LocalDate(
+        params.get("yearTo", Integer.class),
+        params.get("monthTo", Integer.class),
+        params.get("dayTo", Integer.class));
 
     frequentAbsenceCodeList = absenceDao.getFrequentAbsenceCodeForAbsenceFromJson(dateFrom, dateTo);
 
