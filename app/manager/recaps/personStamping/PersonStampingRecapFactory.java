144d1a66cab7787172a00904a90a07a54af3aaeb
Daniele Murgia <daniele.murgia@iit.cnr.it>
2016-02-15 13:08:01 +0100

    Rivista e migliorata la stampa cartellino delle presente mensili

--- app/manager/recaps/personStamping/PersonStampingRecapFactory.java
+++ app/manager/recaps/personStamping/PersonStampingRecapFactory.java
@@ -1,12 +1,8 @@
 package manager.recaps.personStamping;
 
-import dao.MealTicketDao;
 import dao.PersonDayDao;
 import dao.wrapper.IWrapperFactory;
 
-import it.cnr.iit.epas.DateUtility;
-
-import manager.ContractMonthRecapManager;
 import manager.PersonDayManager;
 import manager.PersonManager;
 
@@ -18,31 +14,22 @@ public class PersonStampingRecapFactory {
 
   private final PersonDayManager personDayManager;
   private final PersonDayDao personDayDao;
-  private final MealTicketDao mealTicketDao;
-  private final ContractMonthRecapManager contractMonthRecapManager;
   private final PersonManager personManager;
   private final PersonStampingDayRecapFactory stampingDayRecapFactory;
   private final IWrapperFactory wrapperFactory;
-  private final DateUtility dateUtility;
 
   @Inject
   PersonStampingRecapFactory(PersonDayManager personDayManager,
                              PersonDayDao personDayDao,
                              PersonManager personManager,
-                             MealTicketDao mealTicketDao,
-                             ContractMonthRecapManager contractMonthRecapManager,
                              IWrapperFactory wrapperFactory,
-                             PersonStampingDayRecapFactory stampingDayRecapFactory,
-                             DateUtility dateUtility) {
+                             PersonStampingDayRecapFactory stampingDayRecapFactory) {
 
     this.personDayManager = personDayManager;
     this.personDayDao = personDayDao;
-    this.mealTicketDao = mealTicketDao;
-    this.contractMonthRecapManager = contractMonthRecapManager;
     this.personManager = personManager;
     this.stampingDayRecapFactory = stampingDayRecapFactory;
     this.wrapperFactory = wrapperFactory;
-    this.dateUtility = dateUtility;
   }
 
   /**
@@ -50,9 +37,9 @@ public class PersonStampingRecapFactory {
    */
   public PersonStampingRecap create(Person person, int year, int month) {
 
-    return new PersonStampingRecap(personDayManager, personDayDao, mealTicketDao,
-            personManager, contractMonthRecapManager, stampingDayRecapFactory,
-            wrapperFactory, dateUtility, year, month, person);
+    return new PersonStampingRecap(personDayManager, personDayDao,
+        personManager, stampingDayRecapFactory,
+        wrapperFactory, year, month, person);
   }
 
 }
