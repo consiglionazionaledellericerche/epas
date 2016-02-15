632f8e29346eafdd5490f73f0fa7fb1b41893fce
Alessandro Martelli <alessandro.martelli@iit.cnr.it>
2016-02-15 15:50:01 +0100

    Completate crud nuova gestione dei parametri di configurazione.

--- app/manager/recaps/recomputation/RecomputeRecap.java
+++ app/manager/recaps/recomputation/RecomputeRecap.java
@@ -3,6 +3,7 @@ package manager.recaps.recomputation;
 import com.google.common.base.Optional;
 import com.google.common.collect.Lists;
 
+import models.Configuration;
 import models.base.IPropertyInPeriod;
 
 import org.joda.time.LocalDate;
@@ -17,8 +18,13 @@ public class RecomputeRecap {
   public Optional<LocalDate> recomputeTo;
   public boolean onlyRecaps;
 
+  //Dato da utilizzare in caso di modifica contratto.
   public boolean initMissing;
-
+  
+  //Dato da utilizzare in caso di modifica configurazione.
+  public Configuration configuration;
+  
+  
   public boolean needRecomputation;
 
   public int days;
