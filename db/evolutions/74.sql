7168faa7170268c7b2bd76067f3f2180409aa223
dario <dario.tagliaferri@iit.cnr.it>
2016-02-18 10:45:12 +0100

    aggiunta evoluzione sulla tabella degli utenti

---
+++ db/evolutions/74.sql
@@ -0,0 +1,16 @@
+# ---!Ups
+
+ALTER TABLE users ADD COLUMN disabled boolean;
+ALTER TABLE users ADD COLUMN expire_date date;
+ALTER TABLE users_history ADD COLUMN disabled boolean;
+ALTER TABLE users_history ADD COLUMN expire_date date;
+UPDATE users set disabled = false;
+update users set expire_date = null;
+
+
+# ---!Downs
+
+ALTER TABLE users_history DROP COLUMN disabled;
+ALTER TABLE users_history DROP COLUMN expire_date;
+ALTER TABLE users DROP COLUMN disabled;
+ALTER TABLE users DROP COLUMN expire_date;
