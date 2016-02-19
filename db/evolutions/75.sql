6af8f065a7ee8f9c8656ccb1bb0b7cf177961c0e
Daniele Murgia <daniele.murgia@iit.cnr.it>
2016-02-19 18:08:16 +0100

    Aggiunta rimozione constraint sbagliato su conf_year_history

--- db/evolutions/75.sql
+++ db/evolutions/75.sql
@@ -5,6 +5,8 @@
 ALTER TABLE meal_ticket ADD COLUMN returned BOOLEAN NOT NULL DEFAULT false;
 ALTER TABLE meal_ticket_history ADD COLUMN returned BOOLEAN DEFAULT false;
 
+ALTER TABLE conf_year_history ALTER COLUMN office_id DROP not null ;
+
 # ---!Downs
 
 ALTER TABLE meal_ticket DROP COLUMN returned;
