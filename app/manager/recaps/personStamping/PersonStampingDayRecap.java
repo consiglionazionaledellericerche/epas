5a020c7a30787b43d68477065d19d96b2d95c1b6
Alessandro Martelli <alessandro.martelli@iit.cnr.it>
2016-02-25 12:23:49 +0100

    Sistemato caso renderizzazione buono pasto ottenuto.

--- app/manager/recaps/personStamping/PersonStampingDayRecap.java
+++ app/manager/recaps/personStamping/PersonStampingDayRecap.java
@@ -200,7 +200,10 @@ public class PersonStampingDayRecap {
       return;
     }
     if (!personDay.isTicketAvailable) {
-      if (personDay.isToday() || !thereAreAllDayAbsences) {
+      if (personDay.isPast()) {
+        this.mealTicket = MEALTICKET_NO;
+      }
+      else if (personDay.isToday() || !thereAreAllDayAbsences) {
         this.mealTicket = MEALTICKET_NOT_YET;
       } else {
         this.mealTicket = MEALTICKET_NO;
