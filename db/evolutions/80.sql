5b8a1a0fc3ff3418d4aa9df4bc915143aaada9e7
Cristian Lucchesi <cristian.lucchesi@iit.cnr.it>
2016-04-12 09:37:26 +0200

    person_id nella tabella persons_working_time_types erroneamente rimosso.
    See #202.

--- db/evolutions/80.sql
+++ db/evolutions/80.sql
@@ -5,7 +5,10 @@
 ALTER TABLE conf_period ADD PRIMARY KEY(id);
 ALTER TABLE configurations_history ADD PRIMARY KEY (id, _revision);
 ALTER TABLE shift_type_history ADD PRIMARY KEY (id, _revision);
-ALTER TABLE persons_working_time_types DROP COLUMN person_id;
+
+DELETE FROM persons_working_time_types WHERE id IN
+  (SELECT pwtt.id FROM persons_working_time_types pwtt LEFT JOIN persons p on pwtt.person_id = p.id WHERE p.id IS NULL);
+ALTER TABLE persons_working_time_types ADD CONSTRAINT persons_working_time_types_person_id FOREIGN KEY (person_id) REFERENCES persons (id);
 
 ALTER TABLE contract_month_recap DROP COLUMN abs_fap_usate;
 ALTER TABLE contract_month_recap DROP COLUMN abs_fac_usate;
@@ -15,7 +18,8 @@ ALTER TABLE contract_month_recap DROP COLUMN abs_p_usati;
 ALTER TABLE conf_period DROP CONSTRAINT conf_period_pkey;
 ALTER TABLE configurations_history DROP CONSTRAINT configurations_history_pkey;
 ALTER TABLE shift_type_history DROP CONSTRAINT shift_type_history_pkey;
-ALTER TABLE persons_working_time_types ADD COLUMN person_id INT;
+
+ALTER TABLE persons_working_time_types DROP CONSTRAINT persons_working_time_types_person_id;
 
 ALTER TABLE contract_month_recap ADD COLUMN abs_fap_usate INT;
 ALTER TABLE contract_month_recap ADD COLUMN abs_fac_usate INT;
