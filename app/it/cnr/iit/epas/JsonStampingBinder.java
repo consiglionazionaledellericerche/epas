73b476aaaa8f4c659bc75b44befa31c20b77c065
dario <dario.tagliaferri@iit.cnr.it>
2016-05-31 10:52:36 +0200

    Aggiunta la gestione delle causali sulle timbrature che arrivano via json dai client.

--- app/it/cnr/iit/epas/JsonStampingBinder.java
+++ app/it/cnr/iit/epas/JsonStampingBinder.java
@@ -63,8 +63,8 @@ public class JsonStampingBinder implements TypeBinder<StampingFromClient> {
         log.info("StampingFromClient: l'user non presente");
         return null;
       }
-//      Set<Office> offices = secureManager
-//          .officesBadgeReaderAllowed(Security.getUser().get());
+      Set<Office> offices = secureManager
+          .officesBadgeReaderAllowed(Security.getUser().get());
 
       Person person = null;
 
@@ -85,13 +85,17 @@ public class JsonStampingBinder implements TypeBinder<StampingFromClient> {
         final String causale = jsonObject.get("causale").getAsString();
         if (!Strings.isNullOrEmpty(causale)) {
           
-          if (stamping.stampType == null) {
-            throw new IllegalArgumentException(String
-                .format("Causale con codice %s sconosciuta.", causale));
-          }
           if (StampTypes.isActive(causale)) {
             stamping.stampType = StampTypes.byCode(causale);
           }
+          
+          if (stamping.stampType == null) {
+            
+            log.error(String
+                .format("Causale con codice %s sconosciuta.", causale));
+            return null;
+          }
+          
         }
       }
 
