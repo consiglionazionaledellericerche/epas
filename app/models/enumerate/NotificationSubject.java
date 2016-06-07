8bdf3a600e65313d3b6da81083fa2f829959d1d2
Daniele Murgia <daniele.murgia@iit.cnr.it>
2016-06-07 11:26:07 +0200

    importata (da skynet) struttura di base per le notifiche

---
+++ app/models/enumerate/NotificationSubject.java
@@ -0,0 +1,54 @@
+package models.enumerate;
+
+import com.google.common.collect.Maps;
+
+import play.mvc.Router;
+
+import java.util.Map;
+
+/**
+ * Notification subject types.
+ *
+ * @author marco
+ */
+public enum NotificationSubject {
+  /**
+   * Notifiche di sistema.
+   */
+  SYSTEM,
+  /**
+   * Commento.
+   */
+  COMMENT,
+  /**
+   * Messaggio.
+   */
+  MESSAGE;
+
+  private String toUrl(String action, Integer id) {
+    if (id == null) {
+      return Router.reverse(action).url;
+    } else {
+      final Map<String, Object> params = Maps.newHashMap();
+      params.put("id", id);
+      return Router.reverse(action, params).url;
+    }
+  }
+
+  public String toUrl(Integer referenceId) {
+    switch (this) {
+      case COMMENT:
+        return toUrl("Comments.show", referenceId);
+      case MESSAGE:
+        return toUrl("Messages.show", referenceId);
+      // case SYSTEM:
+      default:
+        throw new IllegalStateException("unknown target: " + this.name());
+    }
+  }
+
+
+  public boolean isRedirect() {
+    return this != SYSTEM;
+  }
+}
