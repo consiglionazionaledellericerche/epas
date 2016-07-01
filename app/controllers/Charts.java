40555a9e10f557a30e118e69b9b65b5315cd52b6
dario <dario.tagliaferri@iit.cnr.it>
2016-07-01 17:00:17 +0200

    Riabilitata la funzionalità di check delle assenze anno passato tramite schedone. Rivista e corretta la procedura di check, anche fornendo dei fix per eventuali file non correttamente inviati in formato csv.

--- app/controllers/Charts.java
+++ app/controllers/Charts.java
@@ -5,6 +5,7 @@ import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 
 import dao.CompetenceDao;
+import dao.OfficeDao;
 import dao.PersonDao;
 
 import manager.ChartsManager;
@@ -27,6 +28,7 @@ import play.mvc.Controller;
 import play.mvc.With;
 import security.SecurityRules;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.List;
@@ -51,6 +53,8 @@ public class Charts extends Controller {
   private static PersonDao personDao;
   @Inject
   private static CompetenceDao competenceDao;
+  @Inject
+  private static OfficeDao officeDao;
 
   public static void overtimeOnPositiveResidual(Integer year, Integer month) {
 
@@ -84,7 +88,7 @@ public class Charts extends Controller {
 
 
   public static void indexCharts() {
-    rules.checkIfPermitted(Security.getUser().get().person.office);
+    
     render();
   }
 
@@ -159,27 +163,23 @@ public class Charts extends Controller {
   }
 
 
-  public static void checkLastYearAbsences() {
-    rules.checkIfPermitted(Security.getUser().get().person.office);
-    render();
-  }
-
-
-  /**
-   * restituisce la lista delle assenze che combaciano e no rispetto allo schedone.
-   *
-   * @param file il file dello schedone
-   */
-  public static void processLastYearAbsences(Blob file) {
-
-    rules.checkIfPermitted(Security.getUser().get().person.office);
-
+  public static void checkLastYearAbsences(File file, Long officeId) {
+    
+    Office office = officeDao.getOfficeById(officeId);
+    notFoundIfNull(office);
+    rules.checkIfPermitted(office);
+    
+    boolean process = false;
+    if (file == null) {
+      render(process, office);
+    }        
+    process = true;
     RenderList render = chartsManager.checkSituationPastYear(file);
     List<RenderResult> listTrueFalse = render.getListTrueFalse();
-    List<RenderResult> listNull = render.getListNull();
-
-    render(listTrueFalse, listNull);
+    
+    render(listTrueFalse, process, office);
   }
+
 
   /**
    * esporta le ore e gli straordinari.
