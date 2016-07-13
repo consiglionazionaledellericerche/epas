6d73392da18534d63810cc35d63a2f2a78cbe57f
Alessandro Martelli <alessandro.martelli@iit.cnr.it>
2016-07-13 12:06:29 +0200

    Avanzamento nuova implementazione Assenze.

--- app/models/absences/GroupAbsenceType.java
+++ app/models/absences/GroupAbsenceType.java
@@ -1,8 +1,14 @@
 package models.absences;
 
+import com.google.common.base.Preconditions;
+
+import it.cnr.iit.epas.DateInterval;
+
+import models.absences.GroupAbsenceType.PeriodType;
 import models.base.BaseModel;
 
 import org.hibernate.envers.Audited;
+import org.joda.time.LocalDate;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -44,8 +50,41 @@ public class GroupAbsenceType extends BaseModel {
   @JoinColumn(name = "next_group_to_check_id")
   public GroupAbsenceType nextGropToCheck;
   
+
+  
   public enum PeriodType {
-    always, year, month, child0_3, child0_6, child0_12, child6_12;
+    
+    always(0, null, null), year(0, null, null), month(0, null, null),
+    child1_0_3(1, 0, 3), child1_0_6(1, 0, 6), child1_0_12(1, 0, 12), child1_6_12(1, 6, 12),
+    child2_0_3(2, 0, 3), child2_0_6(2, 0, 6), child2_0_12(2, 0, 12), child2_6_12(2, 6, 12),
+    child3_0_3(3, 0, 3), child3_0_6(3, 0, 6), child3_0_12(3, 0, 12), child3_6_12(3, 6, 12);
+    
+    public Integer childNumber;
+    public Integer fromYear;
+    public Integer toYear;
+    
+    PeriodType(Integer childNumber, Integer fromYear, Integer toYear) {
+      this.childNumber = childNumber;
+      this.fromYear = fromYear;
+      this.toYear = toYear;
+    }
+    
+    public boolean isChildPeriod() {
+      return childNumber != null;
+    }
+    
+    public Integer getChildNumber() {
+      return childNumber;
+    }
+    
+    public DateInterval getChildInterval(LocalDate birthDate) {
+      if (fromYear == null || toYear == null) {
+        return null;
+      }
+      LocalDate from = birthDate.plusYears(fromYear);
+      LocalDate to = birthDate.plusYears(toYear).minusDays(1);
+      return new DateInterval(from, to);
+    }
   }
   
   public enum GroupAbsenceTypePattern {
