d052a8db661b11f91b124bcf5b33c0e985ac40bb
Daniele Murgia <daniele.murgia@iit.cnr.it>
2016-08-18 15:56:36 +0200

    Rimossi campi e metodi inutilizzati dagli enum relativi alle competenze. Aggiunta unità mese nel LimitUnit

--- app/models/enumerate/LimitUnit.java
+++ app/models/enumerate/LimitUnit.java
@@ -1,26 +1,9 @@
 package models.enumerate;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.val;
-
-@RequiredArgsConstructor
 public enum LimitUnit {
 
-  minutes("minuti"),
-  hours("ore"),
-  days("giorni");
-
-  @Getter
-  private final String description;
-
-  public static LimitUnit getByDescription(String description) {
-    for (val lu : values()) {
-      if (lu.description.equals(description)) {
-        return lu;
-      }
-    }
-    throw new IllegalArgumentException(String.format("unknonw LimitUnit %s", description));
-  }
-
+  minutes,
+  hours,
+  days,
+  months
 }
