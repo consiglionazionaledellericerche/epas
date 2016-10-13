3be3c53c8856e3de830a2c6d5be8f6314bd6135c
Daniele Murgia <daniele.murgia@iit.cnr.it>
2016-10-13 19:17:52 +0200

    Aggiunte drools per la gestione degli allegati (dell'ufficio) da parte dell'amministratore del personale

--- app/manager/configurations/ConfigurationDto.java
+++ app/manager/configurations/ConfigurationDto.java
@@ -1,7 +1,5 @@
 package manager.configurations;
 
-import controllers.Configurations;
-
 import lombok.Data;
 
 import manager.configurations.EpasParam.EpasParamValueType;
@@ -17,28 +15,28 @@ public class ConfigurationDto {
 
   public LocalDate validityBegin;
   public LocalDate validityEnd;
-  
+
   public Integer validityYear;
   public Boolean toTheEnd = false;
-  
+
   public Boolean booleanNewValue;
   public String stringNewValue;
   public Integer integerNewValue;
   public LocalDate localdateNewValue;
-  
+
   /**
    * Default constructor.
    */
   public ConfigurationDto() {
-    
+
   }
-  
+
   /**
    * Constructor from configuration (contiene i valori del dto iniziale).
-   * @param configuration
    */
-  public ConfigurationDto(EpasParam epasParam, LocalDate beginDate, LocalDate calculatedEnd, Object value ) {
-    
+  public ConfigurationDto(EpasParam epasParam, LocalDate beginDate, LocalDate calculatedEnd,
+      Object value) {
+
     if (epasParam.isGeneral()) {
       this.validityBegin = beginDate;
       this.validityEnd = calculatedEnd;
@@ -52,32 +50,32 @@ public class ConfigurationDto {
     }
 
     if (epasParam.epasParamValueType.equals(EpasParamValueType.BOOLEAN)) {
-      this.booleanNewValue = (Boolean)value;
+      this.booleanNewValue = (Boolean) value;
     }
     if (epasParam.epasParamValueType.equals(EpasParamValueType.INTEGER)) {
-      this.integerNewValue = (Integer)value;
+      this.integerNewValue = (Integer) value;
     }
     if (epasParam.epasParamValueType.equals(EpasParamValueType.IP_LIST)) {
-      this.stringNewValue = EpasParamValueType.formatValue((IpList)value);
+      this.stringNewValue = EpasParamValueType.formatValue((IpList) value);
     }
     if (epasParam.epasParamValueType.equals(EpasParamValueType.LOCALDATE)) {
-      this.localdateNewValue = (LocalDate)value;
+      this.localdateNewValue = (LocalDate) value;
     }
     if (epasParam.epasParamValueType.equals(EpasParamValueType.EMAIL)) {
-      this.stringNewValue = (String)value;
+      this.stringNewValue = (String) value;
     }
     if (epasParam.epasParamValueType.equals(EpasParamValueType.DAY_MONTH)) {
-      this.stringNewValue = EpasParamValueType.formatValue((MonthDay)value);
+      this.stringNewValue = EpasParamValueType.formatValue((MonthDay) value);
     }
     if (epasParam.epasParamValueType.equals(EpasParamValueType.MONTH)) {
-      this.integerNewValue = (Integer)value;
+      this.integerNewValue = (Integer) value;
     }
     if (epasParam.epasParamValueType.equals(EpasParamValueType.LOCALTIME)) {
-      this.stringNewValue = EpasParamValueType.formatValue((LocalTime)value);
+      this.stringNewValue = EpasParamValueType.formatValue((LocalTime) value);
     }
     if (epasParam.epasParamValueType.equals(EpasParamValueType.LOCALTIME_INTERVAL)) {
       this.stringNewValue = EpasParamValueType
-          .formatValue((LocalTimeInterval)value);
+          .formatValue((LocalTimeInterval) value);
     }
   }
 }
