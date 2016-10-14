e4dce7ec3fc91338851f374f1d7e55f54d718dfe
Cristian Lucchesi <cristian.lucchesi@iit.cnr.it>
2016-10-14 11:33:45 +0200

    Correzioni varie di stile.

--- app/models/absences/TakableAbsenceBehaviour.java
+++ app/models/absences/TakableAbsenceBehaviour.java
@@ -34,27 +34,23 @@ public class TakableAbsenceBehaviour extends BaseModel {
   @Enumerated(EnumType.STRING)
   public AmountType amountType;
   
-//  @Column(name = "taken_count_behaviour")
-//  @Enumerated(EnumType.STRING)
-//  public TakeCountBehaviour takenCountBehaviour;
-  
   @ManyToMany
   @JoinTable(name = "taken_codes_group", 
-  joinColumns = { @JoinColumn(name = "takable_behaviour_id") }, 
-  inverseJoinColumns = { @JoinColumn(name = "absence_types_id") })
+      joinColumns = { @JoinColumn(name = "takable_behaviour_id") }, 
+        inverseJoinColumns = { @JoinColumn(name = "absence_types_id") })
   @OrderBy("code")
   public Set<AbsenceType> takenCodes = Sets.newHashSet();
   
-//  @Column(name = "takable_count_behaviour")
-//  @Enumerated(EnumType.STRING)
-//  public TakeCountBehaviour takableCountBehaviour;
+  //  @Column(name = "takable_count_behaviour")
+  //  @Enumerated(EnumType.STRING)
+  //  public TakeCountBehaviour takableCountBehaviour;
   
   @ManyToMany
   @JoinTable(name = "takable_codes_group", 
-  joinColumns = { @JoinColumn(name = "takable_behaviour_id") }, 
-  inverseJoinColumns = { @JoinColumn(name = "absence_types_id") })
+      joinColumns = { @JoinColumn(name = "takable_behaviour_id") }, 
+      inverseJoinColumns = { @JoinColumn(name = "absence_types_id") })
   @OrderBy("code")
-  public Set<AbsenceType> takableCodes = Sets.newHashSet();;
+  public Set<AbsenceType> takableCodes = Sets.newHashSet();
   
   @Column(name = "fixed_limit")
   public Integer fixedLimit;
