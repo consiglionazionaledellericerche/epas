576cd267e591108deeafa7d17f40fc9562e02798
Daniele Murgia <daniele.murgia@iit.cnr.it>
2016-10-20 12:10:20 +0200

    Rivisti Jobs DarkNight e Expandable

--- app/jobs/ExpandableJob.java
+++ app/jobs/ExpandableJob.java
@@ -1,44 +1,38 @@
 package jobs;
 
-import com.google.common.base.Optional;
 import com.google.common.collect.ImmutableList;
-import com.google.common.collect.Sets;
 
-import dao.OfficeDao;
 import dao.PersonDao;
 
 import lombok.extern.slf4j.Slf4j;
 
 import manager.PersonDayInTroubleManager;
 
-import models.Person;
 import models.enumerate.Troubles;
 
 import org.joda.time.LocalDate;
 
 import play.Play;
-import play.jobs.Every;
 import play.jobs.Job;
-
-import java.util.List;
+import play.jobs.On;
 
 import javax.inject.Inject;
 
 //@On("0 34 15 ? * *")
 @SuppressWarnings("rawtypes")
 @Slf4j
-//@On("0 0 15 ? * MON,WED,FRI")
-@Every("1min")
+@On("0 0 15 ? * MON,WED,FRI")
 public class ExpandableJob extends Job {
 
   private static final String JOBS_CONF = "jobs.active";
   @Inject
-  private static PersonDayInTroubleManager personDayInTroubleManager;
+  static PersonDayInTroubleManager personDayInTroubleManager;
   @Inject
-  private static OfficeDao officeDao;
-  @Inject
-  private static PersonDao personDao;
+  static PersonDao personDao;
 
+  /**
+   * Esecuzione Job.
+   */
   public void doJob() {
 
     // in modo da inibire l'esecuzione dei job in base alla configurazione
@@ -49,23 +43,11 @@ public class ExpandableJob extends Job {
 
     log.info("Start Job expandable");
 
-    LocalDate fromDate = LocalDate.now().minusMonths(2);
-    LocalDate toDate = LocalDate.now().minusDays(1);
+    final LocalDate fromDate = LocalDate.now().minusMonths(2);
+    final LocalDate toDate = LocalDate.now().minusDays(1);
 
-    List<Person> personList = personDao.list(
-        Optional.<String>absent(),
-        Sets.newHashSet(officeDao.getAllOffices()),
-        false,
-        fromDate,
-        toDate,
-        true).list();
-
-    int nuovaQuery = personDao.peopleForTrouble().size();
-
-    personDayInTroubleManager.sendTroubleEmails(personList, fromDate, toDate,
-        ImmutableList.of(Troubles.NO_ABS_NO_STAMP));
-
-    log.info("\n\n\n NUOVA QUERY: {}\n\n\n", nuovaQuery);
+    personDayInTroubleManager.sendTroubleEmails(personDao.eligiblesForSendingAlerts(),
+        fromDate, toDate, ImmutableList.of(Troubles.NO_ABS_NO_STAMP));
 
     log.info("Concluso Job expandable");
   }
