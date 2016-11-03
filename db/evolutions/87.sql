89ab405e7c74f1de54cd23c6cb43ee2c4df073c7
Daniele Murgia <daniele.murgia@iit.cnr.it>
2016-11-03 17:32:52 +0100

    Closes #337 Corretta tabella badge_readers_badge_systems_history

---
+++ db/evolutions/87.sql
@@ -0,0 +1,7 @@
+# ---!Ups
+
+ALTER TABLE badge_readers_badge_systems_history DROP CONSTRAINT badge_readers_badge_systems_history_pkey;
+ALTER TABLE badge_readers_badge_systems_history DROP COLUMN id;
+ALTER TABLE badge_readers_badge_systems_history ADD PRIMARY KEY (_revision, _revision_type, badgereaders_id, badgesystems_id);
+
+# ---!Downs
