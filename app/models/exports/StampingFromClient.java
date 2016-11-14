f9c1ff9949d041e2865b379e60c72bf8e4fedded
Daniele Murgia <dmurgia85@gmail.com>
2016-11-14 16:17:57 +0100

    Closes #339. Aggiunti i vari codici di risposta http sull'endpoint per l'inserimento delle timbrature

--- app/models/exports/StampingFromClient.java
+++ app/models/exports/StampingFromClient.java
@@ -1,22 +1,24 @@
 package models.exports;
 
 import models.BadgeReader;
+import models.Person;
 import models.enumerate.StampTypes;
 
 import org.joda.time.LocalDateTime;
 
 /**
  * Esportazione delle informazioni relative ad una timbratura.
- * 
+ *
  * @author cristian
  */
 public class StampingFromClient {
 
+  public String numeroBadge;
   public Integer inOut;
   public BadgeReader badgeReader;
   public StampTypes stampType;
-  public Long personId;
-  public boolean markedByAdmin = false;
+  public Person person;
+  public boolean markedByAdmin;
   public LocalDateTime dateTime;
 
 }
