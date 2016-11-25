c2cada75809207d373a363b644da019024fc8fde
Daniele Murgia <dmurgia85@gmail.com>
2016-11-25 19:24:27 +0100

    Modificato DTO relativo al token prelevato dal server di attestati

---
+++ app/manager/attestati/service/OauthToken.java
@@ -0,0 +1,17 @@
+package manager.attestati.service;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+/**
+ * @author dario.
+ */
+public class OauthToken implements Serializable{
+  private static final long serialVersionUID = -1971342980047760996L;
+  public String access_token;
+  public String token_type;
+  public String refresh_token;
+  public int expires_in;
+  public String scope;
+  public LocalDateTime tookAt = LocalDateTime.now();
+}
