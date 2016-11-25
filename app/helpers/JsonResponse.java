b78059f1737bb2e1c6570d846944a94f22353654
Daniele Murgia <dmurgia85@gmail.com>
2016-11-25 11:36:22 +0100

    Generalizzate risposte per le API Json

--- app/helpers/JsonResponse.java
+++ app/helpers/JsonResponse.java
@@ -3,29 +3,54 @@ package helpers;
 import play.mvc.Http;
 import play.mvc.results.RenderJson;
 
-public class JsonResponse {
+public final class JsonResponse {
 
-  private static final String NOT_FOUND = "HTTP 404";
-  private static final String BAD_REQUEST = "HTTP 400";
+  private static final String SIMPLE = "HTTP %s";
+  private static final String WITH_MSG = "HTTP %s - %s";
+
+  public static final int CONFLICT = 409;
+
+  private JsonResponse() {
+  }
+
+  public static void ok() {
+    Http.Response.current().status = Http.StatusCode.OK;
+    throw new RenderJson(String.format(SIMPLE, Http.StatusCode.OK));
+  }
+
+  public static void ok(String message) {
+    Http.Response.current().status = Http.StatusCode.OK;
+    throw new RenderJson(String.format(WITH_MSG, Http.StatusCode.OK, message));
+  }
 
   public static void notFound() {
     Http.Response.current().status = Http.StatusCode.NOT_FOUND;
-    throw new RenderJson(NOT_FOUND);
+    throw new RenderJson(String.format(SIMPLE, Http.StatusCode.NOT_FOUND));
   }
 
   public static void notFound(String message) {
     Http.Response.current().status = Http.StatusCode.NOT_FOUND;
-    throw new RenderJson(NOT_FOUND + " - " + message);
+    throw new RenderJson(String.format(WITH_MSG, Http.StatusCode.NOT_FOUND, message));
   }
 
   public static void badRequest() {
     Http.Response.current().status = Http.StatusCode.BAD_REQUEST;
-    throw new RenderJson(BAD_REQUEST);
+    throw new RenderJson(String.format(SIMPLE, Http.StatusCode.BAD_REQUEST));
   }
 
   public static void badRequest(String message) {
     Http.Response.current().status = Http.StatusCode.BAD_REQUEST;
-    throw new RenderJson(BAD_REQUEST + " - " + message);
+    throw new RenderJson(String.format(WITH_MSG, Http.StatusCode.BAD_REQUEST, message));
+  }
+
+  public static void conflict() {
+    Http.Response.current().status = CONFLICT;
+    throw new RenderJson(String.format(SIMPLE, CONFLICT));
+  }
+
+  public static void conflict(String message) {
+    Http.Response.current().status = CONFLICT;
+    throw new RenderJson(String.format(WITH_MSG, CONFLICT, message));
   }
 
 }
