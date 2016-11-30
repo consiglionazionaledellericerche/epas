8f69fa03cb8b10ed1b01d6e1f3f39b3333d5dab2
Cristian Lucchesi <cristian.lucchesi@iit.cnr.it>
2016-11-30 17:03:17 +0100

    Closes #381.

--- app/helpers/LogEnhancer.java
+++ app/helpers/LogEnhancer.java
@@ -4,6 +4,7 @@ import controllers.Security;
 
 import org.slf4j.MDC;
 
+import play.mvc.After;
 import play.mvc.Before;
 import play.mvc.Controller;
 
@@ -17,10 +18,16 @@ public class LogEnhancer extends Controller {
 
   private static final String ANONYMOUS_USERNAME = "anonymous"; 
   
-  @Before(unless = {"login", "authenticate"})
-  static void injectTemplateData() {
+  @Before
+  static void mdcPutUser() {
     MDC.put(
         "user", 
         Security.getUser().isPresent() ? Security.getUser().get().username : ANONYMOUS_USERNAME);
+  }
+  
+  @After
+  static void mdcRemoveUser() {    
+    MDC.remove("user");
+    
   }
 }
