6e7831ce0826a594eff9203323c748cb22409b29
dario <dario.tagliaferri@iit.cnr.it>
2016-12-12 17:56:25 +0100

    Aggiunto campo alla tabella PersonShift che determina se una persona è disabilitata o meno al turno. Questo campo viene messo a true quando, dalla configurazione delle competenze della persona, viene rimossa l'abilitazione a qualsiasi tipo di turno (T1, T2, T3). Aggiunte le modifiche sul controller e ricreati i QModel con il nuovo campo della classe PersonShift

--- db/evolutions/87.sql
+++ db/evolutions/87.sql
@@ -9,7 +9,9 @@ ALTER TABLE shift_categories_history ADD COLUMN disabled BOOLEAN;
 ALTER TABLE shift_categories ADD COLUMN office_id BIGINT;
 ALTER TABLE shift_categories ADD COLUMN disabled BOOLEAN;
 ALTER TABLE shift_categories ADD FOREIGN KEY (office_id) REFERENCES office(id);
+ALTER TABLE person_shift ADD COLUMN disabled BOOLEAN;
 
+UPDATE person_shift SET disabled = false;
 UPDATE shift_categories SET disabled = false;
 
 UPDATE shift_time_table SET end_morning_lunch_time = '23:59:59' where id in (6,7,8,9);
@@ -21,6 +23,7 @@ ALTER TABLE shift_categories DROP CONSTRAINT shift_categories_office_id_fkey;
 ALTER TABLE shift_categories DROP COLUMN office_id;
 ALTER TABLE shift_categories DROP COLUMN disabled;
 
-
 ALTER TABLE shift_categories_history DROP COLUMN office_id;
 ALTER TABLE shift_categories_history DROP COLUMN disabled;
+
+ALTER TABLE person_shift DROP COLUMN disabled;
