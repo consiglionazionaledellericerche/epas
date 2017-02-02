d3a2a1d3fdf9c048361651148252d4fc9f5e8c45
Cristian Lucchesi <cristian.lucchesi@iit.cnr.it>
2017-02-02 18:33:27 +0100

    Disabilitati job per correzioni turni e reperibilità.

--- app/jobs/AssociateOfficeToShiftService.java
+++ app/jobs/AssociateOfficeToShiftService.java
@@ -2,23 +2,20 @@ package jobs;
 
 import lombok.extern.slf4j.Slf4j;
 
-import manager.CompetenceManager;
-import manager.ReperibilityManager;
 import manager.ShiftManager;
 
 import play.jobs.Job;
-import play.jobs.OnApplicationStart;
 
 import javax.inject.Inject;
 
 @Slf4j
-@OnApplicationStart(async = true)
-public class AssociateOfficeToShiftService extends Job {
+//@OnApplicationStart(async = true)
+public class AssociateOfficeToShiftService extends Job<Void> {
 
   @Inject
   static ShiftManager shiftManager;
   
-  
+  @Override
   public void doJob() {
     
     shiftManager.associateOfficeToShiftService();
