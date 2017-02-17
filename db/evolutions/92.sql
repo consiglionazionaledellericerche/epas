a5001fd03ee7a076ff919159a8937dd64c9a62db
dario <dario.tagliaferri@iit.cnr.it>
2017-02-17 17:12:29 +0100

    Migliorata evoluzione 92 specificando il campo jolly sul turno direttamente nella personShiftShiftType. Rivisto di conseguenza il modello e aggiornati i qmodel. Rivista la visione della gestione dell'attività da modale a pagina intera con nuova formattazione delle informazioni.

--- db/evolutions/92.sql
+++ db/evolutions/92.sql
@@ -2,15 +2,27 @@
 
 ALTER TABLE shift_type ADD COLUMN tolerance INT;
 ALTER TABLE shift_type ADD COLUMN hour_tolerance INT;
+
 ALTER TABLE shift_type_history ADD COLUMN tolerance INT;
 ALTER TABLE shift_type_history ADD COLUMN hour_tolerance INT;
-UPDATE shift_type SET tolerance = 0, hour_tolerance = 0;
 
+ALTER TABLE person_shift DROP COLUMN jolly;
+
+ALTER TABLE person_shift_shift_type ADD COLUMN jolly BOOLEAN;
+
+UPDATE shift_type SET tolerance = 0, hour_tolerance = 0;
+UPDATE person_shift_shift_type SET jolly = false;
 
 
 # ---!Downs
 
 ALTER TABLE shift_type_history DROP COLUMN hour_tolerance;
+ALTER TABLE shift_type_history DROP COLUMN tolerance;
+
 ALTER TABLE shift_type DROP COLUMN hour_tolerance;
 ALTER TABLE shift_type DROP COLUMN tolerance;
-ALTER TABLE shift_type_history DROP COLUMN tolerance;
+
+ALTER TABLE person_shift_shift_type DROP COLUMN jolly;
+ALTER TABLE person_shift ADD COLUMN jolly BOOLEAN;
+
+UPDATE person_shift SET jolly = FALSE;
