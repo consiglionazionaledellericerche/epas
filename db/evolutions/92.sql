ee11e4b4b189c6890e0309336452a4494f736ebb
dario <dario.tagliaferri@iit.cnr.it>
2017-03-13 15:13:55 +0100

    Aggiornata l'evoluzione 92 con la differenziazione delle tolleranze, separate in entrata e uscita alla luce delle informazioni prese dalla sede centrale. Ricreati di conseguenza i qmodel e aggiunto enumerato per la gestione delle tolleranze in fase di inserimento dalla form.

--- db/evolutions/92.sql
+++ db/evolutions/92.sql
@@ -1,26 +1,38 @@
 # ---!Ups
 
-ALTER TABLE shift_type ADD COLUMN tolerance INT;
+ALTER TABLE shift_type ADD COLUMN entrance_tolerance INT;
+ALTER TABLE shift_type ADD COLUMN exit_tolerance INT;
 ALTER TABLE shift_type ADD COLUMN hour_tolerance INT;
+ALTER TABLE shift_type ADD COLUMN break_in_shift_enabled BOOLEAN;
+ALTER TABLE shift_type ADD COLUMN break_in_shift INT;
 
-ALTER TABLE shift_type_history ADD COLUMN tolerance INT;
+ALTER TABLE shift_type_history ADD COLUMN entrance_tolerance INT;
+ALTER TABLE shift_type_history ADD COLUMN exit_tolerance INT;
 ALTER TABLE shift_type_history ADD COLUMN hour_tolerance INT;
+ALTER TABLE shift_type_history ADD COLUMN break_in_shift_enabled BOOLEAN;
+ALTER TABLE shift_type_history ADD COLUMN break_in_shift INT;
 
 ALTER TABLE person_shift DROP COLUMN jolly;
 
 ALTER TABLE person_shift_shift_type ADD COLUMN jolly BOOLEAN;
 
-UPDATE shift_type SET tolerance = 0, hour_tolerance = 0;
+UPDATE shift_type SET entrance_tolerance = 0, exit_tolerance = 0, hour_tolerance = 0, break_in_shift = 0, break_in_shift_enabled = false;
 UPDATE person_shift_shift_type SET jolly = false;
 
 
 # ---!Downs
 
 ALTER TABLE shift_type_history DROP COLUMN hour_tolerance;
-ALTER TABLE shift_type_history DROP COLUMN tolerance;
+ALTER TABLE shift_type_history DROP COLUMN entrance_tolerance;
+ALTER TABLE shift_type_history DROP COLUMN exit_tolerance;
+ALTER TABLE shift_type_history DROP COLUMN break_in_shift_enabled;
+ALTER TABLE shift_type_history DROP COLUMN break_in_shift;
 
 ALTER TABLE shift_type DROP COLUMN hour_tolerance;
-ALTER TABLE shift_type DROP COLUMN tolerance;
+ALTER TABLE shift_type DROP COLUMN entrance_tolerance;
+ALTER TABLE shift_type DROP COLUMN exit_tolerance;
+ALTER TABLE shift_type DROP COLUMN break_in_shift_enabled;
+ALTER TABLE shift_type DROP COLUMN break_in_shift;
 
 ALTER TABLE person_shift_shift_type DROP COLUMN jolly;
 ALTER TABLE person_shift ADD COLUMN jolly BOOLEAN;
