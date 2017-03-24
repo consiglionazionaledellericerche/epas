82578a03ebe1e8dd78b8add7e8433102d44a2d6c
alessandro.martelli <alessandro.martelli@iit.cnr.it>
2017-03-24 12:24:58 +0100

    Inizializzazione buoni pasto inizio del contratto.
    E' possibile in via eccezionale impostare una inizializzazione buoni
    pasto al giorno precedente l'inizio del contratto selezionato, nel caso
    in cui il contratto inizi il primo del mese e non vi sia
    inizializzazione generale.

--- app/manager/ContractMonthRecapManager.java
+++ app/manager/ContractMonthRecapManager.java
@@ -121,13 +121,26 @@ public class ContractMonthRecapManager {
       cmr.buoniPastoDalMesePrecedente =
           recapPreviousMonth.get().remainingMealTickets;
     }
+    //Se è il primo riepilogo dovuto ad inzializzazione utilizzo i dati
+    //in source
     if (contract.sourceDateMealTicket != null
         && contract.sourceDateMealTicket.getYear() == yearMonth.getYear()
         && contract.sourceDateMealTicket.getMonthOfYear() == yearMonth.getMonthOfYear()) {
-      //Se è il primo riepilogo dovuto ad inzializzazione utilizzo i dati
-      //in source
       cmr.buoniPastoDaInizializzazione = contract.sourceRemainingMealTicket;
       cmr.buoniPastoDalMesePrecedente = 0;
+    }
+    //Ma c'è il particolarissimo caso in cui il contratto inizia il primo del mese,
+    // non ho definito inizializzazione generale, e voglio impostare il residuo iniziale 
+    // (all'ultimo giorno del mese precedente).
+    // TODO: capire se rifattorizzando si può escludere questa orribile condizione.
+    if (contract.sourceDateResidual == null 
+        && contract.sourceDateMealTicket != null 
+        && contract.beginDate.getDayOfMonth() == 1 
+        && contract.sourceDateMealTicket.isEqual(contract.beginDate.minusDays(1))
+        && new YearMonth(contract.beginDate).equals(yearMonth)) {
+      
+      cmr.buoniPastoDaInizializzazione = 0;
+      cmr.buoniPastoDalMesePrecedente = contract.sourceRemainingMealTicket;
     }
 
     //////////////////////////////////////////////////////////////////////////
