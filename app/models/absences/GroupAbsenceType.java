fc6b5ffa09f2ea7f3385b5c396c5658c5b4eb229
alessandro.martelli <alessandro.martelli@iit.cnr.it>
2017-04-05 17:36:17 +0200

    Enum Gruppi e rinominato metodo che si confondeva coi getter play.
    Rinominato il metodo groupAbsenceType.getChainDescription() in
    computeChainDescription().

--- app/models/absences/GroupAbsenceType.java
+++ app/models/absences/GroupAbsenceType.java
@@ -97,14 +97,14 @@ public class GroupAbsenceType extends BaseModel {
    * @return label
    */
   public String getLabel() {
-    return getChainDescription();
+    return computeChainDescription();
   }
   
   /**
    * La stringa che rappresenta la catena cui appartiene il gruppo.
    * @return chainDescription
    */
-  public String getChainDescription() {
+  public String computeChainDescription() {
     if (!Strings.isNullOrEmpty(this.chainDescription)) {
       return this.chainDescription;
     } else {
@@ -185,39 +185,155 @@ public class GroupAbsenceType extends BaseModel {
   }
   
   public enum DefaultGroup {
-    G_18, 
-    G_19, 
-    G_661, 
     
-    G_23, 
-    G_25, 
-    G_232, 
-    G_252, 
-    G_233, 
-    G_253,
-
-    G_89, 
-    G_09, 
-    MISSIONE, 
-    ALTRI, 
-    FERIE_CNR, 
-    RIPOSI_CNR, 
-    G_95,
+    G_18("18 - Permesso assistenza parenti/affini disabili L. 104/92 tre giorni mese", 
+        "", 
+        DefaultCategoryType.L_104, 
+        GroupAbsenceTypePattern.programmed, PeriodType.month, 
+        DefaultTakable.T_18, DefaultComplation.C_18, null, false, false),
     
-    MALATTIA, 
-    MALATTIA_FIGLIO_1_12,
-    MALATTIA_FIGLIO_1_13,
-    MALATTIA_FIGLIO_1_14,
-    MALATTIA_FIGLIO_2_12,
-    MALATTIA_FIGLIO_2_13,
-    MALATTIA_FIGLIO_2_14,
-    MALATTIA_FIGLIO_3_12,
-    MALATTIA_FIGLIO_3_13,
-    MALATTIA_FIGLIO_3_14,
+    G_19("19 - Permesso per dipendente disabile L. 104/92 tre giorni mese", 
+        "", 
+        DefaultCategoryType.L_104, 
+        GroupAbsenceTypePattern.programmed, PeriodType.month, 
+        DefaultTakable.T_19, DefaultComplation.C_19, null, false, false),
+    G_661("661 - Permesso orario per motivi personali 18 ore anno", 
+        "", 
+        DefaultCategoryType.PERMESSI_VARI, 
+        GroupAbsenceTypePattern.programmed, PeriodType.year, 
+        DefaultTakable.T_661, DefaultComplation.C_661, null, false, false),
+    G_89("89 - Permesso diritto allo studio 150 ore anno", 
+        "", 
+        DefaultCategoryType.PERMESSI_VARI, 
+        GroupAbsenceTypePattern.programmed, PeriodType.year, 
+        DefaultTakable.T_89, DefaultComplation.C_89, null, false, false),
     
-    PB,
-    EMPLOYEE,
-    FUORI_SEDE_105BP;
+    G_09("09 - Permesso visita medica", 
+        "", 
+        DefaultCategoryType.PERMESSI_VARI, 
+        GroupAbsenceTypePattern.programmed, PeriodType.always, 
+        DefaultTakable.T_09, DefaultComplation.C_09, null, false, false),
+    MISSIONE("Missione", 
+        "", 
+        DefaultCategoryType.MISSIONE_CNR, 
+        GroupAbsenceTypePattern.simpleGrouping, PeriodType.always, 
+        DefaultTakable.T_MISSIONE, null, null, false, false),
+    FERIE_CNR("Ferie e permessi legge CNR", 
+        "", 
+        DefaultCategoryType.FERIE_CNR, 
+        GroupAbsenceTypePattern.vacationsCnr, PeriodType.always, 
+        DefaultTakable.T_FERIE_CNR, null, null, false, false),
+    RIPOSI_CNR("Riposi compensativi CNR", 
+        "", 
+        DefaultCategoryType.RIPOSI_COMPENSATIVI_CNR, 
+        GroupAbsenceTypePattern.compensatoryRestCnr, PeriodType.always, 
+        DefaultTakable.T_RIPOSI_CNR, null, null, false, false),
+    MALATTIA("111 - Malattia", 
+        "", 
+        DefaultCategoryType.MALATTIA_DIPENDENTE, 
+        GroupAbsenceTypePattern.simpleGrouping, PeriodType.always, 
+        DefaultTakable.T_MALATTIA, null, null, false, false),
+    MALATTIA_FIGLIO_1_12("12 - Malattia primo figlio <= 3 anni retribuita 100%", 
+        "", 
+        DefaultCategoryType.MALATTIA_FIGLIO_1, 
+        GroupAbsenceTypePattern.programmed, PeriodType.child1_0_3, 
+        DefaultTakable.T_MALATTIA_FIGLIO_1_12, null, null, false, false),
+    MALATTIA_FIGLIO_1_13("13 - Malattia primo figlio oltre 3 anni non retribuita", 
+        "", 
+        DefaultCategoryType.MALATTIA_FIGLIO_1, 
+        GroupAbsenceTypePattern.programmed, PeriodType.child1_3_12, 
+        DefaultTakable.T_MALATTIA_FIGLIO_1_13, null, null, false, false),
+    MALATTIA_FIGLIO_1_14("14 - Malattia primo figlio <= 3 anni non retribuita", 
+        "", 
+        DefaultCategoryType.MALATTIA_FIGLIO_1, 
+        GroupAbsenceTypePattern.programmed, PeriodType.child1_0_3, 
+        DefaultTakable.T_MALATTIA_FIGLIO_1_14, null, null, false, false),
+    MALATTIA_FIGLIO_2_12("122 - Malattia secondo figlio <= 3 anni retribuita 100%", 
+        "", 
+        DefaultCategoryType.MALATTIA_FIGLIO_2, 
+        GroupAbsenceTypePattern.programmed, PeriodType.child2_0_3, 
+        DefaultTakable.T_MALATTIA_FIGLIO_2_12, null, null, false, false),
+    MALATTIA_FIGLIO_2_13("132 - Malattia secondo figlio oltre 3 anni non retribuita", 
+        "", 
+        DefaultCategoryType.MALATTIA_FIGLIO_2, 
+        GroupAbsenceTypePattern.programmed, PeriodType.child2_3_12, 
+        DefaultTakable.T_MALATTIA_FIGLIO_2_13, null, null, false, false),
+    G_24("24 - Astensione facoltativa post partum non retrib. primo figlio 0-12 anni 600 giorni", 
+        "", 
+        DefaultCategoryType.CONGEDI_PARENTALI, 
+        GroupAbsenceTypePattern.programmed, PeriodType.child1_0_12, 
+        DefaultTakable.T_24, DefaultComplation.C_24, null, false, true),
+    G_25("25 - Astensione facoltativa post partum 30% primo figlio 0-6 anni 150 giorni", 
+        "", 
+        DefaultCategoryType.CONGEDI_PARENTALI, 
+        GroupAbsenceTypePattern.programmed, PeriodType.child1_0_6, 
+        DefaultTakable.T_25, DefaultComplation.C_25, DefaultGroup.G_24, false, true),
+    G_23("23 - Astensione facoltativa post partum 100% primo figlio 0-12 anni 30 giorni", 
+        "23/25/24 - Astensione facoltativa post partum primo figlio", 
+        DefaultCategoryType.CONGEDI_PARENTALI, 
+        GroupAbsenceTypePattern.programmed, PeriodType.child1_0_12, 
+        DefaultTakable.T_23, DefaultComplation.C_23, DefaultGroup.G_25, false, true),
+    
+    G_242("242 - Astensione facoltativa post partum non retrib. secondo figlio 0-12 anni 600 giorni", 
+        "", 
+        DefaultCategoryType.CONGEDI_PARENTALI, 
+        GroupAbsenceTypePattern.programmed, PeriodType.child2_0_12, 
+        DefaultTakable.T_242, DefaultComplation.C_242, null, false, true),
+    G_252("252 - Astensione facoltativa post partum 30% secondo figlio 0-6 anni 150 giorni", 
+        "", 
+        DefaultCategoryType.CONGEDI_PARENTALI, 
+        GroupAbsenceTypePattern.programmed, PeriodType.child2_0_6, 
+        DefaultTakable.T_252, DefaultComplation.C_252, DefaultGroup.G_242, false, true),
+    G_232("232 - Astensione facoltativa post partum 100% secondo figlio 0-12 anni 30 giorni", 
+        "232/252/242 - Astensione facoltativa post partum secondo figlio", 
+        DefaultCategoryType.CONGEDI_PARENTALI, 
+        GroupAbsenceTypePattern.programmed, PeriodType.child2_0_12, 
+        DefaultTakable.T_232, DefaultComplation.C_232, DefaultGroup.G_252, false, true),
+   
+    G_243("243 - Astensione facoltativa post partum non retrib. terzo figlio 0-12 anni 600 giorni", 
+        "", DefaultCategoryType.CONGEDI_PARENTALI, 
+        GroupAbsenceTypePattern.programmed, PeriodType.child3_0_12, 
+        DefaultTakable.T_243, DefaultComplation.C_243, null, false, true),
+    G_253("253 - Astensione facoltativa post partum 30% terzo figlio 0-6 anni 150 giorni", 
+        "", DefaultCategoryType.CONGEDI_PARENTALI, 
+        GroupAbsenceTypePattern.programmed, PeriodType.child3_0_6, 
+        DefaultTakable.T_253, DefaultComplation.C_253, DefaultGroup.G_243, false, true),
+    G_233("233 - Astensione facoltativa post partum 100% terzo figlio 0-12 anni 30 giorni", 
+        "233/253/243 - Astensione facoltativa post partum terzo figlio", 
+        DefaultCategoryType.CONGEDI_PARENTALI, 
+        GroupAbsenceTypePattern.programmed, PeriodType.child3_0_12, 
+        DefaultTakable.T_233, DefaultComplation.C_233, DefaultGroup.G_253, false, true),
+    
+    MALATTIA_FIGLIO_2_14("142 - Malattia secondo figlio <= 3 anni non retribuita", 
+        "", 
+        DefaultCategoryType.MALATTIA_FIGLIO_2, 
+        GroupAbsenceTypePattern.programmed, PeriodType.child2_0_3, 
+        DefaultTakable.T_MALATTIA_FIGLIO_2_14, null, null, false, false),
+    MALATTIA_FIGLIO_3_12("123 - Malattia terzo figlio <= 3 anni retribuita 100%", 
+        "", 
+        DefaultCategoryType.MALATTIA_FIGLIO_3, 
+        GroupAbsenceTypePattern.programmed, PeriodType.child3_0_3, 
+        DefaultTakable.T_MALATTIA_FIGLIO_3_12, null, null, false, false),
+    MALATTIA_FIGLIO_3_13("133 - Malattia terzo figlio oltre 3 anni non retribuita", 
+        "", 
+        DefaultCategoryType.MALATTIA_FIGLIO_3, 
+        GroupAbsenceTypePattern.programmed, PeriodType.child3_3_12, 
+        DefaultTakable.T_MALATTIA_FIGLIO_3_13, null, null, false, false),
+    MALATTIA_FIGLIO_3_14("143 - Malattia terzo figlio <= 3 anni non retribuita", 
+        "", 
+        DefaultCategoryType.MALATTIA_FIGLIO_3, 
+        GroupAbsenceTypePattern.programmed, PeriodType.child3_0_3, 
+        DefaultTakable.T_MALATTIA_FIGLIO_3_14, null, null, false, false),
+    PB("PB - Permesso breve 36 ore anno", 
+        "", 
+        DefaultCategoryType.CODICI_AUTOMATICI, 
+        GroupAbsenceTypePattern.programmed, PeriodType.year, 
+        DefaultTakable.T_PB, null, null, false, false),
+    EMPLOYEE("Codici inseribili dai dipendenti", 
+        "", 
+        DefaultCategoryType.CODICI_DIPENDENTI, 
+        GroupAbsenceTypePattern.simpleGrouping, PeriodType.always, 
+        DefaultTakable.T_EMPLOYEE, null, null, false, false);
     
     public String description;
     public String chainDescription;
@@ -229,6 +345,26 @@ public class GroupAbsenceType extends BaseModel {
     public DefaultGroup nextGroupToCheck;
     public boolean automatic;
     public boolean initializable;
+    
+    private DefaultGroup(String description, 
+        String chainDescription, 
+        DefaultCategoryType category, 
+        GroupAbsenceTypePattern pattern, PeriodType periodType, 
+        DefaultTakable takable, DefaultComplation complation, DefaultGroup nextGroupToCheck, 
+        boolean automatic, boolean initializable) {
+
+      this.description = description;
+      this.chainDescription = chainDescription;
+      this.category = category;
+      this.pattern = pattern;
+      this.periodType = periodType;
+      this.takable = takable;
+      this.complation = complation;
+      this.nextGroupToCheck = nextGroupToCheck;
+      this.automatic = automatic;
+      this.initializable = initializable;
+
+    }
     
     
     
