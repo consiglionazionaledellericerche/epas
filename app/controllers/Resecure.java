f37837f5924224094a5323f22cea7fdb2b6a1f0e
Cristian Lucchesi <cristian.lucchesi@iit.cnr.it>
2017-04-12 18:55:00 +0200

    Semplificazione security con basic auth limitata a dove specificato.

--- app/controllers/Resecure.java
+++ app/controllers/Resecure.java
@@ -1,15 +1,18 @@
 package controllers;
 
 import helpers.LogEnhancer;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+
 import javax.inject.Inject;
-import play.Play;
+
 import play.mvc.Before;
 import play.mvc.Controller;
 import play.mvc.With;
+
 import security.SecurityRules;
 
 /**
@@ -31,52 +34,25 @@ public class Resecure extends Controller {
         || getControllerInheritedAnnotation(NoCheck.class) != null) {
       return;
     } else {
-      //Se si tenta di accedere con Basic Auth si controlla subito utente e password
-      if (request.user != null && !Security.authenticate(request.user, request.password)) {
-        unauthorized(REALM);
-      }
-
-      if (!Security.getUser().isPresent()) {
-        flash.put(
-            "url",
-            // seems a good default
-            "GET".equals(request.method) ? request.url : Play.ctxPath + "/");
-        Secure.login();
-      }
-      // Checks
-      Check check = getActionAnnotation(Check.class);
-      if (check != null) {
-        check(check);
-      }
-      check = getControllerInheritedAnnotation(Check.class);
-      if (check != null) {
-        check(check);
-      }
+      if (getActionAnnotation(BasicAuth.class) != null
+          || getControllerInheritedAnnotation(BasicAuth.class) != null) {
+        if (request.user == null
+            || !Security.authenticate(request.user, request.password)) {
+          unauthorized(REALM);
+        } else {
+          return;
+        }
+      }      
+      Secure.checkAccess();      
       rules.checkIfPermitted();
     }
   }
 
-  private static void check(Check check) throws Throwable {
-    for (String profile : check.value()) {
-      boolean hasProfile = (Boolean) Security.invoke("check", profile);
-      if (!hasProfile) {
-        Security.invoke("onCheckFailed", profile);
-      }
-    }
-  }
-
-  public static boolean check(String action, Object instance) {
-    if (instance != null) {
-      return session.contains("username") && rules.check(action, instance);
-    } else {
-      return session.contains("username") && rules.checkAction(action);
-    }
-  }
-
   /**
+   * Con questo si evitano i controlli. 
+   *
    * @author marco
    *
-   *         Con questo si evitano i controlli.
    */
   @Retention(RetentionPolicy.RUNTIME)
   @Target({ElementType.METHOD})
@@ -85,9 +61,10 @@ public class Resecure extends Controller {
   }
 
   /**
+   * Con questo si adotta soltanto la basicauth.
+   * 
    * @author marco
-   *
-   *         Con questo si adotta soltanto la basicauth.
+   * 
    */
   @Retention(RetentionPolicy.RUNTIME)
   @Target({ElementType.METHOD})
