e3cdbfb46416e9244fe552f7ab2288cb4d3cfff8
Daniele Murgia <daniele.murgia@iit.cnr.it>
2017-04-13 19:20:01 +0200

    Rifattorizzata lievemente la Resecure.checkAccess

--- app/controllers/Resecure.java
+++ app/controllers/Resecure.java
@@ -1,19 +1,14 @@
 package controllers;
 
 import helpers.LogEnhancer;
-
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-
 import javax.inject.Inject;
-
 import play.mvc.Before;
 import play.mvc.Controller;
 import play.mvc.With;
-import play.mvc.Scope.Session;
-
 import security.SecurityRules;
 
 /**
@@ -34,20 +29,16 @@ public class Resecure extends Controller {
     if (getActionAnnotation(NoCheck.class) != null
         || getControllerInheritedAnnotation(NoCheck.class) != null) {
       return;
-    } else {
-      if (getActionAnnotation(BasicAuth.class) != null
-          || getControllerInheritedAnnotation(BasicAuth.class) != null) {
-        if (request.user == null
-            || !Security.authenticate(request.user, request.password)) {
-          unauthorized(REALM);
-        } else {
-          // WARNING: inserimento in sessione per farlo digerire alla play Security
-          Session.current().put("username", request.user);
-        }
-      }
-      Secure.checkAccess();
-      rules.checkIfPermitted();
     }
+    if (getActionAnnotation(BasicAuth.class) != null
+        || getControllerInheritedAnnotation(BasicAuth.class) != null) {
+      if (!request.args.containsKey(Security.CURRENT_USER)) {
+        unauthorized(REALM);
+      }
+    } else {
+      Secure.checkAccess();
+    }
+    rules.checkIfPermitted();
   }
 
   public static boolean check(String action, Object instance) {
@@ -59,10 +50,9 @@ public class Resecure extends Controller {
   }
 
   /**
-   * Con questo si evitano i controlli. 
+   * Con questo si evitano i controlli.
    *
    * @author marco
-   *
    */
   @Retention(RetentionPolicy.RUNTIME)
   @Target({ElementType.METHOD})
@@ -72,9 +62,8 @@ public class Resecure extends Controller {
 
   /**
    * Con questo si adotta soltanto la basicauth.
-   * 
+   *
    * @author marco
-   * 
    */
   @Retention(RetentionPolicy.RUNTIME)
   @Target({ElementType.METHOD})
