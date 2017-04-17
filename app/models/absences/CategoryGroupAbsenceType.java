86258bdc08228a7c5167265532a2ab699a038aa2
alessandro.martelli <alessandro.martelli@iit.cnr.it>
2017-04-17 16:34:58 +0200

    Priorità ai gruppi.

--- app/models/absences/CategoryGroupAbsenceType.java
+++ app/models/absences/CategoryGroupAbsenceType.java
@@ -1,9 +1,13 @@
 package models.absences;
 
 import com.google.common.base.Optional;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Set;
+import java.util.SortedMap;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -12,6 +16,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 
 import models.absences.CategoryTab.DefaultTab;
 import models.base.BaseModel;
@@ -46,6 +51,37 @@ public class CategoryGroupAbsenceType extends BaseModel
   @Override
   public int compareTo(CategoryGroupAbsenceType obj) {
     return name.compareTo(obj.name);
+  }
+  
+  /**
+   * Un modo (semplificabile) per ordinare i gruppi della categoria per priorità, prendendoli 
+   * (se ci sono) dallo heap.
+   * @param onlyFirstOfChain se voglio solo i primi della catena.
+   */
+  @Transient
+  public List<GroupAbsenceType> orderedGroupsInCategory(boolean onlyFirstOfChain) {
+    
+    SortedMap<Integer, Set<GroupAbsenceType>> setByPriority = 
+        Maps.newTreeMap();
+    
+    //ogni gruppo lo inserisco con quelli della stessa priorità
+    for (GroupAbsenceType group : this.groupAbsenceTypes) {
+      if (onlyFirstOfChain && !group.previousGroupChecked.isEmpty()) {
+        continue;
+      }
+      Set<GroupAbsenceType> prioritySet = setByPriority.get(group.priority);
+      if (prioritySet == null) {
+        prioritySet = Sets.newHashSet();
+        setByPriority.put(group.priority, prioritySet);
+      }
+      prioritySet.add(group);
+    }
+    //lista unica ordinata 
+    List<GroupAbsenceType> orderedGroupInCateogory = Lists.newArrayList();
+    for (Set<GroupAbsenceType> set : setByPriority.values()) {
+      orderedGroupInCateogory.addAll(set);
+    }
+    return orderedGroupInCateogory;
   }
   
   /**
