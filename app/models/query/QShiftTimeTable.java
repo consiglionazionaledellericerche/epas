6de1eb539db54c8cbe1f9066c855cb3cfff5d8fa
dario <dario.tagliaferri@iit.cnr.it>
2017-04-18 17:19:00 +0200

    Aggiunta relazione tra la timetable e la sede così che ogni sede possa eventualmente definirsi la propria timetable per i turni. Aggiunti i campi per il turno notturno. Modificata la creazione dell'attività separandola in più step. Ricreati i qmodel. Iniziata la parte di creazione di una timetable personalizzata

--- app/models/query/QShiftTimeTable.java
+++ app/models/query/QShiftTimeTable.java
@@ -20,6 +20,8 @@ public class QShiftTimeTable extends EntityPathBase<ShiftTimeTable> {
 
     private static final long serialVersionUID = -144019773L;
 
+    private static final PathInits INITS = PathInits.DIRECT2;
+
     public static final QShiftTimeTable shiftTimeTable = new QShiftTimeTable("shiftTimeTable");
 
     public final models.base.query.QBaseModel _super = new models.base.query.QBaseModel(this);
@@ -27,6 +29,10 @@ public class QShiftTimeTable extends EntityPathBase<ShiftTimeTable> {
     public final TimePath<org.joda.time.LocalTime> endAfternoon = createTime("endAfternoon", org.joda.time.LocalTime.class);
 
     public final TimePath<org.joda.time.LocalTime> endAfternoonLunchTime = createTime("endAfternoonLunchTime", org.joda.time.LocalTime.class);
+
+    public final TimePath<org.joda.time.LocalTime> endEvening = createTime("endEvening", org.joda.time.LocalTime.class);
+
+    public final TimePath<org.joda.time.LocalTime> endEveningLunchTime = createTime("endEveningLunchTime", org.joda.time.LocalTime.class);
 
     public final TimePath<org.joda.time.LocalTime> endMorning = createTime("endMorning", org.joda.time.LocalTime.class);
 
@@ -37,6 +43,8 @@ public class QShiftTimeTable extends EntityPathBase<ShiftTimeTable> {
 
     //inherited
     public final NumberPath<Long> id = _super.id;
+
+    public final QOffice office;
 
     public final NumberPath<Integer> paidMinutes = createNumber("paidMinutes", Integer.class);
 
@@ -49,6 +57,10 @@ public class QShiftTimeTable extends EntityPathBase<ShiftTimeTable> {
 
     public final TimePath<org.joda.time.LocalTime> startAfternoonLunchTime = createTime("startAfternoonLunchTime", org.joda.time.LocalTime.class);
 
+    public final TimePath<org.joda.time.LocalTime> startEvening = createTime("startEvening", org.joda.time.LocalTime.class);
+
+    public final TimePath<org.joda.time.LocalTime> startEveningLunchTime = createTime("startEveningLunchTime", org.joda.time.LocalTime.class);
+
     public final TimePath<org.joda.time.LocalTime> startMorning = createTime("startMorning", org.joda.time.LocalTime.class);
 
     public final TimePath<org.joda.time.LocalTime> startMorningLunchTime = createTime("startMorningLunchTime", org.joda.time.LocalTime.class);
@@ -56,15 +68,24 @@ public class QShiftTimeTable extends EntityPathBase<ShiftTimeTable> {
     public final NumberPath<Integer> totalWorkMinutes = createNumber("totalWorkMinutes", Integer.class);
 
     public QShiftTimeTable(String variable) {
-        super(ShiftTimeTable.class, forVariable(variable));
+        this(ShiftTimeTable.class, forVariable(variable), INITS);
     }
 
     public QShiftTimeTable(Path<? extends ShiftTimeTable> path) {
-        super(path.getType(), path.getMetadata());
+        this(path.getType(), path.getMetadata(), path.getMetadata().isRoot() ? INITS : PathInits.DEFAULT);
     }
 
     public QShiftTimeTable(PathMetadata<?> metadata) {
-        super(ShiftTimeTable.class, metadata);
+        this(metadata, metadata.isRoot() ? INITS : PathInits.DEFAULT);
+    }
+
+    public QShiftTimeTable(PathMetadata<?> metadata, PathInits inits) {
+        this(ShiftTimeTable.class, metadata, inits);
+    }
+
+    public QShiftTimeTable(Class<? extends ShiftTimeTable> type, PathMetadata<?> metadata, PathInits inits) {
+        super(type, metadata, inits);
+        this.office = inits.isInitialized("office") ? new QOffice(forProperty("office"), inits.get("office")) : null;
     }
 
 }
