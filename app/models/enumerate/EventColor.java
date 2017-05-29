b93f8db50290a4ad85cce9c7ec1669e495c5c54b
dario <dario.tagliaferri@iit.cnr.it>
2017-05-29 09:25:10 +0200

    aggiunto nuovo colore

--- app/models/enumerate/EventColor.java
+++ app/models/enumerate/EventColor.java
@@ -5,9 +5,10 @@ package models.enumerate;
  * @since 26/05/17.
  */
 public enum EventColor {
-  ONE("#B1D13D", "#09822D", "black"),
-  TWO("#41718E", "#1A159F", "white"),
-  THREE("#FFDC47", "#4B4705", "#4B4705")
+  ONE("#C7E55A", "#09822D", "black"),
+  TWO("#d9edf7", "#1A159F", "white"),
+  THREE("#FFDC47", "#4B4705", "#4B4705"),
+  FOUR("#f2dede", "#ebccd1", "#A94442")
   ;
 
   public final String backgroundColor;
