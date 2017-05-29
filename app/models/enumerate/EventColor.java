8a7208e80091683c8718917f8db5c215f89328ea
dario <dario.tagliaferri@iit.cnr.it>
2017-05-29 09:27:48 +0200

    Merge branch 'calendar-refactor-v2' of ssh://git@gitlab.tools.iit.cnr.it:10022/epas/epas.git into calendar-refactor-v2
    Conflicts:
    	app/models/enumerate/EventColor.java

--- app/models/enumerate/EventColor.java
+++ app/models/enumerate/EventColor.java
@@ -5,11 +5,13 @@ package models.enumerate;
  * @since 26/05/17.
  */
 public enum EventColor {
-  ONE("#C7E55A", "#09822D", "black"),
-  TWO("#d9edf7", "#1A159F", "white"),
-  THREE("#FFDC47", "#4B4705", "#4B4705"),
-  FOUR("#f2dede", "#ebccd1", "#A94442")
-  ;
+
+  ORANGE("#FFB847", "#FFAA24", "#633E05"),
+  ACQUA_GREEN("#6CC8B9", "#158673", "#06322B"),
+  ONE("#B1D13D", "#09822D", "black"),
+  TWO("#41718E", "#1A159F", "white"),
+  THREE("#FFDC47", "#4B4705", "#4B4705");
+
 
   public final String backgroundColor;
   public final String borderColor;
