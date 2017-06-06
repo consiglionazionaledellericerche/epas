9587f3c8591f48084aab5dd76ed7dd84dc11d77b
Daniele Murgia <daniele.murgia@iit.cnr.it>
2017-06-06 16:24:36 +0200

    Aggiunte query nel PersonShiftDayDao utili per i controlli sui turni

--- app/dao/PersonShiftDayDao.java
+++ app/dao/PersonShiftDayDao.java
@@ -4,12 +4,9 @@ import com.google.common.base.Optional;
 import com.google.inject.Provider;
 import com.mysema.query.jpa.JPQLQuery;
 import com.mysema.query.jpa.JPQLQueryFactory;
-
 import java.util.List;
-
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
-
 import models.Person;
 import models.PersonShift;
 import models.PersonShiftDay;
@@ -19,7 +16,6 @@ import models.enumerate.ShiftSlot;
 import models.query.QPersonShift;
 import models.query.QPersonShiftDay;
 import models.query.QShiftCategories;
-
 import org.joda.time.LocalDate;
 
 /**
@@ -36,15 +32,15 @@ public class PersonShiftDayDao extends DaoBase {
 
   /**
    * @return il personShiftDay relativo alla persona person nel caso in cui in data date fosse in
-   *     turno Null altrimenti.
+   * turno Null altrimenti.
    */
   public Optional<PersonShiftDay> getPersonShiftDay(Person person, LocalDate date) {
     final QPersonShiftDay personShiftDay = QPersonShiftDay.personShiftDay;
     final QPersonShift personShift = QPersonShift.personShift;
 
     JPQLQuery query = getQueryFactory().from(personShiftDay)
-            .join(personShiftDay.personShift, personShift)
-            .where(personShift.person.eq(person).and(personShiftDay.date.eq(date)));
+        .join(personShiftDay.personShift, personShift)
+        .where(personShift.person.eq(person).and(personShiftDay.date.eq(date)));
     PersonShiftDay psd = query.singleResult(personShiftDay);
 
     return Optional.fromNullable(psd);
@@ -52,12 +48,10 @@ public class PersonShiftDayDao extends DaoBase {
 
   /**
    * @return la lista dei personShiftDay presenti nel periodo compreso tra 'from' e 'to' aventi lo
-   *     shiftType 'type'.
+   * shiftType 'type'.
    *
-   * <p>
-   * PersonShiftDay.find("SELECT psd FROM PersonShiftDay psd WHERE date BETWEEN ? AND ? AND
-   * psd.shiftType = ? ORDER by date", firstOfMonth, lastOfMonth, shiftType).fetch();
-   * </p>
+   * <p> PersonShiftDay.find("SELECT psd FROM PersonShiftDay psd WHERE date BETWEEN ? AND ? AND
+   * psd.shiftType = ? ORDER by date", firstOfMonth, lastOfMonth, shiftType).fetch(); </p>
    */
   public List<PersonShiftDay> getPersonShiftDayByTypeAndPeriod(
       LocalDate from, LocalDate to, ShiftType type) {
@@ -65,22 +59,22 @@ public class PersonShiftDayDao extends DaoBase {
 
     JPQLQuery query =
         getQueryFactory().from(personShiftDay)
-          .where(personShiftDay.date.goe(from).and(personShiftDay.date.loe(to))
-            .and(personShiftDay.shiftType.eq(type))).orderBy(personShiftDay.date.asc());
+            .where(personShiftDay.date.goe(from).and(personShiftDay.date.loe(to))
+                .and(personShiftDay.shiftType.eq(type))).orderBy(personShiftDay.date.asc());
     return query.list(personShiftDay);
   }
 
   /**
    * @return il personShiftDay relativo al tipo 'shiftType' nel giorno 'date' con lo slot
-   *     'shiftSlot'.
+   * 'shiftSlot'.
    */
   public PersonShiftDay getPersonShiftDayByTypeDateAndSlot(
       ShiftType shiftType, LocalDate date, ShiftSlot shiftSlot) {
     final QPersonShiftDay personShiftDay = QPersonShiftDay.personShiftDay;
 
     JPQLQuery query = getQueryFactory().from(personShiftDay).where(personShiftDay.date.eq(date)
-            .and(personShiftDay.shiftType.eq(shiftType)
-                    .and(personShiftDay.shiftSlot.eq(shiftSlot))));
+        .and(personShiftDay.shiftType.eq(shiftType)
+            .and(personShiftDay.shiftSlot.eq(shiftSlot))));
     return query.singleResult(personShiftDay);
   }
 
@@ -93,9 +87,8 @@ public class PersonShiftDayDao extends DaoBase {
     JPQLQuery query = getQueryFactory().from(personShift).where(personShift.person.eq(person));
     return query.singleResult(personShift);
   }
-  
+
   /**
-   * 
    * @return la lista di tutti i PersonReperibilityType presenti sul db.
    */
   public List<ShiftCategories> getAllShiftType() {
@@ -104,4 +97,18 @@ public class PersonShiftDayDao extends DaoBase {
     return query.list(shift);
   }
 
+  public Optional<PersonShiftDay> byPersonAndDate(Person person, LocalDate date) {
+    final QPersonShiftDay shiftDay = QPersonShiftDay.personShiftDay;
+
+    return Optional.fromNullable(getQueryFactory().from(shiftDay)
+        .where(shiftDay.personShift.person.eq(person).and(shiftDay.date.eq(date)))
+        .singleResult(shiftDay));
+  }
+
+  public long countByPersonAndDate(Person person, LocalDate date) {
+
+    final QPersonShiftDay shiftDay = QPersonShiftDay.personShiftDay;
+    return getQueryFactory().from(shiftDay)
+        .where(shiftDay.personShift.person.eq(person).and(shiftDay.date.eq(date))).count();
+  }
 }
