d1e4d8ae5e6123b181cff8bd316cb7593190785d
dario <dario.tagliaferri@iit.cnr.it>
2017-06-15 11:09:13 +0200

    Aggiunte drools per utilizzo funzionalità per il supervisore dei turni

--- app/models/ShiftCategories.java
+++ app/models/ShiftCategories.java
@@ -3,14 +3,6 @@ package models;
 
 import com.google.common.collect.Lists;
 
-import models.base.BaseModel;
-
-import org.hibernate.envers.Audited;
-import org.hibernate.envers.NotAudited;
-
-import play.data.validation.Required;
-import play.data.validation.Unique;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,6 +14,16 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
+
+import models.base.BaseModel;
+
+import org.hibernate.envers.Audited;
+import org.hibernate.envers.NotAudited;
+
+import play.data.validation.Required;
+import play.data.validation.Unique;
+
+
 
 
 @Entity
