b2b6d091fe31e476a2fcd4e20ad8f95fc47bb8c5
Daniele Murgia <daniele.murgia@iit.cnr.it>
2017-07-05 16:56:36 +0200

    modificati controlli sui turni seguendo la nuova modellazione delle soglie

--- app/models/enumerate/ShiftTroubles.java
+++ app/models/enumerate/ShiftTroubles.java
@@ -1,13 +1,26 @@
 package models.enumerate;
 
+import com.google.common.collect.Lists;
+import java.util.List;
+
 public enum ShiftTroubles {
 
   //persona non assegnata all'attività
   PERSON_NOT_ASSIGNED,
-  //turno non completo ma prendibile con una decurtazione di un'ora
-  NOT_COMPLETED_SHIFT,
-  //timbratura fuori dalla tolleranza per ingresso/uscita
-  OUT_OF_STAMPING_TOLERANCE,
+  //timbratura fuori dalla tolleranza minima in ingresso (turno decurtato di 1 ora)
+  MIN_ENTRANCE_TOLERANCE_EXCEEDED,
+  //timbratura fuori dalla tolleranza massima in ingresso (turno non valido)
+  MAX_ENTRANCE_TOLERANCE_EXCEEDED,
+  //timbratura fuori dalla tolleranza minima in uscita (turno decurtato di 1 ora)
+  MIN_EXIT_TOLERANCE_EXCEEDED,
+  //timbratura fuori dalla tolleranza massima in uscita (turno non valido)
+  MAX_EXIT_TOLERANCE_EXCEEDED,
+  // pausa fatta durante il turno superiore a quella permessa (turno decurtato di 1 ora)
+  MIN_BREAK_TOLERANCE_EXCEEDED,
+  // pausa durante il turno superiore alla massima permessa (turno non valido)
+  MAX_BREAK_TOLERANCE_EXCEEDED,
+  // Superate più soglie di quelle permesse
+  TOO_MANY_EXCEEDED_THRESHOLDS,
   // non c'è abbastanza tempo a lavoro per avere il turno
   NOT_ENOUGH_WORKING_TIME,
   // problemi su altro slot
@@ -15,7 +28,36 @@ public enum ShiftTroubles {
   // turno incompleto a causa della mancanza di uno slot
   SHIFT_INCOMPLETED,
   // la persona è assente nel giorno
-  PERSON_IS_ABSENT,
-  // superato il tempo previsto per la pausa in turno
-  EXCEEDED_BREAKTIME
+  PERSON_IS_ABSENT;
+
+  /**
+   * @return la lista degli errori che sono specifici di un singolo turno (non include quelli
+   * derivanti dai controlli sul giorno di turno).
+   */
+  public static List<ShiftTroubles> shiftSpecific() {
+    return Lists.newArrayList(
+        MIN_ENTRANCE_TOLERANCE_EXCEEDED,
+        MAX_ENTRANCE_TOLERANCE_EXCEEDED,
+        MIN_EXIT_TOLERANCE_EXCEEDED,
+        MAX_EXIT_TOLERANCE_EXCEEDED,
+        MIN_BREAK_TOLERANCE_EXCEEDED,
+        MAX_BREAK_TOLERANCE_EXCEEDED,
+        TOO_MANY_EXCEEDED_THRESHOLDS,
+        NOT_ENOUGH_WORKING_TIME,
+        PERSON_IS_ABSENT);
+  }
+
+  /**
+   * @return la lista degli errori che invalidano il turno.
+   */
+  public static List<ShiftTroubles> invalidatingTroubles() {
+    return Lists.newArrayList(
+        MAX_ENTRANCE_TOLERANCE_EXCEEDED,
+        MAX_EXIT_TOLERANCE_EXCEEDED,
+        MAX_BREAK_TOLERANCE_EXCEEDED,
+        TOO_MANY_EXCEEDED_THRESHOLDS,
+        NOT_ENOUGH_WORKING_TIME,
+        PERSON_IS_ABSENT);
+  }
+
 }
