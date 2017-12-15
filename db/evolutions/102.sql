66739b39dbb16af957c97c002805a70b0aa8dbb3
dario <dario.tagliaferri@iit.cnr.it>
2017-12-15 11:16:46 +0100

    sistemata evoluzione e corretto controllo sull'identità dell'user che fa la richiesta di inserimento missione

--- db/evolutions/102.sql
+++ db/evolutions/102.sql
@@ -6,8 +6,8 @@ ALTER TABLE person_days	ADD COLUMN working_time_in_mission int;
 ALTER TABLE person_days_history	ADD COLUMN working_time_in_mission int;
 INSERT INTO users (id, expire_recovery_token, password, recovery_token, username, 
 disabled, expire_date, office_owner_id ) 
-select nextval('seq_users'), null, md5('missioni'), null, 'missioni',false, null, null;
-INSERT INTO user_roles select id,'MISSIONS_MANAGER' from users WHERE username = 'missioni';
+select nextval('seq_users'), null, md5('Doh5aule'), null, 'app.missioni',false, null, null;
+INSERT INTO user_roles select id,'MISSIONS_MANAGER' from users WHERE username = 'app.missioni';
 
 ALTER TABLE person_reperibility_days ALTER id SET DEFAULT nextval('seq_person_reperibility_days'::regclass);
 
@@ -18,5 +18,5 @@ ALTER TABLE absences DROP COLUMN external_identifier;
 ALTER TABLE person_days_history DROP COLUMN working_time_in_mission;
 ALTER TABLE person_days DROP COLUMN working_time_in_mission;
 DELETE FROM user_roles where roles = 'MISSIONS_MANAGER';
-DELETE FROM users where username = 'missioni';
+DELETE FROM users where username = 'app.missioni';
 
