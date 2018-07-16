9867d2d88355d50a9ee661b62bbd235db2e7e5bb
Alessandro Martelli <alessandro.martelli@iit.cnr.it>
2018-07-16 12:58:32 +0200

    Aggiunto campo noOvertime all'absenceType.

--- db/evolutions/116.sql
+++ db/evolutions/116.sql
@@ -2,18 +2,22 @@
 
 ALTER TABLE absence_types ADD COLUMN minimum_time INT;
 ALTER TABLE absence_types ADD COLUMN percentage_time INT;
+ALTER TABLE absence_types ADD COLUMN no_overtime BOOLEAN DEFAULT FALSE;
 
 ALTER TABLE absence_types_history ADD COLUMN minimum_time INT;
 ALTER TABLE absence_types_history ADD COLUMN percentage_time INT;
+ALTER TABLE absence_types_history ADD COLUMN no_overtime BOOLEAN DEFAULT FALSE;
 
 UPDATE absence_types SET code = '661MO' where code = '661M';
 UPDATE absence_types_history SET code = '661MO' where code = '661M';
 
 # ---!Downs
 
+ALTER TABLE absence_types_history DROP COLUMN no_overtime;
 ALTER TABLE absence_types_history DROP COLUMN minimum_time;
 ALTER TABLE absence_types_history DROP COLUMN percentage_time;
 
+ALTER TABLE absence_types DROP COLUMN no_overtime;
 ALTER TABLE absence_types DROP COLUMN minimum_time;
 ALTER TABLE absence_types DROP COLUMN percentage_time;
 
