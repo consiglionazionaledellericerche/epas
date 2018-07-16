b452f9ff0c5cb083af3c4e108f26db010c45f6bc
Alessandro Martelli <alessandro.martelli@iit.cnr.it>
2018-07-16 12:03:17 +0200

    Aggiunti nuovi campi all'enumerato ed aggiornati i mapper. Rinominato 661M a 661MO (old). Aggiornati tutti i vecchi riferimenti a quell'enumerato.

--- db/evolutions/116.sql
+++ db/evolutions/116.sql
@@ -1,21 +1,21 @@
 # ---!Ups
 
-ALTER TABLE absences ADD COLUMN minimum_time INT;
-ALTER TABLE absences ADD COLUMN percentage_time INT;
+ALTER TABLE absence_types ADD COLUMN minimum_time INT;
+ALTER TABLE absence_types ADD COLUMN percentage_time INT;
 
-ALTER TABLE absences_history ADD COLUMN minimum_time DATE;
-ALTER TABLE absences_history ADD COLUMN percentage_time INT;
+ALTER TABLE absence_types_history ADD COLUMN minimum_time INT;
+ALTER TABLE absence_types_history ADD COLUMN percentage_time INT;
 
 UPDATE absence_types SET code = '661MO' where code = '661M';
 UPDATE absence_types_history SET code = '661MO' where code = '661M';
 
 # ---!Downs
 
-ALTER TABLE absences_history DROP COLUMN minimum_time;
-ALTER TABLE absences_history DROP COLUMN percentage_time;
+ALTER TABLE absence_types_history DROP COLUMN minimum_time;
+ALTER TABLE absence_types_history DROP COLUMN percentage_time;
 
-ALTER TABLE absences DROP COLUMN minimum_time;
-ALTER TABLE absences DROP COLUMN percentage_time;
+ALTER TABLE absence_types DROP COLUMN minimum_time;
+ALTER TABLE absence_types DROP COLUMN percentage_time;
 
 UPDATE absence_types SET code = '661M' where code = '661MO';
 UPDATE absence_types_history SET code = '661M' where code = '661MO';
