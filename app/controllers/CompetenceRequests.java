79ec821d02bc1a1285059d05155e6b88e2708299
dario <dario@dario-pc.iit.cnr.it>
2020-01-10 17:12:50 +0100

    Aggiunti metodi di visualizzazione lista e lista da approvare di competenze

--- app/controllers/CompetenceRequests.java
+++ app/controllers/CompetenceRequests.java
@@ -1,16 +1,24 @@
 package controllers;
 
+import java.util.List;
 import javax.inject.Inject;
 import org.joda.time.LocalDateTime;
+import com.google.common.base.Optional;
 import com.google.common.base.Verify;
+import dao.CompetenceRequestDao;
 import dao.GroupDao;
 import dao.PersonDao;
+import dao.UsersRolesOfficesDao;
 import dao.wrapper.IWrapperFactory;
 import lombok.val;
 import lombok.extern.slf4j.Slf4j;
 import manager.NotificationManager;
 import manager.configurations.ConfigurationManager;
 import manager.flows.CompetenceRequestManager;
+import models.UsersRolesOffices;
+import models.flows.AbsenceRequest;
+import models.flows.CompetenceRequest;
+import models.flows.Group;
 import models.flows.enumerate.AbsenceRequestType;
 import models.flows.enumerate.CompetenceRequestType;
 import play.mvc.Controller;
@@ -41,9 +49,19 @@ public class CompetenceRequests extends Controller{
   static ConfigurationManager configurationManager;
   @Inject
   static IWrapperFactory wrapperFactory;
+  @Inject
+  static CompetenceRequestDao competenceRequestDao;
+  @Inject
+  static UsersRolesOfficesDao uroDao;
+  
+  
   
   public static void overtimes() {
     list(CompetenceRequestType.OVERTIME_REQUEST);
+  }
+  
+  public static void overtimesToApprove() {
+    listToApprove(CompetenceRequestType.OVERTIME_REQUEST);
   }
   
   /**
@@ -66,5 +84,41 @@ public class CompetenceRequests extends Controller{
         type, person, fromDate);
     
     val config = competenceRequestManager.getConfiguration(type, person);
+    List<CompetenceRequest> myResults = competenceRequestDao
+        .findByPersonAndDate(person, fromDate, Optional.absent(), type, true);
+    List<CompetenceRequest> closed = competenceRequestDao
+        .findByPersonAndDate(person, fromDate, Optional.absent(), type, false);
+    val onlyOwn = true;
+    boolean persist = false;
+
+    render(config, myResults, type, onlyOwn, persist, closed);
+  }
+  
+  /**
+   * Lista delle richieste di assenza da approvare da parte dell'utente corrente.
+   *
+   * @param type tipo opzionale di tipo di richiesta di assenza.
+   */
+  public static void listToApprove(CompetenceRequestType type) {
+    Verify.verifyNotNull(type);
+
+    val person = Security.getUser().get().person;
+    val fromDate = LocalDateTime.now().dayOfYear().withMinimumValue();
+    log.debug("Prelevo le richieste da approvare di assenze di tipo {} a partire da {}",
+        type, fromDate);
+    List<Group> groups = groupDao.groupsByOffice(person.office, Optional.absent());
+    List<UsersRolesOffices> roleList = uroDao.getUsersRolesOfficesByUser(person.user);
+    List<CompetenceRequest> results = competenceRequestDao
+        .allResults(roleList, fromDate, Optional.absent(), type, groups, person);
+    List<CompetenceRequest> myResults =
+        competenceRequestDao.toApproveResults(roleList, fromDate, Optional.absent(),
+            type, groups, person);
+    List<CompetenceRequest> approvedResults =
+        competenceRequestDao
+            .totallyApproved(roleList, fromDate, Optional.absent(), type, groups, person);
+    val config = competenceRequestManager.getConfiguration(type, person);
+    val onlyOwn = false;
+
+    render(config, results, type, onlyOwn, approvedResults, myResults);
   }
 }
--- app/dao/CompetenceRequestDao.java
+++ app/dao/CompetenceRequestDao.java
@@ -1,9 +1,21 @@
 package dao;
 
+import java.util.List;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
+import org.joda.time.LocalDateTime;
+import com.google.common.base.Optional;
+import com.google.common.base.Preconditions;
 import com.google.inject.Provider;
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.jpa.JPQLQueryFactory;
+import models.Person;
+import models.flows.AbsenceRequest;
+import models.flows.CompetenceRequest;
+import models.flows.enumerate.AbsenceRequestType;
+import models.flows.enumerate.CompetenceRequestType;
+import models.flows.query.QAbsenceRequest;
+import models.flows.query.QCompetenceRequest;
 
 public class CompetenceRequestDao extends DaoBase {
 
@@ -11,4 +23,38 @@ public class CompetenceRequestDao extends DaoBase {
   CompetenceRequestDao(JPQLQueryFactory queryFactory, Provider<EntityManager> emp) {
     super(queryFactory, emp);
   }
+  
+  /**
+   * Lista delle richiesta di assenza per persona e data.
+   *
+   * @param person La persona della quale recuperare le richieste di assenza
+   * @param fromDate La data iniziale dell'intervallo temporale da considerare
+   * @param toDate La data finale dell'intervallo temporale da considerare (opzionale)
+   * @param absenceRequestType Il tipo di richiesta di assenza specifico
+   * @return La lista delle richieste di assenze sull'intervallo e la persona specificati.
+   */
+  public List<CompetenceRequest> findByPersonAndDate(Person person,
+      LocalDateTime fromDate, Optional<LocalDateTime> toDate,
+      CompetenceRequestType competenceRequestType, boolean active) {
+
+    Preconditions.checkNotNull(person);
+    Preconditions.checkNotNull(fromDate);
+
+    final QCompetenceRequest competenceRequest = QCompetenceRequest.competenceRequest;
+
+    BooleanBuilder conditions = new BooleanBuilder(competenceRequest.person.eq(person)
+        .and(competenceRequest.startAt.after(fromDate))
+        .and(competenceRequest.type.eq(competenceRequestType)));
+    if (toDate.isPresent()) {
+      conditions.and(competenceRequest.endTo.before(toDate.get()));
+    }
+    if (active) {
+      conditions.and(competenceRequest.flowEnded.eq(false));
+    } else {
+      conditions.and(competenceRequest.flowEnded.eq(true));
+    }
+    return getQueryFactory().selectFrom(competenceRequest)
+        .where(conditions).orderBy(competenceRequest.startAt.desc()).fetch();
+  }
+
 }
