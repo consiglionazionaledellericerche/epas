1876db9100a2bde11ee2cac89982feaea70b3184
dario <dario@dario-pc.iit.cnr.it>
2020-02-27 14:58:38 +0100

    Cambiato comportamento del codice oltre a modificare la descrizione dello stesso

--- app/models/absences/definitions/DefaultAbsenceType.java
+++ app/models/absences/definitions/DefaultAbsenceType.java
@@ -19,8 +19,8 @@ import org.joda.time.LocalDate;
  */
 public enum DefaultAbsenceType {
   
-  A_covid19("covid19", "Permesso per gestione corona virus",
-      false, ImmutableSet.of(JustifiedTypeName.all_day), 0, false, false,
+  A_COVID19("COVID19", "Emergenza coronavirus, attività lavorativa presso il domicilio dei dipendenti",
+      false, ImmutableSet.of(JustifiedTypeName.assign_all_day), 0, false, false,
       0, null, Sets.newHashSet(), null, null, false),
 
   A_18M("18M", "Permesso assistenza parenti/affini disabili L. 104/92 in ore e minuti",
