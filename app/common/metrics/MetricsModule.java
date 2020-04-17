aaeaf726f72e3fbbadd67d9efd46114fe51f4f26
Cristian Lucchesi <cristian.lucchesi@iit.cnr.it>
2020-04-17 16:37:40 +0200

    Introduzione delle metriche sui thread.

--- app/common/metrics/MetricsModule.java
+++ app/common/metrics/MetricsModule.java
@@ -1,5 +1,6 @@
 package common.metrics;
 
+import com.google.common.base.Splitter;
 import com.google.common.collect.ImmutableList;
 import com.google.inject.AbstractModule;
 import com.google.inject.Provider;
@@ -9,10 +10,12 @@ import com.google.inject.multibindings.Multibinder;
 import injection.AutoRegister;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.binder.MeterBinder;
+import io.micrometer.core.instrument.binder.db.PostgreSQLDatabaseMetrics;
 import io.micrometer.core.instrument.binder.jpa.HibernateMetrics;
 import io.micrometer.core.instrument.binder.jvm.ClassLoaderMetrics;
 import io.micrometer.core.instrument.binder.jvm.JvmGcMetrics;
 import io.micrometer.core.instrument.binder.jvm.JvmMemoryMetrics;
+import io.micrometer.core.instrument.binder.jvm.JvmThreadMetrics;
 import io.micrometer.core.instrument.binder.system.FileDescriptorMetrics;
 import io.micrometer.core.instrument.binder.system.ProcessorMetrics;
 import io.micrometer.core.instrument.binder.system.UptimeMetrics;
@@ -22,8 +25,10 @@ import io.micrometer.prometheus.PrometheusMeterRegistry;
 import java.time.Duration;
 import java.util.Optional;
 import javax.persistence.EntityManager;
+import javax.sql.DataSource;
 import lombok.val;
 import org.hibernate.SessionFactory;
+import org.hibernate.engine.jdbc.connections.spi.ConnectionProvider;
 import play.Play;
 import play.db.jpa.JPA;
 
@@ -71,6 +76,24 @@ public class MetricsModule extends AbstractModule {
           JPA.DEFAULT, ImmutableList.of());
   }
 
+  public DataSource getDataSource(Provider<EntityManager> emp) {
+    val entityManagerFactory = emp.get().getEntityManagerFactory();
+    ConnectionProvider cp = ((SessionFactory) entityManagerFactory).getSessionFactoryOptions()
+          .getServiceRegistry()
+          .getService(ConnectionProvider.class);
+  return cp.unwrap(DataSource.class);
+  }
+  
+  public String getDatabaseName() {
+   val databaseUrl = Splitter.on("/").splitToList(Play.configuration.getProperty("db"));
+   return databaseUrl.get(databaseUrl.size() - 1);
+  }
+  
+  @Provides
+  public PostgreSQLDatabaseMetrics postgresqlMetrics(Provider<EntityManager> emp) {
+    return new PostgreSQLDatabaseMetrics(getDataSource(emp), getDatabaseName());
+  }
+  
   @Provides
   @Singleton
   public IMinDurationCheck checker() {
@@ -88,7 +111,10 @@ public class MetricsModule extends AbstractModule {
     meterBinder.addBinding().to(HibernateMetrics.class);
     meterBinder.addBinding().to(JvmGcMetrics.class);
     meterBinder.addBinding().to(JvmMemoryMetrics.class);
+    meterBinder.addBinding().to(JvmThreadMetrics.class);
     meterBinder.addBinding().to(ProcessorMetrics.class);
+    //XXX: servono queste metriche?
+    //meterBinder.addBinding().to(PostgreSQLDatabaseMetrics.class);
     meterBinder.addBinding().to(UptimeMetrics.class);
   }
 }
