96ea7ea77a8a2f30dda0ec95747e4fb75c97497d
dario <dario@darietto-pc>
2020-04-28 19:28:34 +0200

    Aggiunto metodo che ritorna un json contenente la mappa dei giorni di un anno/mese con le loro informazioni prese dai personDay relativi. Aggiunte le drools per consentirne l'utilizzo

--- app/controllers/rest/v2/PersonDays.java
+++ app/controllers/rest/v2/PersonDays.java
@@ -3,20 +3,30 @@ package controllers.rest.v2;
 import cnr.sync.dto.v2.AbsenceDto;
 import cnr.sync.dto.v2.PersonDayDto;
 import cnr.sync.dto.v2.StampingDto;
+import com.beust.jcommander.internal.Lists;
+import com.beust.jcommander.internal.Maps;
+import com.beust.jcommander.internal.Sets;
 import com.google.common.base.Optional;
 import controllers.Resecure;
 import controllers.Resecure.BasicAuth;
+import dao.OfficeDao;
 import dao.PersonDao;
 import dao.PersonDayDao;
 import dao.wrapper.IWrapperFactory;
 import helpers.JsonResponse;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
+import models.Office;
 import models.Person;
 import models.PersonDay;
 import models.Stamping;
 import models.absences.Absence;
 import org.joda.time.LocalDate;
+import org.joda.time.YearMonth;
+import org.joda.time.format.DateTimeFormatter;
 import play.mvc.Controller;
 import play.mvc.With;
 import security.SecurityRules;
@@ -33,6 +43,8 @@ public class PersonDays extends Controller {
   static IWrapperFactory wrapperFactory;
   @Inject
   private static SecurityRules rules;
+  @Inject
+  private static OfficeDao officeDao;
 
   /**
    * Metodo rest che ritorna la situazione della persona (passata per email o eppn) in un giorno 
@@ -61,8 +73,56 @@ public class PersonDays extends Controller {
       JsonResponse.notFound("Non sono presenti informazioni per "
               + person.get().name + " " + person.get().surname + " nel giorno " + date);
     }
-    PersonDayDto pdDTO = generateDayDTO(pd);
-    renderJSON(pdDTO);
+    PersonDayDto pdDto = generateDayDto(pd);
+    renderJSON(pdDto);
+  }
+  
+  /**
+   * Metodo rest che ritorna un json contenente la lista dei person day di tutti i dipendenti
+   * di una certa sede nell'anno/mese passati come parametro.
+   * @param sedeId l'identificativo della sede di cui ricercare la situazione delle persone
+   * @param year l'anno di riferimento
+   * @param month il mese di riferimento
+   */
+  @BasicAuth
+  public static void getDaySituationByOffice(String sedeId, Integer year, Integer month) {
+    log.debug("getDaySituationByOffice -> sedeId={}, year={}, month={}", sedeId, year, month);
+    if ((year == null && month == null) || sedeId == null) {
+      notFound();
+    }
+    Optional<Office> office = officeDao.byCodeId(sedeId);
+    if (!office.isPresent()) {
+      notFound();
+    }
+    rules.checkIfPermitted(office.get());
+    Set<Office> offices = Sets.newHashSet();
+    offices.add(office.get());
+    List<Person> personList = personDao.getActivePersonInMonth(offices, new YearMonth(year, month));
+    Map<Person, List<PersonDayDto>> map = Maps.newHashMap();
+    for (Person person : personList) {
+      List<PersonDayDto> list = Lists.newArrayList();
+      LocalDate date = new LocalDate(year, month, 1);
+      LocalDate until = date.dayOfMonth().withMaximumValue();
+      while (!date.isAfter(until)) {
+        PersonDay pd = personDayDao.getPersonDay(person, date).orNull();
+        if (pd == null) {
+          JsonResponse.notFound("Non sono presenti informazioni per "
+                  + person.name + " " + person.surname + " nel giorno " + date);
+        }
+        PersonDayDto pdDto = generateDayDto(pd);
+        
+        if (map.containsKey(person)) {
+          list = map.get(person);          
+        } 
+        list.add(pdDto);
+        map.put(person, list);
+        date = date.plusDays(1);
+      }
+      
+    }
+    log.debug("Terminato invio di informazioni della sede {} per l'anno {} mese {}", 
+        office.get().name, year, month);
+    renderJSON(map);
   }
 
   /**
@@ -70,9 +130,10 @@ public class PersonDays extends Controller {
    * @return il personDayDTO costruito sulla base del personDay passato come 
    *     parametro da ritornare alle funzioni rest.
    */
-  private static PersonDayDto generateDayDTO(PersonDay pd) {
+  private static PersonDayDto generateDayDto(PersonDay pd) {
     PersonDayDto pdDto = 
         PersonDayDto.builder()
+          .data(pd.date.toString())
           .buonoPasto(pd.isTicketAvailable)
           .differenza(pd.difference)
           .progressivo(pd.progressive)
