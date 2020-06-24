a0e06b44fa2eaab68b33adc8781b5ec318fbde8d
dario <dario@darietto-pc>
2020-06-24 17:34:13 +0200

    Aggiunti i campi di indirizzo, username e password per collegarsi all'applicazione che gestisce le timbrature in telelavoro. Creata classe in cui vengono inseriti i metodi di comunicazione con l'applicazione TeleworkStampings. Creato dto per le comunicazioni tra ePAS e TeleworkStampings

---
+++ app/models/dto/TeleworkDto.java
@@ -0,0 +1,15 @@
+package models.dto;
+
+import java.time.LocalDateTime;
+
+public class TeleworkDto {
+
+  private Long personDayId;
+  
+  private String stampType;
+  
+  private LocalDateTime date;
+  
+  private String note;
+  
+}
