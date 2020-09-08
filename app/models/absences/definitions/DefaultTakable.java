144d5b601ab3cc49a38b46c815bc52badf26c2fe
dario <dario@darietto-pc>
2020-09-08 17:46:59 +0200

    Rimosso il 91CE dalla disponibilità dei codici inseribili

--- app/models/absences/definitions/DefaultTakable.java
+++ app/models/absences/definitions/DefaultTakable.java
@@ -178,10 +178,10 @@ public enum DefaultTakable {
       ImmutableSet.of(DefaultAbsenceType.A_91F), 
       -1, null),
 
-  T_RIPOSI_CHIUSURA_ENTE(AmountType.units,
-      ImmutableSet.of(DefaultAbsenceType.A_91CE),
-      ImmutableSet.of(DefaultAbsenceType.A_91CE),
-      -1, null),
+//  T_RIPOSI_CHIUSURA_ENTE(AmountType.units,
+//      ImmutableSet.of(DefaultAbsenceType.A_91CE),
+//      ImmutableSet.of(DefaultAbsenceType.A_91CE),
+//      -1, null),
 
   T_LAVORO_FUORI_SEDE(AmountType.units, 
       ImmutableSet.of(DefaultAbsenceType.A_105BP), 
@@ -624,9 +624,9 @@ public enum DefaultTakable {
 
   T_RIPOSI_CNR_ATTESTATI(AmountType.units, 
       ImmutableSet.of(
-          DefaultAbsenceType.A_91, DefaultAbsenceType.A_91F, DefaultAbsenceType.A_91CE), 
+          DefaultAbsenceType.A_91, DefaultAbsenceType.A_91F/*, DefaultAbsenceType.A_91CE*/), 
       ImmutableSet.of(
-          DefaultAbsenceType.A_91, DefaultAbsenceType.A_91F, DefaultAbsenceType.A_91CE), 
+          DefaultAbsenceType.A_91, DefaultAbsenceType.A_91F/*, DefaultAbsenceType.A_91CE*/), 
       -1, null);
 
 
