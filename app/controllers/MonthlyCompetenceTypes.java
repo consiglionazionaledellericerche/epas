b075361792c623416a2cd52362d1828365e033ec
dario <dario@darietto-pc>
2020-09-24 17:55:29 +0200

    Completato l'inserimento dei monthlycompetencetyes

--- app/controllers/MonthlyCompetenceTypes.java
+++ app/controllers/MonthlyCompetenceTypes.java
@@ -1,12 +1,14 @@
 package controllers;
 
+import dao.CompetenceCodeDao;
+import helpers.Web;
 import java.util.List;
 import javax.inject.Inject;
-import org.joda.time.LocalDate;
-import dao.CompetenceCodeDao;
 import lombok.extern.slf4j.Slf4j;
 import models.CompetenceCode;
 import models.MonthlyCompetenceType;
+import org.joda.time.LocalDate;
+import play.data.validation.Validation;
 import play.mvc.Controller;
 import play.mvc.With;
 
@@ -18,7 +20,7 @@ public class MonthlyCompetenceTypes extends Controller {
   private static CompetenceCodeDao competenceCodeDao;
 
   /**
-   * 
+   * Genera la form di creazione di un monthlyCompetenceType.
    */
   public static void insertMonthlyCompetenceType() {
     
@@ -29,10 +31,20 @@ public class MonthlyCompetenceTypes extends Controller {
   }
   
   /**
-   * 
-   * @param monthlyCompetencetype
+   * Persiste il monthlyCompetenceType.
+   * @param monthlyCompetencetype l'oggetto da persistere
    */
   public static void saveType(MonthlyCompetenceType monthlyCompetencetype) {
+    
+    if (Validation.hasErrors()) {
+      response.status = 400;
+      flash.error(Web.msgHasErrors());
+      render("@insertMonthlyCompetenceType", monthlyCompetencetype);      
+    }
+    
     monthlyCompetencetype.save();
+    flash.success(String.format("Codice %s aggiunto con successo", monthlyCompetencetype.name));
+
+    Competences.manageCompetenceCode();
   }
 }
