075701a04897bcd98e8164c275749bd682385d90
dario <dario@darietto-pc>
2021-01-19 17:49:18 +0100

    Aggiunto lo storico separato per contratto ed inizializzazione. Nel controller viene fatta la distinzione di cosa ritornare sulla base della tipologia di storico richiesta (contratto/inizializzazione).

--- app/controllers/Historicals.java
+++ app/controllers/Historicals.java
@@ -1,13 +1,14 @@
 package controllers;
 
 import com.google.common.collect.Lists;
-import dao.history.HistoricalDao;
 import dao.history.ContractHistoryDao;
+import dao.history.HistoricalDao;
 import dao.history.HistoryValue;
 import java.util.List;
 import javax.inject.Inject;
-import lombok.val;
 import lombok.extern.slf4j.Slf4j;
+import lombok.val;
+import manager.HistoricalManager;
 import models.Competence;
 import models.Contract;
 import models.enumerate.HistorycalType;
@@ -20,8 +21,15 @@ public class Historicals extends Controller {
   
   @Inject
   static ContractHistoryDao contractHistoryDao;
+  @Inject
   static HistoricalDao historicalDao;
+  @Inject
+  static HistoricalManager historicalManager;
 
+  /**
+   * Ritorna lo storico sulle modifiche ad una competenza.
+   * @param competenceId l'id della competenza di cui cercare lo storico
+   */
   public static void competenceHistory(long competenceId) {
     boolean found = false;
     final Competence competence = Competence.findById(competenceId);
@@ -36,6 +44,11 @@ public class Historicals extends Controller {
     render(historyCompetence, competence, found);
   }
   
+  /**
+   * Ritorna lo storico del contratto filtrato per tipologia: contratto/inizializzazione.
+   * @param contractId l'id del contratto di cui cercare lo storico
+   * @param type la tipologia di informazioni che si richiedono: contratto/inizializzazione
+   */
   public static void contractHistory(long contractId, HistorycalType type) {
     boolean found = false;
     final Contract contract = Contract.findById(contractId);
@@ -49,27 +62,39 @@ public class Historicals extends Controller {
     Contract temp = contract;
     if (type.equals(HistorycalType.CONTRACT)) {
       for (HistoryValue<Contract> story : historyContract) {
-        //val con = historicalDao.valueAtRevision(Contract.class, contractId, story.revision.id);
+        
         Contract con = story.value;
         log.info("Contract id = {}, revision = {}, perseoId = {}, "
             + "beginDate = {}, endDate = {}, endContract = {}", 
             contract.id, story.revision.id, con.perseoId, con.beginDate, con.endDate, 
-            con.endContract);        
-        if (!temp.beginDate.isEqual(con.beginDate) 
-//            || !temp.endDate.equals(con.endDate)
-//            || !temp.endContract.equals(con.endContract) || temp.onCertificate != con.onCertificate
-//            || temp.getPreviousContract().equals(con.getPreviousContract())
-            ) {
+            con.endContract);  
+        if (con.beginDate == null && con.endDate == null && con.endContract == null) {
+          log.debug("Storico con dati incompleti per {}", contract.person.getFullname());
+          continue;
+        }
+        if (!historicalManager.checkDates(temp.beginDate, con.beginDate)
+            || !historicalManager.checkDates(temp.endDate, con.endDate)
+            || !historicalManager.checkDates(temp.endContract, con.endContract) 
+            || temp.onCertificate != con.onCertificate
+            || !historicalManager.checkObjects(temp, con)) {
           contractModifications.add(story);
         }
         temp = con;      
       }
       render(contractModifications, contract, found);
     } else {
-      //Per l'inizializzazioni...
-    }
+      for (HistoryValue<Contract> story : historyContract) {
+        Contract con = story.value;
+        if (!historicalManager.checkDates(temp.sourceDateResidual, con.sourceDateResidual)
+            || temp.sourceRemainingMinutesLastYear != con.sourceRemainingMinutesLastYear
+            || temp.sourceRemainingMinutesCurrentYear != con.sourceRemainingMinutesCurrentYear) {
+          
+          initializationModifications.add(story);
+        }
+        temp = con;
+      }
+      render("@initializationHistory", initializationModifications, contract, found);
+    }    
     
-    
-    render(historyContract, contract, found);
   }
 }
