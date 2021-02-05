661fee3170f8fd426547906b0213fa7673e1942e
dario <dario@darietto-pc>
2021-02-05 16:33:24 +0100

    Aggiunta la possibilità di auto inserirsi le assenze per secondo parente disabile previa abilitazione da configurazione personale Aggiunte le regole drools per questo permesso e per quello per congedo parentale

--- app/manager/configurations/EpasParam.java
+++ app/manager/configurations/EpasParam.java
@@ -100,6 +100,15 @@ public enum EpasParam {
       EpasParamValueType.formatValue(false),
       Lists.<RecomputationType>newArrayList(),
       Person.class),
+  
+  SECOND_DISABLED_RELATIVE_PERMISSION("second_disabled_relative_permission",
+
+      EpasParamCategory.GENERAL,
+      EpasParamTimeType.GENERAL,
+      EpasParamValueType.BOOLEAN,
+      EpasParamValueType.formatValue(false),
+      Lists.<RecomputationType>newArrayList(),
+      Person.class),
 
   OFF_SITE_ABSENCE_WITH_CONVENTION("off_site_absence_with_convention",
 
@@ -145,6 +154,8 @@ public enum EpasParam {
       EpasParamValueType.formatValue(false),
       Lists.<RecomputationType>newArrayList(),
       Person.class),
+  
+
 
 
   DAY_OF_PATRON("dayOfPatron",
