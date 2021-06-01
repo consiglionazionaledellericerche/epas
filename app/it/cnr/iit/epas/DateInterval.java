e917151743dc58bc665647d967a16199b273f2ab
dario <dario@darietto-pc>
2021-06-01 15:00:45 +0200

    Aggiunti nuovi parametri e sistemato ordinamento importazioni in alcune classi

--- app/it/cnr/iit/epas/DateInterval.java
+++ app/it/cnr/iit/epas/DateInterval.java
@@ -19,8 +19,8 @@ package it.cnr.iit.epas;
 
 import com.google.common.base.Optional;
 import com.google.common.base.Preconditions;
-import lombok.NoArgsConstructor;
 import org.joda.time.LocalDate;
+import lombok.NoArgsConstructor;
 
 /**
  * Rappresenta un intervallo di date.
