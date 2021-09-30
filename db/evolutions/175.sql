559382bcbbddb1246257593c324340682fae94d8
darietto1983 <dario.tagliaferri@yahoo.it>
2021-09-30 13:27:09 +0200

    Modificata evoluzione con impostazione del valore del nuovo parametro per i blocchetti esistenti

--- db/evolutions/175.sql
+++ db/evolutions/175.sql
@@ -3,6 +3,8 @@
 ALTER TABLE meal_ticket ADD COLUMN block_type TEXT;
 ALTER TABLE meal_ticket_history ADD COLUMN block_type TEXT;
 
+UPDATE meal_ticket SET block_type = 'papery';
+
 # --- !Downs
 
 ALTER TABLE meal_ticket_history DROP COLUMN block_type;
