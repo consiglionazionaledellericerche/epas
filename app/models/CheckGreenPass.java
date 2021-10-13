02957b0019459a9f0a4910bc0ce4bb05be476563
dario <dario@dario-pc.iit.cnr.it>
2021-10-13 12:21:26 +0200

    Aggiunta evoluzione e nuova classe con relazione

---
+++ app/models/CheckGreenPass.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2021  Consiglio Nazionale delle Ricerche
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Affero General Public License as
+ *     published by the Free Software Foundation, either version 3 of the
+ *     License, or (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Affero General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Affero General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package models;
+
+import java.time.LocalDate;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import models.base.BaseModel;
+import org.hibernate.envers.Audited;
+import play.data.validation.Required;
+
+@Entity
+@Audited
+public class CheckGreenPass extends BaseModel {
+  
+  private static final long serialVersionUID = 4909012051833782360L;
+
+  public LocalDate checkDate;
+  
+  @Required
+  @ManyToOne(optional = false)
+  @JoinColumn(name = "person_id", nullable = false)
+  public Person person;
+  
+  public boolean checked;
+}
