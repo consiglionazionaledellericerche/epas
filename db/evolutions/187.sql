85365c459f19dcbe9fde16c8ae987bc92870c2e4
Cristian Lucchesi <cristian.lucchesi@iit.cnr.it>
2022-04-13 12:39:40 +0200

    Corretta svista nella down dell'evoluzione 187.

--- db/evolutions/187.sql
+++ db/evolutions/187.sql
@@ -6,4 +6,4 @@ ALTER TABLE meal_ticket_history DROP COLUMN quarter;
 # --- !Downs
 
 ALTER TABLE meal_ticket ADD COLUMN quarter INTEGER;
-ALTER TABLE meal_ticket_history ADD COLUMN INTEGER;
+ALTER TABLE meal_ticket_history ADD COLUMN quarter INTEGER;
