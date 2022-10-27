9454503086b35f3cbd3fc30515795b00088bd274
dario <dario@dario-pc.iit.cnr.it>
2022-10-27 11:44:56 +0200

    Creati e aggiornati i qmodel delle classi del modello

--- app/models/MealTicketCard.java
+++ app/models/MealTicketCard.java
@@ -1,20 +1,43 @@
 package models;
 
 import com.google.common.collect.Lists;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Set;
+import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import models.absences.InitializationGroup;
 import models.base.PeriodModel;
+import models.flows.Affiliation;
+import models.flows.Group;
+import org.hibernate.envers.Audited;
+import org.joda.time.LocalDate;
 import play.data.validation.Required;
 
-public class MealTicketCard extends PeriodModel{
+/**
+ * Nuova classe che implementa le card dei buoni elettronici.
+ *
+ * @author dario
+ *
+ */
+@Getter
+@Setter
+@Entity
+@Audited
+@Table(name = "meal_ticket_card")
+public class MealTicketCard extends PeriodModel {
   
-  public int number;
+  private int number;
 
   @ManyToOne
   @Required
-  public Person person;
+  private Person person;
   
   @OneToMany(mappedBy = "mealTicketCard")
-  public List<MealTicket> mealTickets = Lists.newArrayList();
+  private List<MealTicket> mealTickets = Lists.newArrayList();
 }
