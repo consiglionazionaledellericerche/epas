67c1ccb31f76f11da2c17904fc612ac6f6028b1f
dario <dario@dario-XPS-8960>
2024-05-22 16:44:59 +0200

    Modificata la quantità di minuti per pausa pranzo.

--- db/evolutions/215.sql
+++ db/evolutions/215.sql
@@ -6,31 +6,31 @@ VALUES ('Maternità CNR', false, true, null, false, true, 0, false, null, now())
 
 INSERT INTO working_time_type_days (break_ticket_time, day_of_week, holiday, meal_ticket_time, time_meal_from, time_meal_to, time_slot_entrance_from,time_slot_entrance_to, 
 time_slot_exit_from, time_slot_exit_to, working_time, working_time_type_id, ticket_afternoon_threshold, ticket_afternoon_working_time, version, updated_at)
-VALUES (0, 1, false, 240, 0, 0, 0, 0, 0, 0, 312, (select id from working_time_types where description = 'Maternità CNR'), 0, 0, 0, now());
+VALUES (30, 1, false, 240, 0, 0, 0, 0, 0, 0, 312, (select id from working_time_types where description = 'Maternità CNR'), 0, 0, 0, now());
 
 INSERT INTO working_time_type_days (break_ticket_time, day_of_week, holiday, meal_ticket_time, time_meal_from, time_meal_to, time_slot_entrance_from,time_slot_entrance_to, 
 time_slot_exit_from, time_slot_exit_to, working_time, working_time_type_id, ticket_afternoon_threshold, ticket_afternoon_working_time, version, updated_at)
-VALUES (0, 2, false, 240, 0, 0, 0, 0, 0, 0, 312, (select id from working_time_types where description = 'Maternità CNR'), 0, 0, 0, now());
+VALUES (30, 2, false, 240, 0, 0, 0, 0, 0, 0, 312, (select id from working_time_types where description = 'Maternità CNR'), 0, 0, 0, now());
 
 INSERT INTO working_time_type_days (break_ticket_time, day_of_week, holiday, meal_ticket_time, time_meal_from, time_meal_to, time_slot_entrance_from,time_slot_entrance_to, 
 time_slot_exit_from, time_slot_exit_to, working_time, working_time_type_id, ticket_afternoon_threshold, ticket_afternoon_working_time, version, updated_at)
-VALUES (0, 3, false, 240, 0, 0, 0, 0, 0, 0, 312, (select id from working_time_types where description = 'Maternità CNR'), 0, 0, 0, now());
+VALUES (30, 3, false, 240, 0, 0, 0, 0, 0, 0, 312, (select id from working_time_types where description = 'Maternità CNR'), 0, 0, 0, now());
 
 INSERT INTO working_time_type_days (break_ticket_time, day_of_week, holiday, meal_ticket_time, time_meal_from, time_meal_to, time_slot_entrance_from,time_slot_entrance_to, 
 time_slot_exit_from, time_slot_exit_to, working_time, working_time_type_id, ticket_afternoon_threshold, ticket_afternoon_working_time, version, updated_at)
-VALUES (0, 4, false, 240, 0, 0, 0, 0, 0, 0, 312, (select id from working_time_types where description = 'Maternità CNR'), 0, 0, 0, now());
+VALUES (30, 4, false, 240, 0, 0, 0, 0, 0, 0, 312, (select id from working_time_types where description = 'Maternità CNR'), 0, 0, 0, now());
 
 INSERT INTO working_time_type_days (break_ticket_time, day_of_week, holiday, meal_ticket_time, time_meal_from, time_meal_to, time_slot_entrance_from,time_slot_entrance_to, 
 time_slot_exit_from, time_slot_exit_to, working_time, working_time_type_id, ticket_afternoon_threshold, ticket_afternoon_working_time, version, updated_at)
-VALUES (0, 5, false, 240, 0, 0, 0, 0, 0, 0, 312, (select id from working_time_types where description = 'Maternità CNR'), 0, 0, 0, now());
+VALUES (30, 5, false, 240, 0, 0, 0, 0, 0, 0, 312, (select id from working_time_types where description = 'Maternità CNR'), 0, 0, 0, now());
 
 INSERT INTO working_time_type_days (break_ticket_time, day_of_week, holiday, meal_ticket_time, time_meal_from, time_meal_to, time_slot_entrance_from,time_slot_entrance_to, 
 time_slot_exit_from, time_slot_exit_to, working_time, working_time_type_id, ticket_afternoon_threshold, ticket_afternoon_working_time, version, updated_at)
-VALUES (0, 6, true, 0, 0, 0, 0, 0, 0, 0, 312, (select id from working_time_types where description = 'Maternità CNR'), 0, 0, 0, now());
+VALUES (30, 6, true, 0, 0, 0, 0, 0, 0, 0, 312, (select id from working_time_types where description = 'Maternità CNR'), 0, 0, 0, now());
 
 INSERT INTO working_time_type_days (break_ticket_time, day_of_week, holiday, meal_ticket_time, time_meal_from, time_meal_to, time_slot_entrance_from,time_slot_entrance_to, 
 time_slot_exit_from, time_slot_exit_to, working_time, working_time_type_id, ticket_afternoon_threshold, ticket_afternoon_working_time, version, updated_at)
-VALUES (0, 7, true, 0, 0, 0, 0, 0, 0, 0, 312, (select id from working_time_types where description = 'Maternità CNR'), 0, 0, 0, now());
+VALUES (30, 7, true, 0, 0, 0, 0, 0, 0, 0, 312, (select id from working_time_types where description = 'Maternità CNR'), 0, 0, 0, now());
 
 # --- !Downs
 -- Non è necessaria una down
