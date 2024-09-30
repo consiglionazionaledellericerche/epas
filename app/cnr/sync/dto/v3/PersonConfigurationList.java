33b51a90d3a739e3c2691af79dd179da3743e24a
Dario Tagliaferri <dario.tagliaferri@iit.cnr.it>
2024-09-30 15:48:39 +0200

    Aggiunto dto per le configurazioni delle persone

--- app/cnr/sync/dto/v3/PersonConfigurationList.java
+++ app/cnr/sync/dto/v3/PersonConfigurationList.java
@@ -17,28 +17,13 @@
 
 package cnr.sync.dto.v3;
 
+import java.util.List;
 import org.joda.time.LocalDate;
-import org.modelmapper.ModelMapper;
 import lombok.Data;
-import lombok.val;
-import models.Configuration;
-import models.PersonConfiguration;
 
 @Data
-public class PersonConfigurationShowDto {
+public class PersonConfigurationList {
 
-  private String epasParam;
-  private String fieldValue;
-  private LocalDate beginDate;
-  private LocalDate endDate;
-  
-  public static PersonConfigurationShowDto build(PersonConfiguration personConfiguration) {
-    ModelMapper modelMapper = new ModelMapper();
-    modelMapper.getConfiguration().setAmbiguityIgnored(true);
-    val configurationDto = modelMapper.map(personConfiguration, PersonConfigurationShowDto.class);
-    if (personConfiguration.getEpasParam() != null) {
-      configurationDto.setEpasParam(personConfiguration.getEpasParam().name);
-    }
-    return configurationDto;
-  }
+  private String number;
+  private List<PersonConfigurationShowDto> list;
 }
