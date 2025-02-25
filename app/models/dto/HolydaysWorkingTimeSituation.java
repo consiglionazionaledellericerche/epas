7db858c11ece460b1a746c9f4e9793fff4594bc2
dario <dario@darietto-portatile>
2025-02-25 14:30:35 +0100

    Quasi completata la procedura che permette di specificare meglio le quantità di ore disponibili per il lavoro straordinario festivo da assegnare da parte dell'amministratore del personale da interfaccia.

--- app/models/dto/HolydaysWorkingTimeSituation.java
+++ app/models/dto/HolydaysWorkingTimeSituation.java
@@ -17,30 +17,10 @@
 
 package models.dto;
 
-import java.time.LocalDate;
-import java.time.format.TextStyle;
-import java.util.Locale;
-import javax.persistence.Transient;
-
-/**
- * Dto per l'oggetto telework da mandare al teleworkstampings.
- *
- * @author dario
- *
- */
-public class NewTeleworkDto {
-
-  public LocalDate date;
-  public TeleworkDto beginDay;
-  public TeleworkDto endDay;
-  public TeleworkDto beginMeal;
-  public TeleworkDto endMeal;
-  public TeleworkDto beginInterruption;
-  public TeleworkDto endInterruption;
+public class HolydaysWorkingTimeSituation {
   
-  @Transient
-  public String displayDay() {
-    return "" + this.date.getDayOfMonth() + '\t' 
-        + this.date.getDayOfWeek().getDisplayName(TextStyle.SHORT, Locale.ITALY);
-  }
+  public int decurtedTimeAtWork;
+  
+  public int assignedTimeAtWork;
+
 }
